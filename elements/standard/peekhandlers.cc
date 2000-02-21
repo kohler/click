0cfb379a244998868563e68d7fd25001c3c9ff9f
eddietwo <eddietwo>
2000-02-21 23:02:07 +0000

    peekhandlers

---
+++ elements/standard/peekhandlers.cc
@@ -0,0 +1,116 @@
+/*
+ * peekhandlers.{cc,hh} -- element runs read handlers
+ * Eddie Kohler
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology.
+ *
+ * This software is being provided by the copyright holders under the GNU
+ * General Public License, either version 2 or, at your discretion, any later
+ * version. For more information, see the `COPYRIGHT' file in the source
+ * distribution.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include "peekhandlers.hh"
+#include "confparse.hh"
+#include "error.hh"
+#include "router.hh"
+
+PeekHandlers::PeekHandlers()
+  : _timer(timer_hook, (unsigned long)this)
+{
+}
+
+PeekHandlers::~PeekHandlers()
+{
+  uninitialize();
+}
+
+int
+PeekHandlers::configure(const String &conf, ErrorHandler *errh)
+{
+  ErrorHandler *silent_errh = ErrorHandler::silent_handler();
+  _h_element.clear();
+  _h_handler.clear();
+  _h_timeout.clear();
+  
+  Vector<String> args;
+  cp_argvec(conf, args);
+  int next_timeout = 0;
+  for (int i = 0; i < args.size(); i++) {
+    String first;
+    cp_word(args[i], first);
+    int gap;
+    if (cp_va_parse(first, this, silent_errh,
+		    cpInterval, "timeout interval", &gap, 0) >= 0) {
+      next_timeout += gap;
+      continue;
+    } else {
+      int dot = first.find_left('.');
+      if (dot >= 0) {
+	if (Element *e = router()->find(this, first.substring(0, dot), errh)) {
+	  _h_element.push_back(e);
+	  _h_handler.push_back(first.substring(dot + 1));
+	  _h_timeout.push_back(next_timeout);
+	  next_timeout = 0;
+	}
+	continue;
+      }
+    }
+    errh->error("argument %d: expected `TIMEOUT' or `ELEMENT.HANDLER'",
+		i + 1);
+  }
+
+  return 0;
+}
+
+int
+PeekHandlers::initialize(ErrorHandler *)
+{
+  _pos = 0;
+  if (_h_timeout.size() != 0)
+    _timer.schedule_after_ms(_h_timeout[0] + 1);
+  return 0;
+}
+
+void
+PeekHandlers::uninitialize()
+{
+  _timer.unschedule();
+}
+
+
+void
+PeekHandlers::timer_hook(unsigned long thunk)
+{
+  PeekHandlers *peek = (PeekHandlers *)thunk;
+  ErrorHandler *errh = ErrorHandler::default_handler();
+  Router *router = peek->router();
+
+  int h = peek->_pos;
+  do {
+    Element *he = peek->_h_element[h];
+    String hname = peek->_h_handler[h];
+    int i = router->find_handler(he, hname);
+    if (i < 0)
+      errh->error("%s: no handler `%s.%s'", peek->id().cc(), he->id().cc(), hname.cc());
+    else {
+      const Router::Handler &rh = router->handler(i);
+      if (rh.read) {
+	String value = rh.read(he, rh.read_thunk);
+	errh->message("%s.%s:", he->id().cc(), hname.cc());
+	errh->message("%s", value.cc());
+      } else
+	errh->error("%s: no read handler `%s.%s'", peek->id().cc(), he->id().cc(), hname.cc());
+    }
+    h++;
+  } while (h < peek->_h_timeout.size() && peek->_h_timeout[h] == 0);
+
+  if (h < peek->_h_timeout.size())
+    peek->_timer.schedule_after_ms(peek->_h_timeout[h]);
+  peek->_pos = h;
+}
+
+EXPORT_ELEMENT(PeekHandlers)
---
+++ elements/standard/peekhandlers.hh
@@ -0,0 +1,49 @@
+#ifndef PEEKHANDLERS_HH
+#define PEEKHANDLERS_HH
+#include "element.hh"
+#include "timer.hh"
+
+/*
+ * =c
+ * PeekHandlers([TIME or HANDLER] ...)
+ * =io
+ * None
+ * =d
+ * Runs read handlers automatically at specified times. Each configuration
+ * argument should be either `DELAY', a delay in seconds, or
+ * `ELEMENT.HANDLERNAME', a directive to call ELEMENT's
+ * HANDLERNAME read handler and report the result.
+ * PeekHandlers processes its arguments in order,
+ * reading handlers as they appear.
+ * A `DELAY' directive causes it to wait for DELAY seconds before
+ * continuing.
+ * =e
+ * = PeekHandlers(q.length,
+ * =              1.5, // delay for 1.5 seconds
+ * =              q.length);
+ */
+
+class PeekHandlers : public Element {
+
+  int _pos;
+  Vector<Element *> _h_element;
+  Vector<String> _h_handler;
+  Vector<int> _h_timeout;
+  Timer _timer;
+
+  static void timer_hook(unsigned long);
+  
+ public:
+
+  PeekHandlers();
+  ~PeekHandlers();
+
+  const char *class_name() const		{ return "PeekHandlers"; }
+  PeekHandlers *clone() const			{ return new PeekHandlers; }
+  int configure(const String &, ErrorHandler *);
+  int initialize(ErrorHandler *);
+  void uninitialize();
+
+};
+
+#endif
