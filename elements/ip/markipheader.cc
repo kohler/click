25f3b8fadcfea0ff535a34923d4b7644bba130b1
eddietwo <eddietwo>
2000-03-16 17:04:31 +0000

    markipheader

---
+++ elements/ip/markipheader.cc
@@ -0,0 +1,49 @@
+/*
+ * markipheader.{cc,hh} -- element sets IP Header annotation
+ * Eddie Kohler
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology.
+ *
+ * This software is being provided by the copyright holders under the GNU
+ * General Public License, either version 2 or, at your discretion, any later
+ * version. For more information, see the `COPYRIGHT' file in the source
+ * distribution.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include "markipheader.hh"
+#include "confparse.hh"
+#include "click_ip.h"
+
+MarkIPHeader::MarkIPHeader()
+{
+  add_input();
+  add_output();
+}
+
+MarkIPHeader *
+MarkIPHeader::clone() const
+{
+  return new MarkIPHeader();
+}
+
+int
+MarkIPHeader::configure(const String &conf, ErrorHandler *errh)
+{
+  _offset = 0;
+  return cp_va_parse(conf, this, errh,
+		     cpUnsigned, "offset to IP header", &_offset,
+		     0);
+}
+
+Packet *
+MarkIPHeader::simple_action(Packet *p)
+{
+  click_ip *ip = (click_ip *)(p->data() + _offset);
+  p->set_ip_header(ip);
+  return p;
+}
+
+EXPORT_ELEMENT(MarkIPHeader)
---
+++ elements/ip/markipheader.hh
@@ -0,0 +1,35 @@
+#ifndef MARKIPHEADER_HH
+#define MARKIPHEADER_HH
+
+/*
+ * =c
+ * MarkIPHeader([OFFSET])
+ * =d
+ * Marks packets as IP packets by setting the IP Header annotation.
+ * The IP header starts OFFSET bytes into the packet.
+ * Default OFFSET is 0.
+ *
+ * =a CheckIPHeader
+ * =a CheckIPHeader2
+ */
+
+#include "element.hh"
+
+class MarkIPHeader : public Element {
+
+  int _offset;
+  
+ public:
+  
+  MarkIPHeader();
+  
+  const char *class_name() const		{ return "MarkIPHeader"; }
+  int configure(const String &, ErrorHandler *);
+  
+  MarkIPHeader *clone() const;
+
+  Packet *simple_action(Packet *);
+  
+};
+
+#endif
