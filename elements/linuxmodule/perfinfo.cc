cb400304e8140216c3a24a8e46deb2ee14d1fe33
benjie <benjie>
2000-04-25 15:11:51 +0000

    *** empty log message ***

--- elements/linuxmodule/perfinfo.cc
+++ elements/linuxmodule/perfinfo.cc
@@ -43,6 +43,7 @@ PerfInfo::configure(const String &conf, ErrorHandler *errh)
   metrics.insert("IFU_FETCH", 0x80);
   metrics.insert("IFU_FETCH_MISS", 0x81);
   metrics.insert("L2_IFETCH", 0x28 | (0xf<<8));
+  metrics.insert("L2_LD", 0x29 | (0xf<<8));
   metrics.insert("L2_LINES_OUTM", 0x27);
   metrics.insert("L2_RQSTS", 0x2e | (0xf<<8));
   metrics.insert("BUS_TRAN_MEM", 0x6f);
