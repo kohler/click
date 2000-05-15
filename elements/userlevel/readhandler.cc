6d66d7ffd48b67e45010cbfcdd4955a6cbf47235
benjie <benjie>
2000-05-15 16:18:17 +0000

    *** empty log message ***

--- elements/userlevel/readhandler.cc
+++ elements/userlevel/readhandler.cc
@@ -5,21 +5,43 @@
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
+#include "glue.hh"
+#include "confparse.hh"
 #include "readhandler.hh"
 #include "../standard/scheduleinfo.hh"
-  
+ 
+int
+ReadHandlerCaller::configure(const String &conf, ErrorHandler *errh)
+{
+  if (cp_va_parse(conf, this, errh,
+		  cpUnsigned, "approximate duration between run", &_n,
+		  cpEnd) < 0)
+    return -1;
+  return 0;
+}
+
 int
 ReadHandlerCaller::initialize(ErrorHandler *errh)
 {
-  ScheduleInfo::join_scheduler(this, errh);
+#ifndef RR_SCHED
+  int max_tickets = ScheduleInfo::query(this,errh);
+  set_max_tickets(max_tickets);
+  set_tickets(max_tickets);
+#endif
+  join_scheduler();
+  _last_time = click_jiffies();
   return 0;
 }
   
 void
 ReadHandlerCaller::run_scheduled()
 {
-  extern int call_read_handlers();
-  call_read_handlers();
+  int jiffs = click_jiffies();
+  if ((jiffs-_last_time)/CLICK_HZ > _n) {
+    extern int call_read_handlers();
+    call_read_handlers();
+    _last_time = jiffs;
+  }
   reschedule();
 }
 
