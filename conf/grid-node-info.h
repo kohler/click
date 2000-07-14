be9a7942983d647e8efa319bd905dbe4402dada3
decouto <decouto>
2000-07-14 15:04:57 +0000

    user level grid client configs for use with cpp

---
+++ conf/grid-node-info.h
@@ -0,0 +1,33 @@
+// -*- c++ -*-
+// grid-node-info.h
+
+// Grid physical device
+#define NET_DEVICE eth0
+
+// MAC address of NET_DEVICE
+#define MAC_ADDR  00:E0:98:09:27:C5
+
+// tunnel device for user-level grid configuration
+#define TUN_DEVICE tap
+
+// this node's Grid address information
+#define GRID_IP 18.26.7.2
+#define GRID_NETMASK 255.255.255.0
+
+// Grid network number in Click hex notation, with IP source address
+// offset (typically 16) e.g. 16/121a07
+#define GRID_NET_HEX 16/121a07 
+
+// Grid network gateway
+#define GRID_GW 18.26.7.1
+
+// immediate neighbor table entry timeout
+#define NBR_TIMEOUT 2000 // msecs
+
+// Hello broadcast paramters
+#define HELLO_PERIOD 500 // msecs
+#define HELLO_JITTER 100 // msecs
+
+// Grid ethernet protocol number in Click hex notation with ethernet
+// protocol offset (typically 12) e.g. 12/BABE
+#define GRID_ETH_PROTO 12/BABE
---
+++ conf/grid2.cpp
@@ -0,0 +1,32 @@
+// -*- c++ *-
+// grid2.cpp
+
+// version of grid2.click designed for running through cpp
+
+// use like: gcc -E -P grid2.cpp | click
+
+#include "grid-node-info.h"
+
+rh :: ReadHandlerCaller(1) 
+
+// protocol els
+nb :: Neighbor(NBR_TIMEOUT, MAC_ADDR, GRID_IP)
+h :: Hello(HELLO_PERIOD, HELLO_JITTER, MAC_ADDR, GRID_IP)
+
+// device layer els
+ps :: PacketSocket(NET_DEVICE, 0)
+q :: Queue -> ps
+
+// linux ip layer els
+linux :: Tun(TUN_DEVICE, GRID_IP, GRID_NETMASK)
+
+// hook it all up
+ps -> Classifier(GRID_ETH_PROTO) -> [0] nb [0] -> q 
+
+linux -> cl :: Classifier(GRID_NET_HEX, // ip for Grid network
+			  -) // the rest of the world
+cl [0] -> GetIPAddress(16) -> [1] nb [1] -> Queue -> ck :: CheckIPHeader [0] -> linux
+ck [1] -> Discard
+cl [1] -> SetIPAddress(GRID_GW) -> [1] nb // for grid gateway
+h -> q 
+
