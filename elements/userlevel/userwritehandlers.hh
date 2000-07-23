c4ca757a4b10c24a77e3cf54e3a121cf57937c12
decouto <decouto>
2000-07-23 00:29:32 +0000

    *** empty log message ***

---
+++ elements/userlevel/userwritehandlers.hh
@@ -0,0 +1,60 @@
+#ifndef USERWRITEHANDLERS_HH
+#define USERWRITEHANDLERS_HH
+#include "element.hh"
+#include "timer.hh"
+
+/*
+ * =c
+ * UserWriteHandlers(SOCK [, MAXCMDLEN])
+ * =io
+ * None
+ * =d
+ *
+ * the args are really: (SOCK, PERIOD [, MAXCMDLEN) because i can't
+ * get the select-based implementation working right now.
+ *
+ * Runs write handlers as read from the UNIX domain socket named SOCK.
+ * Command strings of the form ``handler-name;handler-arg\n'', causing
+ * handler-name(handler-arg) to be run.
+ *
+ * The number of chars in the command string must be less than
+ * MAXCMDLEN, which default to 255.  */
+
+/* XXX hmmm... the select implementation was working briefly but no it
+   stopped, once i put in the UWH_USE_SELECT macro... */
+#define UWH_USE_SELECT 0
+
+class UserWriteHandlers : public Element {
+
+#if !UWH_USE_SELECT
+  Timer _timer;
+  int _period; // msecs
+#endif
+  String _fname;
+  int _buflen;
+  char *_buf;
+  int _fd;
+  int _pos;
+
+  static void timer_hook(unsigned long);
+  int read_command_strings(String &hndlr_str, String &arg_str, ErrorHandler *errh);
+  
+ public:
+
+  UserWriteHandlers();
+  ~UserWriteHandlers();
+
+  const char *class_name() const		{ return "UserWriteHandlers"; }
+  UserWriteHandlers *clone() const		{ return new UserWriteHandlers; }
+  int configure(const Vector<String> &, ErrorHandler *);
+  int initialize(ErrorHandler *);
+  void uninitialize();
+
+#if UWH_USE_SELECT
+  int select_fd() const { return _fd; }
+  void selected(int fd);
+#endif
+
+};
+
+#endif
