c8efbc7b7da065b8f1791f0d4c256c0a815a53ed
benjie <benjie>
2000-08-23 20:30:14 +0000

    *** empty log message ***

---
+++ lib/bigewma.cc
@@ -0,0 +1,31 @@
+/*
+ * bigewma.{cc,hh} -- Exponential Weighted Moving Averages using ulonglong
+ * Eddie Kohler
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology.
+ *
+ * This software is being provided by the copyright holders under the GNU
+ * General Public License, either version 2 or, at your discretion, any later
+ * version. For more information, see the `COPYRIGHT' file in the source
+ * distribution.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include "bigewma.hh"
+
+template <unsigned stability_shift, unsigned scale>
+void
+DirectBigEWMAX<stability_shift, scale>::update_zero_period(unsigned period)
+{
+  // XXX use table lookup
+  if (period >= 100)
+    _avg = 0;
+  else {
+    int compensation = 1 << (stability_shift - 1); // round off
+    for (; period > 0; period--)
+      _avg += static_cast<signed long long>
+	(-_avg + compensation) >> stability_shift;
+  }
+}
---
+++ lib/bigewma.hh
@@ -0,0 +1,119 @@
+#ifndef BIGEWMA_HH
+#define BIGEWMA_HH
+#include "glue.hh"
+#include "ewma.hh"
+
+template <unsigned Stability_shift, unsigned Scale>
+class DirectBigEWMAX {
+  
+  unsigned long long _avg;
+  
+ public:
+
+  DirectBigEWMAX()				{ _avg = 0; }
+
+  unsigned long long average() const		{ return _avg; }
+  static const unsigned stability_shift = Stability_shift;
+  static const unsigned scale = Scale;
+  
+  void clear()				{ _avg = 0; }
+  
+  inline void update_with(unsigned long long);
+  void update_zero_period(unsigned);
+};
+
+template <unsigned Stability_shift, unsigned Scale, unsigned N, class Timer>
+class RateBigEWMAX {
+  
+  unsigned _now_time;
+  unsigned long long _total[N];
+  DirectBigEWMAX<Stability_shift, Scale> _avg[N];
+  
+ public:
+
+  RateBigEWMAX()				{ }
+
+  unsigned long long average(unsigned which = 0) const	
+  					{ return _avg[which].average(); }
+  static const int stability_shift = Stability_shift;
+  static const int scale = Scale;
+  static unsigned now()			{ return Timer::now(); }
+  static unsigned freq()		{ return Timer::freq(); }
+  
+  void initialize();
+ 
+  inline void update_time(unsigned now);
+  inline void update_time();
+  inline void update_now(long long delta, unsigned which = 0);
+  inline void update(long long delta, unsigned which = 0);
+};
+
+typedef DirectBigEWMAX<4, 10> DirectBigEWMA;
+typedef RateBigEWMAX<4, 10, 1, JiffiesTimer> RateBigEWMA;
+
+template <unsigned stability_shift, unsigned scale>
+inline void
+DirectBigEWMAX<stability_shift, scale>::update_with(unsigned long long val)
+{
+  int val_scaled = val << scale;
+  int compensation = 1 << (stability_shift - 1); // round off
+  _avg += static_cast<signed long long>
+    (val_scaled - _avg + compensation) >> stability_shift;
+  // XXX implementation-defined right shift behavior
+}
+
+template <unsigned stability_shift, unsigned scale, unsigned n, class Timer>
+inline void
+RateBigEWMAX<stability_shift, scale, n, Timer>::initialize()
+{
+  _now_time = now();
+  for (unsigned i = 0; i < n; i++) {
+    _total[i] = 0;
+    _avg[i].clear();
+  }
+}
+
+template <unsigned stability_shift, unsigned scale, unsigned n, class Timer>
+inline void
+RateBigEWMAX<stability_shift, scale, n, Timer>::update_time(unsigned now)
+{
+  unsigned jj = _now_time;
+  if (now != jj) {
+    for (unsigned i = 0; i < n; i++) {
+      // adjust the average rate using the last measured packets
+      _avg[i].update_with(_total[i]);
+
+      // adjust for time w/ no packets
+      if (jj + 1 != now)
+        _avg[i].update_zero_period(now - jj - 1);
+      _total[i] = 0;
+    }
+    _now_time = now;
+  }
+}
+
+template <unsigned stability_shift, unsigned scale, unsigned n, class Timer>
+inline void 
+RateBigEWMAX<stability_shift, scale, n, Timer>::update_now(long long delta, 
+                                                           unsigned which)
+{ 
+  _total[which] += delta; 
+}
+
+template <unsigned stability_shift, unsigned scale, unsigned n, class Timer>
+inline void
+RateBigEWMAX<stability_shift, scale, n, Timer>::update_time()
+{
+  update_time(now());
+}
+
+template <unsigned stability_shift, unsigned scale, unsigned n, class Timer>
+inline void
+RateBigEWMAX<stability_shift, scale, n, Timer>::update(long long delta, 
+                                                       unsigned which)
+{
+  update_time();
+  update_now(delta, which);
+}
+
+#endif
