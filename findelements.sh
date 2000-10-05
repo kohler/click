311edb32ec4cc83789475f9c6fd35565f13eac07
eddietwo <eddietwo>
2000-10-05 03:56:33 +0000

    findelements.sh --help

--- findelements.sh
+++ findelements.sh
@@ -21,20 +21,55 @@
 prefix=""
 all=0
 verbose=""
+provisions=""
 while [ x"$1" != x ]; do
 case $1 in
-  -p|--p|--pr|--pre|--pref|--prefi|--prefix)
+  -p|--pre|--pref|--prefi|--prefix)
      shift 1; prefix="$1/"; shift 1;;
   -p*)
      prefix=`echo "$1" | sed 's/^-p//'`/; shift 1;;
-  --p=*|--pr=*|--pre=*|--pref=*|--prefi=*|--prefix=*)
+  --pre=*|--pref=*|--prefi=*|--prefix=*)
      prefix=`echo "$1" | sed 's/^[^=]*=//'`/; shift 1;;
-  -v|--v|--ve|--ver|--verb|--verbo|--verbos|--verbose)
+  -V|--v|--ve|--ver|--verb|--verbo|--verbos|--verbose)
      verbose=1; shift 1;;
   -a|--a|--al|--all)
      all=1; shift 1;;
+  -r|--pro|--prov|--provi|--provid|--provide)
+     shift 1; provisions="$1
+$provisions"; shift 1;;
+  -r*)
+     provisions=`echo "$1" | sed 's/^-r//'`"
+$provisions"; shift 1;;
+  --pro=*|--prov=*|--provi=*|--provid=*|--provide=*)
+     provisions=`echo "$1" | sed 's/^[^=]*=//'`"
+$provisions"; shift 1;;
+  -h|--h|--he|--hel|--help)
+     cat <<'EOF' 1>&2
+`Findelements.sh' locates valid Click element source code according to a set
+of provisions. It starts from a collection of source code, then repeatedly
+eliminates files whose requirements are not available until a stable set of
+sources is reached. It expects a list of files and directories on standard
+input. Directories are searched for .cc source files; any files found are
+considered. The initial list of available requirements is the list of
+directories given on the standard input, plus the list of EXPORT_ELEMENT()
+and ELEMENT_PROVIDES() keywords, plus any extra requirements specified
+with `-r'.
+
+Usage: findelements.sh [-a] [-V] [-pPREFIX] < [FILES AND DIRECTORIES]
+
+Options:
+  -a, --all                Include all subdirectories of `elements' rather
+                           than reading standard input.
+  -V, --verbose            Print more information about dependency checking.
+  -p, --prefix PREFIX      Preprend PREFIX to every file and/or directory.
+  -r, --provide REQ        Mark the requirement REQ as initially available.
+  -h, --help               Print this message and exit.
+
+Report bugs to <click@pdos.lcs.mit.edu>.
+EOF
+     exit 0;;
   *)
-     echo "Usage: ./findelements.sh [-a] [-v] [-pPREFIX] < [FILES AND DIRECTORIES]" 1>&2
+     echo "Usage: ./findelements.sh [-a] [-V] [-pPREFIX] < [FILES AND DIRECTORIES]" 1>&2
      exit 1;;
 esac
 done
@@ -51,7 +86,6 @@ else
 fi
 
 files=""
-directory_exports=""
 for i in $first_files; do
   pprefix="$prefix"
   if test -d "${prefix}elements/$i" -a ! -d "${prefix}$i"; then
@@ -61,8 +95,8 @@ for i in $first_files; do
     if echo "$i" | grep '/'; then
       :
     else
-      directory_exports="$i
-$directory_exports"
+      provisions="$i
+$provisions"
       i="${pprefix}$i"
     fi
   fi
@@ -99,7 +133,7 @@ while true; do
   exports2=`grep '^ELEMENT_PROVIDES' $files | sed 's/.*(\(.*\)).*/\1/'`
   awk_exports=`echo "$exports1"'
 '"$exports2"'
-'"$directory_exports" | sed 's/\(..*\)/dep["\1"]=1;/'`
+'"$provisions" | sed 's/\(..*\)/dep["\1"]=1;/'`
   new_bad_files=`grep '^ELEMENT_REQUIRES' $files | $awk -F: 'BEGIN {OFS="";'"$awk_exports"'dep["true"]=1; dep["1"]=1;
 }
 {
