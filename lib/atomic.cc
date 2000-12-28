c392a12dd2ade2f8b99b2aa4837658470120649b
eddietwo <eddietwo>
2000-12-28 16:14:54 +0000

    added

---
+++ lib/atomic.cc
@@ -0,0 +1,25 @@
+/*
+ * atomic.{cc,hh} -- atomic integers
+ * Eddie Kohler
+ *
+ * Copyright (c) 2000 Mazu Networks, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Further elaboration of this license, including a DISCLAIMER OF ANY
+ * WARRANTY, EXPRESS OR IMPLIED, is provided in the LICENSE file, which is
+ * also accessible at http://www.pdos.lcs.mit.edu/click/license.html
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <click/config.h>
+#include <click/glue.hh>
+#include <click/atomic.hh>
