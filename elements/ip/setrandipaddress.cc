7131539a9a8f778f35a355fcb6ba8e5be3e8d6e1
rtm <rtm>
2001-02-24 14:05:51 +0000

    SetRandIPAddress creates a random dest IP annotation, according to a given prefix.

---
+++ elements/ip/setrandipaddress.cc
@@ -0,0 +1,93 @@
+/*
+ * setipaddress.{cc,hh} -- element sets destination address annotation
+ * to a random IP address
+ * Eddie Kohler, Robert Morris
+ *
+ * Copyright (c) 2000 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Further elaboration of this license, including a DISCLAIMER OF ANY
+ * WARRANTY, EXPRESS OR IMPLIED, is provided in the LICENSE file, which is
+ * also accessible at http://www.pdos.lcs.mit.edu/click/license.html
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <click/config.h>
+#include <click/package.hh>
+#include "setrandipaddress.hh"
+#include <click/confparse.hh>
+
+SetRandIPAddress::SetRandIPAddress()
+  : Element(1, 1)
+{
+  MOD_INC_USE_COUNT;
+  _max = -1;
+  _addrs = 0;
+}
+
+SetRandIPAddress::~SetRandIPAddress()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+SetRandIPAddress::configure(const Vector<String> &conf, ErrorHandler *errh)
+{
+  int ret;
+
+  _max = -1;
+  ret = cp_va_parse(conf, this, errh,
+                    cpIPAddressOrPrefix, "IP address/len", &_ip, &_mask,
+                    cpOptional,
+                    cpInteger, "MAX addresses", &_max,
+                    0);
+
+  if(_max >= 0){
+    _addrs = new IPAddress [_max] ();
+
+    int i;
+    for(i = 0; i < _max; i++)
+      _addrs[i] = pick();
+  }
+
+  return(ret);
+}
+
+IPAddress
+SetRandIPAddress::pick()
+{
+  u_int32_t x;
+  u_int32_t mask = (u_int32_t) _mask;
+
+  x = (random() & ~mask) | ((u_int32_t)_ip & mask);
+  
+  return(IPAddress(x));
+}
+
+Packet *
+SetRandIPAddress::simple_action(Packet *p)
+{
+  IPAddress ipa;
+
+  if(_addrs && _max > 0){
+    ipa = _addrs[random() % _max];
+  } else {
+    ipa = pick();
+  }
+
+  p->set_dst_ip_anno(ipa);
+
+  return p;
+}
+
+EXPORT_ELEMENT(SetRandIPAddress)
+ELEMENT_MT_SAFE(SetRandIPAddress)
---
+++ elements/ip/setrandipaddress.hh
@@ -0,0 +1,42 @@
+#ifndef SetRandIPAddress_hh
+#define SetRandIPAddress_hh
+#include <click/element.hh>
+#include <click/ipaddress.hh>
+
+/*
+ * =c
+ * SetRandIPAddress(ADDR/LEN, [MAX])
+ * =s IP, annotations
+ * sets destination IP address annotations randomly
+ * =d
+ * Set the destination IP address annotation to a random number whose
+ * upper LEN bits are equal to ADDR.
+ *
+ * If MAX is given, at most MAX distinct addresses will be generated.
+ *
+ * =a StoreIPAddress, GetIPAddress, SetIPAddress
+ */
+
+class SetRandIPAddress : public Element {
+  
+  IPAddress _ip;
+  IPAddress _mask;
+  int _max;
+  IPAddress *_addrs;
+  
+ public:
+  
+  SetRandIPAddress();
+  ~SetRandIPAddress();
+  
+  const char *class_name() const	{ return "SetRandIPAddress"; }
+  const char *processing() const	{ return AGNOSTIC; }
+  SetRandIPAddress *clone() const	{ return new SetRandIPAddress; }
+  
+  int configure(const Vector<String> &, ErrorHandler *);
+  
+  Packet *simple_action(Packet *);
+  IPAddress pick();
+};
+
+#endif
