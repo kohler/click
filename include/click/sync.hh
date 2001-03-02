5b6135d43d48026d04d055768fcd98f2e69582c1
eddietwo <eddietwo>
2001-03-02 18:16:47 +0000

    rearranged code for readability

--- include/click/sync.hh
+++ include/click/sync.hh
@@ -1,82 +1,67 @@
 #ifndef SYNC_HH
 #define SYNC_HH
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#include <assert.h>
 #include <click/glue.hh>
-
-#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
-#include <linux/tasks.h>
-#include <linux/sched.h>
-extern atomic_t num_click_threads;
-#define my_cpu current->processor
-#define click_nthreads (atomic_read(&num_click_threads))
-#endif
-
 #include <click/atomic.hh>
+#include <assert.h>
+#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
+# include <linux/tasks.h>
+# include <linux/sched.h>
+# define my_cpu current->processor
+#endif
 
 // loop-in-cache spinlock implementation: 12 bytes. if the size of this class
 // changes, change size of padding in ReadWriteLock below.
 
-class Spinlock
-{
 #if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
-private:
-  volatile unsigned short _lock;
-  unsigned short _depth;
-  int _owner;
-  int _refcnt;
-#endif
 
-public:
-#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
-  Spinlock() : _lock(0), _depth(0), _owner(-1), _refcnt(0) {
-#ifndef __i386__
-    StaticAssert("no multithread support for non i386 click");
-#endif
-  } 
-#else
-  Spinlock() {}
-#endif
+class Spinlock { public:
+
+  Spinlock();
   ~Spinlock();
-
+  
   void acquire();
   void release();
   bool attempt();
   void ref();
   void unref();
+
+ private:
+
+  volatile unsigned short _lock;
+  unsigned short _depth;
+  int _owner;
+  int _refcnt;
+  
 };
+
+inline
+Spinlock::Spinlock()
+  : _lock(0), _depth(0), _owner(-1), _refcnt(0)
+{
+#ifndef __i386__
+  StaticAssert("no multithread support for non i386 click");
+#endif
+} 
   
 inline
 Spinlock::~Spinlock()
 {
-#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
   if (_lock != 0) 
     click_chatter("warning: freeing unreleased lock");
-#endif
 }
 
 inline void
 Spinlock::ref()
 {
-#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
   _refcnt++;
-#endif
 }
 
 inline void
 Spinlock::unref()
 {
-#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
   if (_refcnt > 0) _refcnt--;
   if (_refcnt == 0) delete this;
-#endif
 }
-
-#if defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__)
 
 inline void
 Spinlock::acquire()
@@ -87,7 +72,7 @@ Spinlock::acquire()
   }
   
   register unsigned short content = 1;
-test_n_set:
+ test_n_set:
   asm volatile ("xchgw %0,%1" :
                 "=r" (content),
                 "=m" (_lock) :
@@ -142,23 +127,22 @@ Spinlock::release()
   }
 }
 
-#else
+#else /* defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__) */
 
-inline void
-Spinlock::acquire()
-{}
+class Spinlock { public:
 
-inline void
-Spinlock::release()
-{}
+  Spinlock()			{ }
+  ~Spinlock()			{ }
 
-inline bool
-Spinlock::attempt()
-{
-  return true;
-}
+  void acquire()		{ }
+  void release()		{ }
+  bool attempt()		{ return true; }
+  void ref()			{ }
+  void unref()			{ }
+  
+};
 
-#endif
+#endif /* defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__) */
 
 
 // read-write lock:
@@ -195,7 +179,6 @@ public:
 inline void
 ReadWriteLock::acquire_read()
 {
-  if (click_nthreads <= 1) return;
   assert(my_cpu >= 0);
   _l[my_cpu]._lock.acquire();
 }
@@ -203,7 +186,6 @@ ReadWriteLock::acquire_read()
 inline bool
 ReadWriteLock::attempt_read()
 {
-  if (click_nthreads <= 1) return true;
   assert(my_cpu >= 0);
   return _l[my_cpu]._lock.attempt();
 }
@@ -211,7 +193,6 @@ ReadWriteLock::attempt_read()
 inline void
 ReadWriteLock::release_read()
 {
-  if (click_nthreads <= 1) return;
   assert(my_cpu >= 0);
   _l[my_cpu]._lock.release();
 }
@@ -219,7 +200,6 @@ ReadWriteLock::release_read()
 inline void
 ReadWriteLock::acquire_write()
 {
-  if (click_nthreads <= 1) return;
   for(unsigned i=0; i<NUM_CLICK_CPUS; i++)
     _l[i]._lock.acquire();
 }
@@ -228,7 +208,6 @@ inline bool
 ReadWriteLock::attempt_write()
 {
   bool all = true;
-  if (click_nthreads <= 1) return all;
   unsigned i;
   for(i=0; i<NUM_CLICK_CPUS; i++) {
     if (!(_l[i]._lock.attempt())) {
@@ -246,13 +225,11 @@ ReadWriteLock::attempt_write()
 inline void
 ReadWriteLock::release_write()
 {
-  if (click_nthreads <= 1) return;
   for(unsigned i=0; i<NUM_CLICK_CPUS; i++)
     _l[i]._lock.release();
 }
 
-#else
-
+#else /* defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__) */
 
 class ReadWriteLock { public:
   
@@ -267,7 +244,8 @@ class ReadWriteLock { public:
 
 };
 
+#endif /* defined(__KERNEL__) && defined(__SMP__) && defined(__MTCLICK__) */
+
 #endif
 
-#endif // SYNC_HH
 
