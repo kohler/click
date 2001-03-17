9022110ad97c81f3560e807e816cc40d81c7ebe2
eddietwo <eddietwo>
2001-03-17 02:38:22 +0000

    better code: actually pay attention to requirements and provisions

--- tools/click-mkmindriver/click-mkmindriver.cc
+++ tools/click-mkmindriver/click-mkmindriver.cc
@@ -21,6 +21,9 @@ static Clp_Option options[] = {
 
 static const char *program_name;
 
+static String::Initializer initialize_strings;
+static HashMap<String, int> initial_requirements(-1);
+
 void
 short_usage()
 {
@@ -42,6 +45,100 @@ Usage: %s ROUTERFILE\n\
 Report bugs to <click@pdos.lcs.mit.edu>.\n", program_name);
 }
 
+static void
+handle_router(const char *filename, const ElementMap &default_map, ErrorHandler *errh)
+{
+  int before = errh->nerrors();
+  RouterT *router = read_router_file(filename, new RouterT, errh);
+  if (router)
+    router->flatten(errh);
+  if (!router || errh->nerrors() != before)
+    return;
+  if (!filename || strcmp(filename, "-") == 0)
+    filename = "<stdin>";
+  
+  // find and parse `elementmap'
+  ElementMap emap(default_map);
+  emap.parse_requirement_files(router, CLICK_SHAREDIR, errh);
+  emap.limit_driver(ElementMap::DRIVER_USERLEVEL); // XXX
+  
+  for (int i = RouterT::FIRST_REAL_TYPE; i < router->ntypes(); i++) {
+    String tname = router->type_name(i);
+    int emap_index = emap.find(tname);
+    if (emap_index > 0) {
+      if (emap.package(emap_index))
+	/* do nothing; element was defined in a package */;
+      else
+	initial_requirements.insert(tname, 1);
+    } else
+      errh->warning("%s: cannot find required element class `%s'", filename, tname.cc());
+  }
+}
+
+static void
+add_provisions_and_requirements(int new_emapi, const ElementMap &emap,
+				HashMap<String, int> &provisions,
+				Vector<String> &requirements)
+{
+  const String &prov = emap.provisions(new_emapi);
+  if (prov) {
+    Vector<String> args;
+    cp_spacevec(prov, args);
+    for (int j = 0; j < args.size(); j++)
+      provisions.insert(args[j], 1);
+  }
+  const String &click_name = emap.name(new_emapi);
+  if (click_name)
+    provisions.insert(click_name, 1);
+
+  const String &req = emap.requirements(new_emapi);
+  if (req) {
+    Vector<String> args;
+    cp_spacevec(req, args);
+    for (int j = 0; j < args.size(); j++)
+      requirements.push_back(args[j]);
+  }
+}
+
+static void
+add_filename(int new_emapi, const ElementMap &emap,
+	     HashMap<String, int> &filenames)
+{
+  const String &fn = emap.header_file(new_emapi);
+  if (fn.substring(-3) == ".hh")
+    filenames.insert(fn.substring(0, -3) + ".cc", 1);
+}
+
+static void
+find_requirement(const String &requirement, const ElementMap &emap,
+		 Vector<int> &new_emapi, ErrorHandler *errh)
+{
+  int try_name_emapi = emap.find(requirement);
+  if (try_name_emapi > 0) {
+    new_emapi.push_back(try_name_emapi);
+    return;
+  }
+  
+  for (int i = 1; i < emap.size(); i++) {
+    const String &prov = emap.provisions(i);
+    if (prov) {
+      int where = prov.find_left(requirement);
+      if (where >= 0) {
+	// be careful about finding the requirement as part of a substring
+	int rwhere = where + requirement.length();
+	if ((where == 0 || isspace(prov[where - 1]))
+	    && (rwhere == prov.length() || isspace(prov[rwhere]))) {
+	  // found it!
+	  new_emapi.push_back(i);
+	  return;
+	}
+      }
+    }
+  }
+
+  errh->error("cannot satisfy requirement `%s' from default elementmap", String(requirement).cc());
+}
+
 int
 main(int argc, char **argv)
 {
@@ -56,7 +153,7 @@ main(int argc, char **argv)
   Clp_SetOptionChar(clp, '+', Clp_ShortNegated);
   program_name = Clp_ProgramName(clp);
 
-  const char *router_file = 0;
+  Vector<String> router_filenames;
   
   while (1) {
     int opt = Clp_Next(clp);
@@ -69,14 +166,9 @@ main(int argc, char **argv)
       
      case Clp_NotOption:
      case ROUTER_OPT:
-      if (router_file) {
-	errh->error("router file specified twice");
-	goto bad_option;
-      }
-      router_file = clp->arg;
+      router_filenames.push_back(clp->arg);
       break;
 
-     bad_option:
      case Clp_BadOption:
       short_usage();
       exit(1);
@@ -89,68 +181,58 @@ main(int argc, char **argv)
   }
   
  done:
-  RouterT *router = read_router_file(router_file, new RouterT, errh);
-  if (router)
-    router->flatten(errh);
-  if (!router || errh->nerrors() > 0)
-    exit(1);
-  
-  // find and parse `elementmap'
-  ElementMap full_elementmap;
-  full_elementmap.parse_all_required(router, CLICK_SHAREDIR, errh);
-  Vector<int> elements;
-  
-  for (int i=0; i<router->ntypes(); i++) {
-    int j = full_elementmap.find(router->type_name(i));
-    if (j >= 0) 
-      elements.push_back(j); 
-  }
-  
-  int j = full_elementmap.find("QuitWatcher"); 
-  for (int z=0; z<elements.size(); z++) { 
-    if (elements[z] == j) { 
-      j = -1; 
-      break; 
-    } 
-  }
-  if (j >= 0) elements.push_back(j); 
-  j = full_elementmap.find("ControlSocket"); 
-  for (int z=0; z<elements.size(); z++) { 
-    if (elements[z] == j) { 
-      j = -1; 
-      break; 
-    } 
-  }
-  if (j >= 0) elements.push_back(j); 
-  
+  if (!router_filenames.size())
+    router_filenames.push_back("-");
 
-  for (int i=0; i<elements.size(); i++) {
-    String requirement = full_elementmap.requirements(elements[i]); 
-    if (requirement) { 
-      Vector<String> reqs;
-      cp_spacevec(requirement, reqs); 
-      for (int k=0; k<reqs.size(); k++) { 
-        int j = full_elementmap.find_cxx(reqs[k]);
-	for (int z=0; z<elements.size(); z++) {
-	  if (elements[z] == j) {
-	    j = -1;
-	    break;
-	  }
-	}
-        if (j >= 0) elements.push_back(j);
-      }
+  ElementMap default_emap;
+  default_emap.parse_default_file(CLICK_SHAREDIR, errh);
+  
+  for (int i = 0; i < router_filenames.size(); i++)
+    handle_router(router_filenames[i], default_emap, errh);
+
+  initial_requirements.insert("QuitWatcher", 1);
+  initial_requirements.insert("ControlSocket", 1);
+  
+  HashMap<String, int> provisions(-1);
+  Vector<String> requirements;
+  HashMap<String, int> filenames(-1);
+
+  default_emap.limit_driver(ElementMap::DRIVER_USERLEVEL); // XXX
+  provisions.insert("userlevel", 1);
+  
+  // process initial requirements and provisions
+  for (HashMap<String, int>::Iterator iter = initial_requirements.first();
+       iter;
+       iter++) {
+    int emapi = default_emap.find(iter.key());
+    add_provisions_and_requirements(emapi, default_emap, provisions, requirements);
+    add_filename(emapi, default_emap, filenames);
+  }
+
+  // now, loop over requirements until you reach closure
+  while (1) {
+    Vector<int> new_emapi;
+    for (int i = 0; i < requirements.size(); i++)
+      if (provisions[ requirements[i] ] > 0)
+	/* OK; already have provision */;
+      else
+	find_requirement(requirements[i], default_emap, new_emapi, errh);
+
+    if (!new_emapi.size())
+      break;
+
+    requirements.clear();
+    for (int i = 0; i < new_emapi.size(); i++) {
+      add_provisions_and_requirements(new_emapi[i], default_emap, provisions, requirements);
+      add_filename(new_emapi[i], default_emap, filenames);
     }
   }
 
-  for (int i=0; i<elements.size(); i++) {
-    String fn = full_elementmap.header_file(elements[i]);
-    String src;
-    if (fn.substring(-3) == ".hh") { 
-      src = fn.substring(0, -3) + ".cc"; 
-      fprintf(stdout, "%s\n", src.cc());
-    }
-  }
+  // print out filenames
+  for (HashMap<String, int>::Iterator iter = filenames.first();
+       iter;
+       iter++)
+    printf("%s\n", String(iter.key()).cc());
+
   return 0;
-
 }
-
