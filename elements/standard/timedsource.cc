98b6d8689982a1a7552a0030d5546fa07d14b761
eddietwo <eddietwo>
2001-03-23 18:40:52 +0000

    add keyword arguments a la InfiniteSource/RatedSource and lots of handlers

--- elements/standard/timedsource.cc
+++ elements/standard/timedsource.cc
@@ -23,11 +23,10 @@
 #include <click/confparse.hh>
 #include <click/error.hh>
 #include <click/glue.hh>
+#include <click/router.hh>
 
 TimedSource::TimedSource()
-  : Element(0, 1),
-    _data("Random bullshit in a packet, at least 64 bytes long. Well, now it is."),
-    _timer(this)
+  : Element(0, 1), _packet(0), _timer(this)
 {
   MOD_INC_USE_COUNT;
 }
@@ -46,18 +45,41 @@ TimedSource::clone() const
 int
 TimedSource::configure(const Vector<String> &conf, ErrorHandler *errh)
 {
-  return cp_va_parse(conf, this, errh,
-		     cpMilliseconds, "packet generation interval", &_interval,
-		     cpOptional,
-		     cpString, "packet data", &_data,
-		     0);
+  String data = "Random bullshit in a packet, at least 64 bytes long. Well, now it is.";
+  int limit = -1;
+  int interval = 500;
+  bool active = true, stop = false;
+
+  if (cp_va_parse(conf, this, errh,
+		  cpOptional,
+		  cpMilliseconds, "packet generation interval", &interval,
+		  cpString, "packet data", &data,
+		  cpKeywords,
+		  "DATA", cpString, "packet data", &data,
+		  "INTERVAL", cpMilliseconds, "packet generation interval", &interval,
+		  "LIMIT", cpInteger, "total packet count", &limit,
+		  "ACTIVE", cpBool, "active?", &active,
+		  "STOP", cpBool, "stop driver when done?", &stop,
+		  0) < 0)
+    return -1;
+
+  _data = data;
+  _interval = interval;
+  _limit = limit;
+  _count = 0;
+  _active = active;
+  _stop = stop;
+  if (_packet) _packet->kill();
+  _packet = Packet::make(_data.data(), _data.length());
+  return 0;
 }
 
 int
 TimedSource::initialize(ErrorHandler *)
 {
   _timer.initialize(this);
-  _timer.schedule_after_ms(_interval);
+  if (_active)
+    _timer.schedule_after_ms(_interval);
   return 0;
 }
 
@@ -65,13 +87,111 @@ void
 TimedSource::uninitialize()
 {
   _timer.unschedule();
+  _packet->kill();
+  _packet = 0;
 }
 
 void
 TimedSource::run_scheduled()
 {
-  output(0).push(Packet::make(_data.data(), _data.length()));
-  _timer.schedule_after_ms(_interval);
+  if (_limit < 0 || _count < _limit) {
+    Packet *p = _packet->clone();
+    click_gettimeofday(&p->timestamp_anno());
+    output(0).push(p);
+    _count++;
+    _timer.schedule_after_ms(_interval);
+  } else if (_stop)
+    router()->please_stop_driver();
+}
+
+String
+TimedSource::read_param(Element *e, void *vparam)
+{
+  TimedSource *ts = (TimedSource *)e;
+  switch ((int)vparam) {
+   case 0:			// data
+    return ts->_data;
+   case 1:			// limit
+    return String(ts->_limit) + "\n";
+   case 2:			// interval
+    return cp_unparse_milliseconds(ts->_interval) + "\n";
+   case 3:			// active
+    return cp_unparse_bool(ts->_active) + "\n";
+   case 4:			// count
+    return String(ts->_count) + "\n";
+   default:
+    return "";
+  }
+}
+
+int
+TimedSource::change_param(const String &in_s, Element *e, void *vparam,
+			  ErrorHandler *errh)
+{
+  TimedSource *ts = (TimedSource *)e;
+  String s = cp_uncomment(in_s);
+  switch ((int)vparam) {
+
+   case 0: {			// data
+     String data;
+     if (!cp_string(s, &data))
+       return errh->error("data parameter must be string");
+     ts->_data = data;
+     if (ts->_packet) ts->_packet->kill();
+     ts->_packet = Packet::make(data.data(), data.length());
+     break;
+   }
+   
+   case 1: {			// limit
+     int limit;
+     if (!cp_integer(s, &limit))
+       return errh->error("limit parameter must be integer");
+     ts->_limit = limit;
+     break;
+   }
+   
+   case 2: {			// interval
+     int interval;
+     if (!cp_milliseconds(s, &interval) || interval < 1)
+       return errh->error("interval parameter must be integer >= 1");
+     ts->_interval = interval;
+     break;
+   }
+   
+   case 3: {			// active
+     bool active;
+     if (!cp_bool(s, &active))
+       return errh->error("active parameter must be boolean");
+     ts->_active = active;
+     if (!ts->_timer.scheduled() && active)
+       ts->_timer.schedule_now();
+     break;
+   }
+
+   case 5: {			// reset
+     ts->_count = 0;
+     if (!ts->_timer.scheduled() && ts->_active)
+       ts->_timer.schedule_now();
+     break;
+   }
+
+  }
+  return 0;
+}
+
+void
+TimedSource::add_handlers()
+{
+  add_read_handler("data", read_param, (void *)0);
+  add_write_handler("data", change_param, (void *)0);
+  add_read_handler("limit", read_param, (void *)1);
+  add_write_handler("limit", change_param, (void *)1);
+  add_read_handler("interval", read_param, (void *)2);
+  add_write_handler("interval", change_param, (void *)2);
+  add_read_handler("active", read_param, (void *)3);
+  add_write_handler("active", change_param, (void *)3);
+  add_read_handler("count", read_param, (void *)4);
+  add_write_handler("reset", change_param, (void *)5);
 }
 
 EXPORT_ELEMENT(TimedSource)
--- elements/standard/timedsource.hh
+++ elements/standard/timedsource.hh
@@ -4,28 +4,59 @@
 #include <click/timer.hh>
 
 /*
- * =c
- * TimedSource(I)
- * TimedSource(I, DATA)
- * =s sources
- * periodically generates a packet
- * =d
- * Creates packets consisting of DATA. Pushes such a packet out
- * its single output about once every I seconds.
- * Default DATA is at least 64 bytes long.
- * =n
- * Linux kernel timers have low granularity, so this element won't perform
- * very well in the kernel.
- * =a InfiniteSource
- */
+=c
 
-class TimedSource : public Element {
-  
-  String _data;
-  Timer _timer;
-  int _interval;
-  
- public:
+TimedSource([INTERVAL, DATA, I<KEYWORDS>])
+
+=s sources
+
+periodically generates a packet
+
+=d
+
+Creates packets consisting of DATA. Pushes such a packet out its single output
+about once every INTERVAL seconds. INTERVAL has millisecond precision. Default
+INTERVAL is 500 milliseconds; default DATA is at least 64 bytes long.
+
+Keyword arguments are:
+
+=over 8
+
+=item DATA
+
+String. Same as the DATA argument.
+
+=item INTERVAL
+
+Number of seconds. Same as the INTERVAL argument.
+
+=item LIMIT
+
+Integer. Stops sending after LIMIT packets are generated; but if LIMIT is
+negative, sends packets forever.
+
+=item STOP
+
+Boolean. If true, then stop the driver once LIMIT packets are sent. Default is
+false.
+
+=back
+
+=e
+
+  TimedSource(INTERVAL 0.333) -> ...
+
+=h data read/write
+
+Returns or sets the DATA parameter.
+
+=h interval read/write
+
+Returns or sets the INTERVAL parameter.
+
+=a InfiniteSource */
+
+class TimedSource : public Element { public:
   
   TimedSource();
   ~TimedSource();
@@ -37,8 +68,23 @@ class TimedSource : public Element {
   int configure(const Vector<String> &, ErrorHandler *);
   int initialize(ErrorHandler *);
   void uninitialize();
+  void add_handlers();
   
   void run_scheduled();
+  
+ private:
+  
+  Packet *_packet;
+  int _interval;
+  int _limit;
+  int _count;
+  bool _active : 1;
+  bool _stop : 1;
+  Timer _timer;
+  String _data;
+
+  static String read_param(Element *, void *);
+  static int change_param(const String &, Element *, void *, ErrorHandler *);
   
 };
 
