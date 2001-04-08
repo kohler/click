2a679f8010384a9cf585ca1a709b2f375ac5e208
benjie <benjie>
2001-04-08 20:57:36 +0000

    unqueue from multiple inputs in round robin fashion; has multiple outputs: send packets from input 0 to output 0, input 1 to output 1, etc.

---
+++ elements/local/rrunqueue.cc
@@ -0,0 +1,114 @@
+/*
+ * rrunqueue.{cc,hh} -- element pulls as many packets as possible from its
+ * inputs, pushes them out its outputs
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Further elaboration of this license, including a DISCLAIMER OF ANY
+ * WARRANTY, EXPRESS OR IMPLIED, is provided in the LICENSE file, which is
+ * also accessible at http://www.pdos.lcs.mit.edu/click/license.html
+ */
+
+#include <click/config.h>
+#include <click/package.hh>
+#include <click/error.hh>
+#include "rrunqueue.hh"
+#include <click/confparse.hh>
+#include "elements/standard/scheduleinfo.hh"
+
+RoundRobinUnqueue::RoundRobinUnqueue()
+  : _task(this), _next(0)
+{
+  MOD_INC_USE_COUNT;
+}
+
+RoundRobinUnqueue::~RoundRobinUnqueue()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+RoundRobinUnqueue::configure(const Vector<String> &conf, ErrorHandler *errh)
+{
+  _burst = 1;
+  return cp_va_parse(conf, this, errh,
+		     cpOptional,
+		     cpUnsigned, "burst size", &_burst,
+		     0);
+}
+
+int
+RoundRobinUnqueue::initialize(ErrorHandler *errh)
+{
+  if (noutputs() != ninputs()) {
+    return errh->error("number of outputs must equal to number of inputs");
+  }
+  _packets = 0;
+  ScheduleInfo::join_scheduler(this, &_task, errh);
+  return 0;
+}
+
+void
+RoundRobinUnqueue::uninitialize()
+{
+  _task.unschedule();
+}
+
+void
+RoundRobinUnqueue::run_scheduled()
+{
+  int sent = 0;
+  Packet *p_next = input(_next).pull();
+  
+  while (p_next) {
+    Packet *p = p_next;
+    sent++;
+    if (sent < _burst || _burst == 0) {
+      p_next = input(_next).pull();
+    }
+    else 
+      p_next = 0;
+#ifdef __KERNEL__
+#if __i386__ && HAVE_INTEL_CPU
+    if (p_next) {
+      struct sk_buff *skb = p_next->steal_skb();
+      asm volatile("prefetcht0 %0" : : "m" (skb->len));
+      asm volatile("prefetcht0 %0" : : "m" (skb->cb[0]));
+    }
+#endif
+#endif
+    output(_next).push(p);
+    _packets++;
+  }
+ 
+  if (_next == noutputs()-1) 
+    _next = 0;
+  else 
+    _next++;
+  _task.fast_reschedule();
+}
+
+String
+RoundRobinUnqueue::read_param(Element *e, void *)
+{
+  RoundRobinUnqueue *u = (RoundRobinUnqueue *)e;
+  return String(u->_packets) + " packets\n";
+}
+
+void
+RoundRobinUnqueue::add_handlers()
+{
+  add_read_handler("packets", read_param, (void *)0);
+  add_task_handlers(&_task);
+}
+
+EXPORT_ELEMENT(RoundRobinUnqueue)
+ELEMENT_MT_SAFE(RoundRobinUnqueue)
---
+++ elements/local/rrunqueue.hh
@@ -0,0 +1,49 @@
+#ifndef RRUNQUEUE_HH
+#define RRUNQUEUE_HH
+#include <click/element.hh>
+#include <click/task.hh>
+
+/*
+ * =c
+ * RoundRobinUnqueue([BURSTSIZE])
+ * =s packet scheduling
+ * pull-to-push converter
+ * =d
+ * Pulls packets from input ports in a round robin fashion, then pushes them
+ * out the output corresponding to the input that the packet came from. Pulls
+ * a maximum of BURSTSIZE packets every time it is scheduled. Default
+ * BURSTSIZE is 1. If BURSTSIZE is 0, pull until nothing comes back.
+ *
+ * =a Unqueue, RatedUnqueue, BandwidthRatedUnqueue
+ */
+
+class RoundRobinUnqueue : public Element { public:
+  
+  RoundRobinUnqueue();
+  ~RoundRobinUnqueue();
+
+  void notify_ninputs(int i) 		{ set_ninputs(i); }
+  void notify_noutputs(int i) 		{ set_noutputs(i); }
+
+  const char *class_name() const	{ return "RoundRobinUnqueue"; }
+  const char *processing() const	{ return PULL_TO_PUSH; }
+ 
+  RoundRobinUnqueue *clone() const	{ return new RoundRobinUnqueue; }
+  int configure(const Vector<String> &, ErrorHandler *);
+  int initialize(ErrorHandler *);
+  void uninitialize();
+  void add_handlers();
+  
+  void run_scheduled();
+
+  static String read_param(Element *e, void *);
+
+ private:
+
+  int _burst;
+  unsigned _packets;
+  Task _task;
+  int _next;
+};
+
+#endif
