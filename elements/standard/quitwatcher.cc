74d05e5aa47551414a544da76658f24924cc8710
eddietwo <eddietwo>
2001-04-17 04:28:48 +0000

    QuitWatcher: adjust to new ways of parsing handlers

--- elements/standard/quitwatcher.cc
+++ elements/standard/quitwatcher.cc
@@ -39,19 +39,31 @@ QuitWatcher::~QuitWatcher()
 }
 
 int
-QuitWatcher::configure(const Vector<String> &conf, ErrorHandler *errh)
+QuitWatcher::configure(const Vector<String> &, ErrorHandler *)
 {
-  int before = errh->nerrors();
-  for (int i = 0; i < conf.size(); i++) {
-    Element *e = router()->find(this, conf[i], errh);
-    if (e) _e.push_back(e);
-  }
-  return (errh->nerrors() == before ? 0 : -1);
+  // always succeed
+  return 0;
 }
 
 int
-QuitWatcher::initialize(ErrorHandler *)
+QuitWatcher::initialize(ErrorHandler *errh)
 {
+  // parse configuration arguments now, because handlers are available
+  Vector<String> conf;
+  configuration(conf);
+
+  for (int i = 0; i < conf.size(); i++) {
+    Element *e;
+    int hi;
+    if (cp_handler(conf[i] + ".scheduled", this, true, false, &e, &hi, errh)) {
+      _e.push_back(e);
+      _handlers.push_back(hi);
+    }
+  }
+
+  if (_e.size() < conf.size())
+    return -1;
+
   _timer.initialize(this);
   _timer.schedule_after_ms(10);
   return 0;
@@ -66,24 +78,6 @@ QuitWatcher::uninitialize()
 void
 QuitWatcher::run_scheduled()
 {
-  // collect handlers
-  if (_handlers.size() < _e.size()) {
-    for (int i = 0; i < _e.size(); i++) {
-      int hi = router()->find_handler(_e[i], "scheduled");
-      if (hi < 0)
-	click_chatter("%s: %s has no `scheduled' handler", id().cc(), _e[i]->id().cc());
-      else {
-	const Router::Handler &h = router()->handler(hi);
-	if (!h.read)
-	  click_chatter("%s: `%s.scheduled' is a write handler", id().cc(), _e[i]->id().cc());
-	else
-	  _handlers.push_back(hi);
-      }
-    }
-    if (_handlers.size() < _e.size())
-      return;
-  }
-
   String unscheduled_string = "false\n";
   for (int i = 0; i < _e.size(); i++) {
     const Router::Handler &h = router()->handler(_handlers[i]);
