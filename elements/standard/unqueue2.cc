af17b977cb4ab3cd8c66cef077075405b88afcc8
benjie <benjie>
2001-04-20 19:10:33 +0000

    a new unqueue element that does not pull and push unless there is room in downstream queues

---
+++ elements/standard/unqueue2.cc
@@ -0,0 +1,132 @@
+/*
+ * unqueue2.{cc,hh} -- element pulls as many packets as possible from its
+ * input, pushes them out its output. don't pull if queues downstream are
+ * full.
+ * Eddie Kohler, Benjie Chen
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Further elaboration of this license, including a DISCLAIMER OF ANY
+ * WARRANTY, EXPRESS OR IMPLIED, is provided in the LICENSE file, which is
+ * also accessible at http://www.pdos.lcs.mit.edu/click/license.html
+ */
+
+#include <click/config.h>
+#include <click/package.hh>
+#include <click/error.hh>
+#include <click/router.hh>
+#include <click/elemfilter.hh>
+#include "queue.hh"
+#include "unqueue2.hh"
+#include <click/confparse.hh>
+#include "elements/standard/scheduleinfo.hh"
+
+Unqueue2::Unqueue2()
+  : Element(1, 1), _task(this)
+{
+  MOD_INC_USE_COUNT;
+}
+
+Unqueue2::~Unqueue2()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+Unqueue2::configure(const Vector<String> &conf, ErrorHandler *errh)
+{
+  _burst = 1;
+  return cp_va_parse(conf, this, errh,
+		     cpOptional,
+		     cpUnsigned, "burst size", &_burst,
+		     0);
+}
+
+int
+Unqueue2::initialize(ErrorHandler *errh)
+{
+  CastElementFilter filter("Storage");
+  if (router()->downstream_elements(this, 0, &filter, _queue_elements) < 0)
+    return errh->error("flow-based router context failure");
+  filter.filter(_queue_elements);
+  click_chatter("Unqueue2: found %d downstream queues", _queue_elements.size());
+  _packets = 0;
+  ScheduleInfo::join_scheduler(this, &_task, errh);
+  return 0;
+}
+
+void
+Unqueue2::uninitialize()
+{
+  _task.unschedule();
+}
+
+void
+Unqueue2::run_scheduled()
+{
+  int burst = -1;
+  for (unsigned i=0; i<_queue_elements.size(); i++) {
+    Storage *s = _queue_elements[i]->cast("Storage");
+    if (s) {
+      int size = s->capacity()-s->size();
+      if (burst < 0 || size < burst) 
+	burst = size;
+    }
+  }
+  if (burst > _burst) burst = _burst;
+  else if (burst == 0) {
+    _task.fast_reschedule();
+    return;
+  }
+
+  int sent = 0;
+  Packet *p_next = input(0).pull();
+  
+  while (p_next) {
+    Packet *p = p_next;
+    sent++;
+    if (sent < burst || burst == 0) {
+      p_next = input(0).pull();
+    }
+    else 
+      p_next = 0;
+#ifdef __KERNEL__
+#if __i386__ && HAVE_INTEL_CPU
+    if (p_next) {
+      struct sk_buff *skb = p_next->steal_skb();
+      asm volatile("prefetcht0 %0" : : "m" (skb->len));
+      asm volatile("prefetcht0 %0" : : "m" (skb->cb[0]));
+    }
+#endif
+#endif
+    output(0).push(p);
+    _packets++;
+  }
+  
+  _task.fast_reschedule();
+}
+
+String
+Unqueue2::read_param(Element *e, void *)
+{
+  Unqueue2 *u = (Unqueue2 *)e;
+  return String(u->_packets) + " packets\n";
+}
+
+void
+Unqueue2::add_handlers()
+{
+  add_read_handler("packets", read_param, (void *)0);
+  add_task_handlers(&_task);
+}
+
+EXPORT_ELEMENT(Unqueue2)
+ELEMENT_MT_SAFE(Unqueue2)
---
+++ elements/standard/unqueue2.hh
@@ -0,0 +1,49 @@
+#ifndef UNQUEUE2_HH
+#define UNQUEUE2_HH
+#include <click/element.hh>
+#include <click/task.hh>
+
+/*
+ * =c
+ * Unqueue2([BURSTSIZE])
+ * =s packet scheduling
+ * pull-to-push converter
+ * =d
+ * Pulls packets whenever they are available, then pushes them out its single
+ * output. Pulls a maximum of BURSTSIZE packets every time it is scheduled,
+ * unless downstream queues are full. Default BURSTSIZE is 1. If BURSTSIZE is
+ * 0, pull until nothing comes back. Unqueue2 will not pull if there is a
+ * downstream queue that is full. It will also limit burst size to equal to
+ * the number of available slots in the fullest downstream queue.
+ *
+ * =a Unqueue, RatedUnqueue, BandwidthRatedUnqueue
+ */
+
+class Unqueue2 : public Element { public:
+  
+  Unqueue2();
+  ~Unqueue2();
+  
+  const char *class_name() const		{ return "Unqueue2"; }
+  const char *processing() const		{ return PULL_TO_PUSH; }
+  
+  Unqueue2 *clone() const			{ return new Unqueue2; }
+  int configure(const Vector<String> &, ErrorHandler *);
+  int initialize(ErrorHandler *);
+  void uninitialize();
+  void add_handlers();
+  
+  void run_scheduled();
+
+  static String read_param(Element *e, void *);
+
+ private:
+
+  int _burst;
+  unsigned _packets;
+  Task _task;
+  Vector<Element*> _queue_elements;
+
+};
+
+#endif
