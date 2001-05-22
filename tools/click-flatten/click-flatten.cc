ec21910a00327ae609cb7032eca93df39b44847f
eddietwo <eddietwo>
2001-05-22 19:32:28 +0000

    Add click-flatten tool (Mz)

---
+++ tools/click-flatten/click-flatten.cc
@@ -0,0 +1,139 @@
+#include <click/config.h>
+#include <click/pathvars.h>
+
+#include <click/error.hh>
+#include <click/package.hh>
+#include <click/confparse.hh>
+#include "lexert.hh"
+#include "routert.hh"
+#include "toolutils.hh"
+#include <click/clp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HELP_OPT		300
+#define VERSION_OPT		301
+#define CLICKPATH_OPT		302
+#define ROUTER_OPT		303
+#define OUTPUT_OPT		305
+
+static Clp_Option options[] = {
+  { "clickpath", 'C', CLICKPATH_OPT, Clp_ArgString, 0 },
+  { "file", 'f', ROUTER_OPT, Clp_ArgString, 0 },
+  { "help", 0, HELP_OPT, 0, 0 },
+  { "output", 'o', OUTPUT_OPT, Clp_ArgString, 0 },
+  { "version", 'v', VERSION_OPT, 0, 0 },
+};
+
+static const char *program_name;
+
+void
+short_usage()
+{
+  fprintf(stderr, "Usage: %s [OPTION]... [ROUTERFILE]\n\
+Try `%s --help' for more information.\n",
+	  program_name, program_name);
+}
+
+void
+usage()
+{
+  printf("\
+`Click-flatten' reads a Click configuration, flattens it (removing any\n\
+compound elements), and writes an equivalent configuration to the standard\n\
+output.\n\
+\n\
+Usage: %s [OPTION]... [ROUTERFILE]\n\
+\n\
+Options:\n\
+  -f, --file FILE           Read router configuration from FILE.\n\
+  -o, --output FILE         Write output configuration to FILE.\n\
+  -C, --clickpath PATH      Use PATH for CLICKPATH.\n\
+      --help                Print this message and exit.\n\
+  -v, --version             Print version number and exit.\n\
+\n\
+Report bugs to <click@pdos.lcs.mit.edu>.\n", program_name);
+}
+
+int
+main(int argc, char **argv)
+{
+  String::static_initialize();
+  cp_va_static_initialize();
+  ErrorHandler::static_initialize(new FileErrorHandler(stderr));
+  ErrorHandler *errh = ErrorHandler::default_handler();
+  CLICK_DEFAULT_PROVIDES;
+
+  // read command line arguments
+  Clp_Parser *clp =
+    Clp_NewParser(argc, argv, sizeof(options) / sizeof(options[0]), options);
+  Clp_SetOptionChar(clp, '+', Clp_ShortNegated);
+  program_name = Clp_ProgramName(clp);
+
+  const char *router_file = 0;
+  const char *output_file = 0;
+  
+  while (1) {
+    int opt = Clp_Next(clp);
+    switch (opt) {
+      
+     case HELP_OPT:
+      usage();
+      exit(0);
+      break;
+
+     case VERSION_OPT:
+      printf("click-flatten (Click) %s\n", CLICK_VERSION);
+      printf("Copyright (c) 2001 Mazu Networks, Inc.\n\
+Copyright (c) 2001 ACIRI\n\
+This is free software; see the source for copying conditions.\n\
+There is NO warranty, not even for merchantability or fitness for a\n\
+particular purpose.\n");
+      exit(0);
+      break;
+      
+     case CLICKPATH_OPT:
+      set_clickpath(clp->arg);
+      break;
+
+     case OUTPUT_OPT:
+      if (output_file) {
+	errh->error("--output file specified twice");
+	goto bad_option;
+      }
+      output_file = clp->arg;
+      break;
+
+     case Clp_NotOption:
+     case ROUTER_OPT:
+      if (router_file) {
+	errh->error("router file specified twice");
+	goto bad_option;
+      }
+      router_file = clp->arg;
+      break;
+
+     case Clp_BadOption:
+     bad_option:
+      short_usage();
+      exit(1);
+      break;
+      
+     case Clp_Done:
+      goto done;
+      
+    }
+  }
+  
+ done:
+  RouterT *router = read_router_file(router_file, errh);
+  if (router)
+    router->flatten(errh);
+  if (!router || errh->nerrors() > 0)
+    exit(1);
+
+  if (write_router_file(router, output_file, errh) < 0)
+    exit(1);
+  return 0;
+}
