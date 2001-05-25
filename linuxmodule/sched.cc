aaeaa1be99cdc1d400d59ad0ca4742903c999521
eddietwo <eddietwo>
2001-05-25 00:03:11 +0000

    everywhere: updated license summary (license remains the same)

--- linuxmodule/sched.cc
+++ linuxmodule/sched.cc
@@ -6,15 +6,13 @@
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Further elaboration of this license, including a DISCLAIMER OF ANY
- * WARRANTY, EXPRESS OR IMPLIED, is provided in the LICENSE file, which is
- * also accessible at http://www.pdos.lcs.mit.edu/click/license.html
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
  */
 
 #define CLICK_SCHED_DEBUG
