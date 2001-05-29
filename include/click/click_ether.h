76343db4e7ae1ef24e4b8f09ec25bfbd2fe4fd61
eddietwo <eddietwo>
2001-05-29 19:14:54 +0000

    rearrange header

--- include/click/click_ether.h
+++ include/click/click_ether.h
@@ -19,14 +19,14 @@ struct click_ether {
 #define ETHERTYPE_IP6	0x86DD
 #define ETHERTYPE_ARP	0x0806
 
-#define ETHERTYPE_GRID	0x7fff // wvlan_cs driver won't transmit frames with high bit of protocol number set
+#define ETHERTYPE_GRID	0x7fff	/* wvlan_cs driver won't transmit frames with high bit of protocol number set */
 
 struct click_arp {
-  unsigned short int ar_hrd;  /* Format of hardware address.  */
-  unsigned short int ar_pro;  /* Format of protocol address.  */
-  unsigned char ar_hln;       /* Length of hardware address.  */
-  unsigned char ar_pln;       /* Length of protocol address.  */
-  unsigned short int ar_op;   /* ARP opcode (command).  */
+  unsigned short int ar_hrd;	/* Format of hardware address.  */
+  unsigned short int ar_pro;	/* Format of protocol address.  */
+  unsigned char ar_hln;		/* Length of hardware address.  */
+  unsigned char ar_pln;		/* Length of protocol address.  */
+  unsigned short int ar_op;	/* ARP opcode (command).  */
 };
 
 /* ARP protocol HARDWARE identifiers. */
@@ -35,18 +35,18 @@ struct click_arp {
 /* ARP protocol opcodes. */
 #define ARPOP_REQUEST   1       /* ARP request          */
 #define ARPOP_REPLY 2           /* ARP reply            */
-#define ND_SOL 0x0087       /* Neighborhood Solicitation Message Type */
-#define ND_ADV 0x0088       /* Neighborhood Advertisement Message Type */
 
 struct click_ether_arp {
-  struct click_arp ea_hdr;    /* fixed-size header */
-  unsigned char arp_sha[6]; /* sender hardware address */
-  unsigned char arp_spa[4]; /* sender protocol address */
-  unsigned char arp_tha[6]; /* target hardware address */
-  unsigned char arp_tpa[4]; /* target protocol address */
+  struct click_arp ea_hdr;	/* fixed-size header */
+  unsigned char arp_sha[6];	/* sender hardware address */
+  unsigned char arp_spa[4];	/* sender protocol address */
+  unsigned char arp_tha[6];	/* target hardware address */
+  unsigned char arp_tpa[4];	/* target protocol address */
 };
 
 
+#define ND_SOL 0x0087       /* Neighborhood Solicitation Message Type */
+#define ND_ADV 0x0088       /* Neighborhood Advertisement Message Type */
 
 //define structure of Neighborhood Solicitation Message
 struct click_nd_sol {
