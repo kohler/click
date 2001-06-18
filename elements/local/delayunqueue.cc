0b4f96e9d0c27a0b704e6aa33e7a197d52ccff0f
benjie <benjie>
2001-06-18 20:15:07 +0000

    ::

---
+++ elements/local/delayunqueue.cc
@@ -0,0 +1,107 @@
+/*
+ * delayunqueue.{cc,hh} -- element pulls packets from input, delays pushing
+ * the packet to output port.
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include <click/package.hh>
+#include <click/error.hh>
+#include <click/confparse.hh>
+#include <click/glue.hh>
+#include "delayunqueue.hh"
+#include "elements/standard/scheduleinfo.hh"
+
+DelayUnqueue::DelayUnqueue()
+  : Element(1,1), _task(this)
+{
+  MOD_INC_USE_COUNT;
+}
+
+DelayUnqueue::~DelayUnqueue()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+DelayUnqueue::configure(const Vector<String> &conf, ErrorHandler *errh)
+{
+  return cp_va_parse(conf, this, errh,
+		     cpUnsigned, "delay (ms)", &_delay, 0);
+}
+
+int
+DelayUnqueue::initialize(ErrorHandler *errh)
+{
+  _p = 0;
+  ScheduleInfo::join_scheduler(this, &_task, errh);
+  return 0;
+}
+
+void
+DelayUnqueue::uninitialize()
+{
+  _p->kill();
+  _task.unschedule();
+}
+
+static inline unsigned
+elapsed_ms(struct timeval tv)
+{
+  struct timeval t;
+  click_gettimeofday(&t);
+  return (t.tv_sec*1000+t.tv_usec/1000)-(tv.tv_sec*1000+tv.tv_usec/1000);
+}
+
+void
+DelayUnqueue::run_scheduled()
+{
+  do {
+    if (!_p) 
+      _p = input(0).pull();
+    if (_p && elapsed_ms(_p->timestamp_anno()) >= _delay) {
+#if 0
+      {
+	struct timeval tv = _p->timestamp_anno();
+	struct timeval t; click_gettimeofday(&t);
+	unsigned z = 
+	  (t.tv_sec*1000+t.tv_usec/1000)-(tv.tv_sec*1000+tv.tv_usec/1000);
+        click_chatter("delay: %d\n", z);
+      }
+#endif
+      output(0).push(_p);
+      _p = 0;
+    }
+  } while(!_p);
+
+  _task.fast_reschedule();
+}
+
+String
+DelayUnqueue::read_param(Element *e, void *)
+{
+  DelayUnqueue *u = (DelayUnqueue *)e;
+  return String(u->_delay) + " ms\n";
+}
+
+void
+DelayUnqueue::add_handlers()
+{
+  add_read_handler("delay", read_param, (void *)0);
+  add_task_handlers(&_task);
+}
+
+EXPORT_ELEMENT(DelayUnqueue)
+ELEMENT_MT_SAFE(DelayUnqueue)
+
---
+++ elements/local/delayunqueue.hh
@@ -0,0 +1,41 @@
+#ifndef DELAYUNQUEUE_HH
+#define DELAYUNQUEUE_HH
+#include <click/element.hh>
+#include <click/task.hh>
+
+/*
+ * =c
+ * DelayUnqueue(DELAY)
+ * =s packet scheduling
+ * pull-to-push converter
+ * =d
+ * Pulls packets from input port. queues packet if current timestamp minus
+ * packet timestamp is less than DELAY ms. otherwise push packet on output.
+ * =a Queue, Unqueue, RatedUnqueue, BandwidthRatedUnqueue
+ */
+
+class DelayUnqueue : public Element { public:
+  
+  DelayUnqueue();
+  ~DelayUnqueue();
+
+  const char *class_name() const	{ return "DelayUnqueue"; }
+  const char *processing() const	{ return PULL_TO_PUSH; }
+  DelayUnqueue *clone() const		{ return new DelayUnqueue; }
+
+  int configure(const Vector<String> &, ErrorHandler *);
+  int initialize(ErrorHandler *);
+  void uninitialize();
+  void add_handlers();
+  
+  void run_scheduled();
+  static String read_param(Element *e, void *);
+
+ private:
+
+  unsigned _delay;
+  Packet *_p;
+  Task _task;
+};
+
+#endif
