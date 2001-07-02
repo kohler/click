d5751be21d938897178ade08a86d31ada6197e49
yipal <yipal>
2001-07-02 20:39:18 +0000

    Changed from sampleelement to ron

--- etc/ron/config.h
+++ etc/ron/config.h
@@ -1,6 +1,8 @@
-#ifndef RON_CONFIG_H
-#define RON_CONFIG_H
+/* config.h.  Generated automatically by configure.  */
+/* Process this file with configure to produce config.h. -*- mode: c -*- */
+#ifndef SAMPLEPACKAGE_CONFIG_H
+#define SAMPLEPACKAGE_CONFIG_H
 
-/* `ron' doesn't need any extra configuration. */
+/* `samplepackage' doesn't need any extra configuration. */
 
-#endif /* RON_CONFIG_H */
+#endif /* SAMPLEPACKAGE_CONFIG_H */
