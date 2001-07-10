dbfafac875bd6ac863ad3f47f29baa2b3da3dc27
benjie <benjie>
2001-07-10 20:14:06 +0000

    simple tcp demultiplexer

---
+++ elements/local/tcpdemux.cc
@@ -0,0 +1,119 @@
+/*
+ * tcpdemux.{cc,hh} -- demultiplexes tcp flows
+ * Benjie Chen
+ *
+ * Copyright (c) 2001 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/click_ip.h>
+#include <click/click_tcp.h>
+#include <click/elemfilter.hh>
+#include <click/router.hh>
+#include <click/error.hh>
+#include "tcpdemux.hh"
+
+TCPDemux::TCPDemux()
+  : Element(1, 3)
+{
+  MOD_INC_USE_COUNT;
+}
+
+TCPDemux::~TCPDemux()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+TCPDemux::configure(const Vector<String> &, ErrorHandler *)
+{
+  return 0;
+}
+
+
+int
+TCPDemux::initialize(ErrorHandler *)
+{
+  return 0;
+}
+
+void
+TCPDemux::uninitialize()
+{
+}
+
+int
+TCPDemux::find_flow(Packet *p)
+{
+  const click_ip *iph = p->ip_header();
+  const click_tcp *tcph = 
+    reinterpret_cast<const click_tcp *>(p->transport_header());
+
+  // match (sa,sp,da,dp)
+  IPFlowID fid(p);
+  int *iptr = _flows.findp(fid);
+  if (iptr)
+    return *iptr;
+
+  // match (0,0,da,dp)
+  fid = IPFlowID(0, 0, iph->ip_dst, tcph->th_dport);
+  iptr = _flows.findp(fid);
+  if (iptr)
+    return *iptr;
+
+  // match (0,0,0,dp)
+  fid = IPFlowID(0, 0, 0, tcph->th_dport);
+  iptr = _flows.findp(fid);
+  if (iptr)
+    return *iptr;
+
+  // match (0,0,da,0)
+  fid = IPFlowID(0, 0, iph->ip_dst, 0);
+  iptr = _flows.findp(fid);
+  if (iptr)
+    return *iptr;
+
+  // match (0,0,0,0)
+  fid = IPFlowID(0, 0, 0, 0);
+  iptr = _flows.findp(fid);
+  if (iptr)
+    return *iptr;
+
+  return -1;
+}
+
+void
+TCPDemux::push(int, Packet *p)
+{
+  int port = find_flow(p);
+  if (port < 0 || port > noutputs()) {
+    click_chatter("reject packet from unknown flow");
+    p->kill();
+  }
+  else
+    output(port).push(p);
+}
+
+bool
+TCPDemux::add_flow(IPAddress sa, unsigned short sp, 
+                   IPAddress da, unsigned short dp, unsigned port)
+{
+  return _flows.insert(IPFlowID(sa, sp, da, dp), port);
+}
+
+#include <click/bighashmap.cc>
+template class BigHashMap<IPFlowID, int>;
+
+EXPORT_ELEMENT(TCPDemux)
+
---
+++ elements/local/tcpdemux.hh
@@ -0,0 +1,52 @@
+
+#ifndef TCPDEMUX_HH
+#define TCPDEMUX_HH
+#include <click/element.hh>
+#include <click/timer.hh>
+#include <click/click_tcp.h>
+#include <click/bighashmap.hh>
+#include <click/ipflowid.hh>
+
+/*
+ * =c
+ * TCPDemux()
+ * =s TCP
+ * demultiplexes TCP flows
+ * =d
+ *
+ * for now, 
+ *
+ * output 0 = SYN
+ * output 1 = FIN | RST
+ * output 2 = others
+ */
+
+typedef BigHashMap<IPFlowID, int> FlowTable;
+
+class TCPDemux : public Element {
+private:
+  FlowTable _flows;
+  int find_flow(Packet *p);
+
+public:
+  TCPDemux();
+  ~TCPDemux();
+  
+  const char *class_name() const		{ return "TCPDemux"; }
+  const char *processing() const		{ return PUSH; }
+  TCPDemux *clone() const			{ return new TCPDemux; }
+
+  int initialize(ErrorHandler *);
+  void uninitialize();
+  int configure(const Vector<String> &conf, ErrorHandler *errh);
+
+  void push(int, Packet *);
+
+  // add new flow to flow table; returns false if key already exists, true
+  // otherwise. the (key, value) pair is added to table in either case
+  bool add_flow(IPAddress sa, unsigned short sp, 
+                IPAddress da, unsigned short dp, unsigned port);
+};
+
+#endif
+
