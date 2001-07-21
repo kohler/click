bb5541e2211d5eba732d619e0d3665e5597d4e1f
eddietwo <eddietwo>
2001-07-21 02:45:08 +0000

    Andrew Gorelik added an "active" handler/ACTIVE keyword argument (Mazu); also changed name of "packets" handler to "count"

--- elements/standard/unqueue.hh
+++ elements/standard/unqueue.hh
@@ -4,18 +4,42 @@
 #include <click/task.hh>
 
 /*
- * =c
- * Unqueue([BURSTSIZE])
- * =s packet scheduling
- * pull-to-push converter
- * =d
- * Pulls packets whenever they are available, then pushes them out
- * its single output. Pulls a maximum of BURSTSIZE packets every time
- * it is scheduled. Default BURSTSIZE is 1. If BURSTSIZE
- * is 0, pull until nothing comes back.
- *
- * =a RatedUnqueue, BandwidthRatedUnqueue
- */
+=c
+
+Unqueue([BURSTSIZE] [, I<KEYWORDS>])
+
+=s packet scheduling
+
+pull-to-push converter
+
+=d
+
+Pulls packets whenever they are available, then pushes them out
+its single output. Pulls a maximum of BURSTSIZE packets every time
+it is scheduled. Default BURSTSIZE is 1. If BURSTSIZE
+is 0, pull until nothing comes back.
+
+Keyword arguments are:
+
+=over 4
+
+=item ACTIVE
+
+If false, does nothing (doesn't pull packets). One possible use
+is to set ACTIVE to false in the configuration, and later
+change it to true with a handler from DriverManager element.
+The default value is true.
+
+=back
+
+=h count read-only
+Returns the count of packets that have passed through Unqueue.
+
+=h active write-only
+The same as ACTIVE keyword.
+
+=a RatedUnqueue, BandwidthRatedUnqueue
+*/
 
 class Unqueue : public Element { public:
   
@@ -34,11 +58,13 @@ class Unqueue : public Element { public:
   void run_scheduled();
 
   static String read_param(Element *e, void *);
+  static int write_param(const String &, Element *, void *, ErrorHandler *);
 
  private:
-
+  
+  bool _active;
   int _burst;
-  unsigned _packets;
+  unsigned _count;
   Task _task;
 
 };
