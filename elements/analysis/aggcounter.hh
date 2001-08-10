78040e3c4a218931211e47c3d1aab138287b7c10
kohler <kohler>
2001-08-10 00:47:42 +0000

    element doc

--- elements/analysis/aggcounter.hh
+++ elements/analysis/aggcounter.hh
@@ -5,9 +5,81 @@
 /*
 =c
 
-AggregateCounter
+AggregateCounter([I<KEYWORDS>])
 
-tcpdpriv(1) */
+=s statistics
+
+maintains information about aggregate annotations
+
+=d
+
+AggregateCounter maintains per-aggregate counts of how many packets or bytes
+it has seen. Each aggregate annotation value it sees gets a different value.
+Call its C<write_file> or C<write_ascii_file> write handler to get a dump of
+the information.
+
+Keyword arguments are:
+
+=over 8
+
+=item BYTES
+
+Boolean. If true, then count bytes seen, not packets seen. Default is false.
+
+=item MULTIPACKET
+
+Boolean. If true, and BYTES is false, then use packets' packet count
+annotations to add to the number of packets seen. Elements like
+FromIPSummaryDump set this annotation. Default is true.
+
+=item EXTRA_LENGTH
+
+Boolean. If true, and BYTES is true, then use packets' extra length
+annotations to add to the length counted. Elements like FromDump set this
+annotation. Default is true.
+
+=back
+
+=h write_file write-only
+
+Argument is a filename, or `C<->', meaning standard out. Write a packed binary
+file containing all current data to the specified filename. The format is a
+couple ASCII lines, followed by a line containing `C<$packed>', followed by N
+8-byte records. In each record, bytes 1-4 are the aggregate, and bytes 5-8 are
+the count. Both values are 32-bit integers in host byte order.
+
+=h write_ascii_file write-only
+
+Argument is a filename, or `C<->', meaning standard out. Write an ASCII file
+containing all current data to the specified filename. The format is a couple
+ASCII lines, followed by N data lines, each containing the aggregate ID in
+decimal, a space, then the count in decimal.
+
+=n
+
+The aggregate identifier is stored in host byte order. Thus, the aggregate ID
+corresponding to IP address 128.0.0.0 equals 2147483648.
+
+Only available in user-level processes.
+
+=e
+
+This configuration reads an IP summary dump in from standard input, aggregates
+based on destination IP address, and counts packets. When the dump is done,
+Click will write the aggregate counter's data to standard output, in ASCII
+form.
+
+  FromIPSummaryDump(-, STOP true)
+	-> AggregateIP(ip dst)
+	-> ac :: AggregateCounter
+	-> Discard;
+
+  DriverManager(wait_pause,
+	write ac.write_ascii_file -);
+
+=a
+
+AggregateIP, FromIPSummaryDump, FromDump, tcpdpriv(1) */
 
 class AggregateCounter : public Element { public:
   
