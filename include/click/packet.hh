eb5a53add8e6d8413349db33817f08ff081827dc
kolya <kolya>
2001-10-04 21:20:10 +0000

    Deal with chained mbuf's, even if not very efficiently; from Luigi.

--- include/click/packet.hh
+++ include/click/packet.hh
@@ -368,6 +368,14 @@ Packet::make(struct mbuf *m)
     click_chatter("Yow, constructing Packet from an mbuf chain!");
 
   Packet *p = new Packet;
+  if (m->m_pkthdr.len != m->m_len) {
+printf("-- m_pkthdr.len %d m->m_len %d\n",
+	m->m_pkthdr.len, m->m_len);
+	/* click needs contiguous data */
+	struct mbuf *m1 = m_dup(m, 0);
+	m_freem(m);
+	m = m1;
+  }
   p->_m = m;
   assimilate_mbuf(p);
 
