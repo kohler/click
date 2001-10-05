d83de30cf1b1adeda6c462efec593b502c682b13
decouto <decouto>
2001-10-05 13:34:38 +0000

    implementation finished & compiles, but.. it is UNTESTEDls!

--- apps/csclient/csclient.cc
+++ apps/csclient/csclient.cc
@@ -12,20 +12,26 @@
 #include <errno.h>
 #include <assert.h>
 #include <stdio.h>
+#include <ctype.h>
+
+#include <algorithm>
 
 #include "csclient.hh"
 
-int
-ControlSocketClient::configure(unsigned int ip, unsigned short port)
+#define check_init() do { if (_init) return init_err; } while (false);
+
+ControlSocketClient::err_t
+ControlSocketClient::configure(unsigned int host_ip, unsigned short port)
 {
-  assert(!_init);
+  if (_init)
+    return reinit_err;
   
-  _host = ip;
+  _host = host_ip;
   _port = port;
   
   _fd = socket(PF_INET, SOCK_STREAM, 0);
   if (_fd < 0) 
-    return -1;
+    return sys_err;
   
   /* 
    * connect to remote ControlSocket
@@ -38,16 +44,20 @@ ControlSocketClient::configure(unsigned int ip, unsigned short port)
   
   char namebuf[32];
   snprintf(namebuf, 32, "%u.%u.%u.%u:%hu", 
-	   (ip & 0xff) >> 0,
-	   (ip & 0xff00) >> 8,
-	   (ip & 0xff0000) >> 16,
-	   (ip & 0xff000000) >> 24,
+	   (_host & 0xff) >> 0,
+	   (_host & 0xff00) >> 8,
+	   (_host & 0xff0000) >> 16,
+	   (_host & 0xff000000) >> 24,
 	   port);
   _name = namebuf;
   
   int res = connect(_fd, (struct sockaddr *)  &sa, sizeof(sa));
-  if (res < 0) 
-    return -1;
+  if (res < 0) {
+    int save_errno = errno;
+    ::close(_fd);
+    errno = save_errno;
+    return sys_err;
+  }
   
   int major, minor;
   unsigned int slash, dot;
@@ -56,14 +66,20 @@ ControlSocketClient::configure(unsigned int ip, unsigned short port)
    * check that we get the expected banner
    */
   string buf;
-  res = readline(buf);
-  if (res < 0)
-    goto err;
+  err_t err = readline(buf);
+  if (err != no_err) {
+    int save_errno = errno;
+    ::close(_fd);
+    errno = save_errno;
+    return err;
+  }
   
-  slash = buf.find_first_of('/');
-  dot = (slash != string::npos ? buf.find_first_of('.', slash + 1) : string::npos);
-  if (slash == string::npos || dot == string::npos)
-    goto err; /* bad format */
+  slash = buf.find('/');
+  dot = (slash != string::npos ? buf.find('.', slash + 1) : string::npos);
+  if (slash == string::npos || dot == string::npos) {
+    ::close(_fd);
+    return click_err; /* bad format */
+  }
   
   /*
    * check ControlSocket protocol version
@@ -71,22 +87,35 @@ ControlSocketClient::configure(unsigned int ip, unsigned short port)
   major = atoi(buf.substr(slash + 1, dot - slash - 1).c_str());
   minor = atoi(buf.substr(dot + 1, buf.size() - dot - 1).c_str());
   if (major != PROTOCOL_MAJOR_VERSION ||
-      minor < PROTOCOL_MINOR_VERSION)
-    goto err; /* wrong version */
-  
-  _init = true;
-  return 0;
+      minor < PROTOCOL_MINOR_VERSION) {
+    ::close(_fd); 
+    return click_err; /* wrong version */
+  }
 
- err:
-  close(_fd);
-  return -1;
+  _init = true;
+  return no_err;
 }
 
 
-int
+ControlSocketClient::err_t
+ControlSocketClient::close()
+{
+  check_init();
+  _init = false;
+  int res = ::close(_fd);
+  if (res < 1)
+    return sys_err;
+  else
+    return no_err;
+}
+
+
+ControlSocketClient::err_t
 ControlSocketClient::readline(string &buf)
 {
   assert(_fd);
+
+#define MAX_LINE_SZ 1024 /* arbitrary... to prevent weirdness */
 
   /* 
    * keep calling read() to get one character at a time, until we get
@@ -97,13 +126,16 @@ ControlSocketClient::readline(string &buf)
   do {
     int res = ::read(_fd, (void *) &c, 1);
     if (res < 0)
-      return -1;
-    assert(res == 1);
+      return sys_err;
+    if (res != 1)
+      return sys_err;
     buf += c;
+    if (buf.size() > MAX_LINE_SZ)
+      return click_err;
   } 
   while (c != '\n');
 
-  return 0;
+  return no_err;
 }
 
 
@@ -128,49 +160,47 @@ ControlSocketClient::get_data_len(string line)
 }
 
 
-int
-ControlSocketClient::read(string name, string handler, string &response)
+ControlSocketClient::err_t
+ControlSocketClient::read(string el, string handler, string &response)
 {
-  assert(_init);
+  check_init();
   
-  if (name.size() > 0)
-    handler = name + "." + handler;
+  if (el.size() > 0)
+    handler = el + "." + handler;
   string cmd = "READ " + handler + "\n";
   
-  /*
-   * write command string)
-   */
-  int res = write(_fd, cmd.c_str(), cmd.size());
+  int res = ::write(_fd, cmd.c_str(), cmd.size());
   if (res < 0)
-    return -1;
-  assert(res == (int) cmd.size());
+    return sys_err;
+  if ((size_t) res != cmd.size())
+    return sys_err;
 
   string cmd_resp;
   string line;
   do {
-    res = readline(line);
-    if (res < 0)
-      return -1;
+    err_t err = readline(line);
+    if (err != no_err)
+      return err;
     if (line.size() < 4)
-      return -1;
+      return click_err;
     cmd_resp += line;
   }
   while (line[3] == '-');
     
   int code = get_resp_code(line);
   if (code != CODE_OK && code != CODE_OK_WARN) 
-    return -code;
+    return handle_err_code(code);
   
   res = readline(line);
   if (res < 0)
-    return -1;
+    return click_err;
   int num = get_data_len(line);
   if (num < 0)
-    return -1;
+    return click_err;
 
   response.resize(0);
   if (num == 0)
-    return 0;
+    return no_err;
   
   char *buf = new char[num];
   int num_read = 0;
@@ -178,7 +208,7 @@ ControlSocketClient::read(string name, string handler, string &response)
     res = ::read(_fd, buf + num_read, num - num_read);
     if (res < 0) {
       delete[] buf;
-      return -1;
+      return sys_err;
     }
     num_read += res;
   }
@@ -186,5 +216,278 @@ ControlSocketClient::read(string name, string handler, string &response)
   response.append(buf, num);
   delete[] buf;
 
-  return 0;
+  return no_err;
+}
+
+
+ControlSocketClient::err_t
+ControlSocketClient::read(string el, string handler, char *buf, int &bufsz)
+{
+  string resp;
+  err_t err = read(el, handler, resp);
+  if (err != no_err)
+    return err;
+
+  bufsz = min((size_t) bufsz, resp.size());
+
+  memcpy(buf, resp.c_str(), bufsz);
+  if (resp.size() > (size_t) bufsz)
+    return too_short;
+  else
+    return no_err;
+}
+
+
+ControlSocketClient::err_t
+ControlSocketClient::write(string el, string handler, const char *buf, int bufsz)
+{
+  check_init();
+  
+  if (el.size() > 0)
+    handler = el + "." + handler;
+  char cbuf[10];
+  snprintf(cbuf, sizeof(cbuf), "%d", bufsz);
+  string cmd = "WRITE " + handler + " " + buf + "\n";
+  
+  int res = ::write(_fd, cmd.c_str(), cmd.size());
+  if (res < 0)
+    return sys_err;
+  if ((size_t) res != cmd.size())
+    return sys_err;
+  
+  res = ::write(_fd, buf, bufsz);
+  if (res < 0)
+    return sys_err;
+  if (res != bufsz)
+    return sys_err;
+
+  string cmd_resp;
+  string line;
+  do {
+    err_t err = readline(line);
+    if (err != no_err)
+      return err;
+    if (line.size() < 4)
+      return click_err;
+    cmd_resp += line;
+  }
+  while (line[3] == '-');
+
+  int code = get_resp_code(line);
+  if (code != CODE_OK && code != CODE_OK_WARN) 
+    return handle_err_code(code);
+  
+  return no_err;
+}
+
+
+ControlSocketClient::err_t
+ControlSocketClient::write(string el, string handler, string data)
+{
+  return write(el, handler, data.c_str(), data.size());
+}
+
+
+ControlSocketClient::err_t
+ControlSocketClient::handle_err_code(int code)
+{
+  switch (code) {
+  case CODE_SYNTAX_ERR: return click_err; break;
+  case CODE_UNIMPLEMENTED: return click_err; break;
+  case CODE_NO_ELEMENT: return no_element; break;
+  case CODE_NO_HANDLER: return no_handler; break;
+  case CODE_HANDLER_ERR: return handler_err; break;
+  case CODE_PERMISSION: return handler_no_perm; break;
+  default: return click_err; break;
+  }
+  return click_err;
+}
+
+
+
+vector<string> 
+ControlSocketClient::split(string s, size_t offset, char terminator)
+{
+  vector<string> v;
+  size_t pos = offset;
+  size_t len = s.size();
+  while (pos < len) {
+    size_t start = pos;
+    while (pos < len && s[pos] != terminator)
+      pos++;
+    if (start < pos || pos < len)
+      v.push_back(s.substr(start, pos - len));
+    pos++;
+  }
+  return v;
+}
+
+
+ControlSocketClient::err_t 
+ControlSocketClient::get_config_el_names(vector<string> &els)
+{
+  string resp;
+  err_t err = read("", "list", resp);
+  if (err != no_err)
+    return err;
+  
+  /* parse how many els */
+  int i = resp.find('\n');
+  int num = atoi(resp.substr(0, i).c_str());
+  
+  
+  els = split(resp, i + 1, '\n');
+  if (els.size() != (size_t) num)
+    return handler_bad_format;
+
+  return no_err;  
+}
+
+
+ControlSocketClient::err_t
+ControlSocketClient::get_string_vec(string el, string h, vector<string> &v)
+{
+  string resp;
+  err_t err = read(el, h, resp);
+  if (err != no_err)
+    return err;
+
+  v = split(resp, 0, '\n');
+  return no_err;
+}
+
+
+ControlSocketClient::err_t 
+ControlSocketClient::get_el_handlers(string el, vector<handler_info_t> &handlers)
+{
+  vector<handler_info_t> v;
+  vector<string> vh;
+
+  string buf;
+  err_t err = read(el, "handlers", buf);
+  if (err != no_err)
+    return err;
+
+  vh = split(buf, 0, '\n');
+  for (vector<string>::iterator i = vh.begin(); i != vh.end(); i++) {
+    string &s = *i;
+    size_t j;
+    for (j = 0; j < s.size() && !isspace(s[j]); j++)
+      ; /* find record split -- don't use s.find because could be any whitespace */
+    if (j == s.size())
+      return click_err;
+    handler_info_t hi;
+    hi.element_name = el;
+    hi.handler_name = trim(s.substr(0, j));
+    while (j < s.size() && isspace(s[j]))
+      j++;
+    for ( ; j < s.size(); j++) {
+      if (tolower(s[j] == 'r'))
+	hi.can_read = true;
+      else if (tolower(s[j] == 'w'))
+	hi.can_write = true;
+      else if (isspace(s[j]))
+	break;
+    }
+    v.push_back(hi);
+  }
+
+  handlers = v;
+  return no_err;
+}
+
+
+ControlSocketClient::err_t 
+ControlSocketClient::check_handler(string el, string h, bool is_write, bool &exists)
+{
+  check_init();
+  
+  if (el.size() > 0)
+    h = el + "." + h;
+  string cmd = (is_write ? "CHECKWRITE " : "CHECKREAD ") + h + "\n";
+  
+  int res = ::write(_fd, cmd.c_str(), cmd.size());
+  if (res < 0)
+    return sys_err;
+  if ((size_t) res != cmd.size())
+    return sys_err;
+
+  string cmd_resp;
+  string line;
+  do {
+    err_t err = readline(line);
+    if (err != no_err)
+      return err;
+    if (line.size() < 4)
+      return click_err;
+    cmd_resp += line;
+  }
+  while (line[3] == '-');
+    
+  int code = get_resp_code(line);
+  switch (code) {
+  case CODE_OK:
+  case CODE_OK_WARN:
+    exists = true;
+    return no_err;;
+  case CODE_NO_ELEMENT:
+  case CODE_NO_HANDLER:
+  case CODE_HANDLER_ERR:
+  case CODE_PERMISSION:
+    exists = false;
+    return no_err;
+  case CODE_UNIMPLEMENTED:
+    if (el.size() == 0)
+      return handle_err_code(code); /* no workaround for top-level router handlers */
+    else
+      return check_handler_workaround(el, h, is_write, exists);
+  default:
+    return handle_err_code(code);
+  }
+}
+
+
+
+ControlSocketClient::err_t 
+ControlSocketClient::check_handler_workaround(string el, string h, bool is_write, bool &exists)
+{
+  /* 
+   * If talking to an old ControlSocket, try the "handlers" handler
+   * instead. 
+   */
+
+  vector<handler_info_t> v;
+  err_t err = get_el_handlers(el, v);
+  if (err != no_err)
+    return err;
+  
+  for (vector<handler_info_t>::iterator i = v.begin(); i != v.end(); i++) {
+    if (i->handler_name == h) {
+      if (is_write && i->can_write ||
+	  !is_write && i->can_read)
+	exists = true;
+      else
+	exists = false;
+      return no_err;
+    }
+  }
+
+  exists = false;
+  return no_err;
+}
+
+
+string
+ControlSocketClient::trim(string s)
+{
+  size_t start, end;
+  for (start = 0; start < s.size() && isspace(s[start]); start++)
+    ; /* */
+  for (end = s.size(); end > 0 && isspace(s[end - 1]); end--)
+    ; /* */
+
+  if (start >= end)
+    return "";
+  
+  return s.substr(start, end - start);
 }
--- apps/csclient/csclient.hh
+++ apps/csclient/csclient.hh
@@ -5,12 +5,196 @@
  */
 
 #include <string>
+#include <vector>
+
 #include <unistd.h>
+
+/*
+ * NB: obscure implementation note: this class does not handle EINTR
+ * errors from any of the read/write calls.  If this is relevant to
+ * your program, this class may not do the ``right thing'' 
+ */
 
 class ControlSocketClient
 {
+public:
+  ControlSocketClient() : _init(false), _fd(0) { }
+  ControlSocketClient(ControlSocketClient &) : _init(false), _fd(0) { }
+
+  enum err_t {
+    no_err = 0,
+    sys_err,            /* O/S or networking error, check errno for more information */
+    init_err,           /* tried to perform operation on an unconfigured ControlSocketClient */
+    reinit_err,         /* tried to re-configure the client before close()ing it */
+    no_element,         /* specified element does not exist */
+    no_handler,         /* specified handler does not exist */
+    handler_no_perm,    /* router denied access to the specified handler */
+    handler_err,        /* handler returned an error */
+    handler_bad_format, /* bad format in calling handler */
+    click_err,          /* unexpected response or error from the router */
+    too_short           /* user buffer was too short */
+  };
+
+  /* 
+   * Configure a new ControlSocketClient.
+   * HOST_IP is IP address (in network byte order) of the machine that user-level click is running on 
+   * PORT is the IP port the ControlSocket is listening on. 
+   * Returns: no_err, sys_err, reinit_err, click_err
+   * If returns no_err, the client is properly configured; otherwise the client is unconfigured.
+   */
+  err_t configure(unsigned int host_ip, unsigned short port);
+
+  /*
+   * Close a configured client.
+   * Returns: no_err, sys_err, init_err
+   * In any case, the client is left unconfigured.
+   */
+  err_t close();
+
+  /* 
+   * Return a string describing the ControlSocket's host and port.
+   * Requires: object is configured 
+   */
+  const string name() { return _name; }
+
+
+  /*
+   * NB: the following functions return data about or send data to the
+   * click router via the ControlSocket.  Unless otherwise noted, the
+   * data returned or sent is undefined unless the function's return
+   * value is no_err.
+   */
+
+  /*
+   * Get a string containing the router's configuration
+   * (get_router_config) or flattened configuration
+   * (get_router_flat_config). 
+   * CONFIG is filled with the configuration; existing contents are replaced.
+   * Returns: no_err, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_router_config(string &config)         { return read("", "config", config); }
+  err_t get_router_flat_config(string &config)    { return read("", "flatconfig", config); }
+  
+  /* 
+   * Get a string containing the router's version
+   * VERS is filled with the version; existing contents are replaced.
+   * Returns: no_err, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_router_version(string &vers)         { return read("", "version", vers); }
+
+  /*
+   * Get the names of the elements in the the current router configuration.
+   * ELS is filled with the names, existing contents are replaced.
+   * Returns: no_err, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_config_el_names(vector<string> &els);
+  
+  /*
+   * Get the names of the element types that the router knows about.
+   * CLASSES is filled with the names, existing contents are replaced.
+   * Returns: no_err, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_router_classes(vector<string> &classes)   { return get_string_vec("", "classes", classes); }
+  
+  /*
+   * Get the names of the packages that the router knows about.
+   * PACKAGES is filled with the names, existing contents are replaced.
+   * Returns: no_err, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_router_packages(vector<string> &pkgs)  { return get_string_vec("", "packages", pkgs); }
+
+  /*
+   * Get the names of the current router configuration requirements.
+   * REQS is filled with the names, existing contents are replaced.
+   * Returns: no_err, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_config_reqs(vector<string> &reqs)         { return get_string_vec("", "requirements", reqs); }
+  
+  struct handler_info_t {
+    string element_name; 
+    string handler_name;
+    bool can_read;
+    bool can_write;
+  };
+
+  /*
+   * Get the information about an element's handlers in the current router configuration.
+   * EL is the element's name.
+   * HANDLERS is filled with the handler info, existing contents are replaced.
+   * Returns: no_err, no_element, handler_err, handler_no_perm, sys_err, init_err, click_err 
+   */
+  err_t get_el_handlers(string el, vector<handler_info_t> &handlers);
+
+  /*
+   * Check whether a read/write handler exists.
+   * EL is the element's name.
+   * H is the handler's name.
+   * IS_WRITE true to check for write handler, otherwise check read handler.
+   * EXISTS is filled with true if the handler exists, otherwise false.
+   * Returns: no_err, sys_err, init_err, click_err 
+   */
+  err_t check_handler(string el, string h, bool is_write, bool &exists);
+protected:
+  err_t check_handler_workaround(string el, string h, bool is_write, bool &exists);
+
+public:
+  /* 
+   * Return the results of reading a handler.
+   * EL is the element's name.
+   * HANDLER is the handler name.
+   * RESPONSE is filled with the handler's output, existing contents are replaced.
+   * If NAME is not empty, calls``NAME.HANDLER''; otherwise calls ``HANDLER''
+   * Returns: no_err, no_element, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err
+   */
+  err_t read(string el, string handler, string &response);
+
+  /* 
+   * Return the results of reading a handler.
+   * EL is the element's name.
+   * HANDLER is the handler name.
+   * BUF receives the data.
+   * BUFSZ is the size of BUF, and recieves the actual number of characters placed into BUF.
+   * If NAME is not empty, calls``NAME.HANDLER''; otherwise calls ``HANDLER''
+   * Returns: no_err, no_element, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err, too_short
+   * If returns too_short, the operation succeeded, but returned more than BUFSZ characters; the first BUFSZ
+   * characters of the result are placed into BUF, and BUFSZ is unchanged.
+   */
+  err_t read(string el, string handler, char *buf, int &bufsz);
+
+  /* 
+   * Write data to an element's handler.
+   * EL is the element's name.
+   * HANDLER is the handler name.
+   * DATA contains the data.  No terminating '\0' is written.
+   * If NAME is not empty, calls``NAME.HANDLER''; otherwise calls ``HANDLER''
+   * Returns: no_err, no_element, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err
+   */
+  err_t write(string el, string handler, string data);
+
+  /* 
+   * Write data to an element's handler.
+   * EL is the element's name.
+   * HANDLER is the handler name.
+   * BUF contains the data.  
+   * BUFSZ is the number of characters to be written from BUF.
+   * If NAME is not empty, calls``NAME.HANDLER''; otherwise calls ``HANDLER''
+   * Returns: no_err, no_element, no_handler, handler_err, handler_no_perm, sys_err, init_err, click_err
+   */
+  err_t write(string el, string handler, const char *buf, int bufsz);
+
+  /* 
+   * sugar, for reading and writing handlers.
+   */
+  err_t read(handler_info_t h, string &response)      { return read(h.element_name, h.handler_name, response); }
+  err_t read(handler_info_t h, char *buf, int &bufsz) { return read(h.element_name, h.handler_name, buf, bufsz); }
+  err_t write(handler_info_t h, string data)          { return write(h.element_name, h.handler_name, data); }
+  err_t write(handler_info_t h, const char *buf, int bufsz) { return write(h.element_name, h.handler_name, buf, bufsz); }
+
+
+  ~ControlSocketClient() { if (_init) ::close(_fd); }
+
 private:
-  int _init;
+  bool _init;
   
   unsigned int _host;
   unsigned short _port;
@@ -32,29 +216,15 @@ private:
   static const int PROTOCOL_MAJOR_VERSION = 1;
   static const int PROTOCOL_MINOR_VERSION = 0;
   
-  static const int _sock_timeout = 0; // msecs
-
   /* Try to read a '\n'-terminated line (including the '\n') from the
-   * socket.  Returns -1 if there was an error. */
-  int readline(string &buf);
+   * socket.  */
+  err_t readline(string &buf);
 
   int get_resp_code(string line);
   int get_data_len(string line);
+  err_t handle_err_code(int code);
 
-public:
-  ControlSocketClient() : _init(false), _fd(0) { }
-  
-  /* HOST is IP address in network byte order.  Returns 0 iff okay,
-   * else -1 */
-  int configure(unsigned int host, unsigned short port);
-
-  const string name() { return _name; }
-  
-  /* Read handler.  Returns the the results of calling handler
-   * ``NAME.HANDLER''; if NAME is empty, return the result of just
-   * calling ``HANDLER'' (i.e. don't put the `.' in). */
-  int read(string name, string handler, string &response);
-
-  ~ControlSocketClient() { if (_init) ::close(_fd); }
-
+  err_t get_string_vec(string el, string h, vector<string> &v);
+  vector<string> split(string s, size_t offset, char terminator);
+  string trim(string s);
 };
