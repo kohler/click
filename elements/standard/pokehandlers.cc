0206d6c5278f9e4bbd1c37c6821fb4cac481b2b5
eddietwo <eddietwo>
2001-10-10 10:32:57 +0000

    PokeHandlers supports "loop"

--- elements/standard/pokehandlers.cc
+++ elements/standard/pokehandlers.cc
@@ -1,3 +1,4 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
 /*
  * pokehandlers.{cc,hh} -- element runs write handlers
  * Eddie Kohler
@@ -22,120 +23,130 @@
 #include <click/router.hh>
 
 PokeHandlers::PokeHandlers()
-  : _timer(timer_hook, this)
+    : _timer(timer_hook, this)
 {
-  MOD_INC_USE_COUNT;
+    MOD_INC_USE_COUNT;
 }
 
 PokeHandlers::~PokeHandlers()
 {
-  MOD_DEC_USE_COUNT;
-  uninitialize();
+    MOD_DEC_USE_COUNT;
+    uninitialize();
+}
+
+void
+PokeHandlers::add(Element *e, const String &hname, const String &value, int timeout)
+{
+    _h_element.push_back(e);
+    _h_handler.push_back(hname);
+    _h_value.push_back(value);
+    _h_timeout.push_back(timeout);
 }
 
 int
 PokeHandlers::configure(const Vector<String> &conf, ErrorHandler *errh)
 {
-  _h_element.clear();
-  _h_handler.clear();
-  _h_value.clear();
-  _h_timeout.clear();
-  
-  int next_timeout = 0;
-  for (int i = 0; i < conf.size(); i++) {
-    if (!conf[i])
-      continue;
-    
-    String first, rest;
-    cp_word(conf[i], &first, &rest);
-    cp_eat_space(rest);
-    if (!rest) {
-      int gap;
-      if (first == "quit") {
-	_h_element.push_back(0);
-	_h_handler.push_back("");
-	_h_value.push_back("");
-	_h_timeout.push_back(next_timeout);
-	if (i < conf.size() - 1)
-	  errh->warning("arguments after `quit' directive ignored");
-	break;
-      } else if (cp_seconds_as_milli(first, reinterpret_cast<uint32_t *>(&gap))) {
-	next_timeout += gap;
-	continue;
-      }
+    _h_element.clear();
+    _h_handler.clear();
+    _h_value.clear();
+    _h_timeout.clear();
+
+    int next_timeout = 0;
+    for (int i = 0; i < conf.size(); i++) {
+	if (!conf[i])
+	    continue;
+
+	String first, rest;
+	cp_word(conf[i], &first, &rest);
+	cp_eat_space(rest);
+	if (!rest) {
+	    int gap;
+	    if (first == "quit") {
+		add(QUIT_MARKER, "", "", next_timeout);
+		if (i < conf.size() - 1)
+		    errh->warning("arguments after `quit' directive ignored");
+		break;
+	    } else if (first == "loop") {
+		add(LOOP_MARKER, "", "", next_timeout);
+		if (i < conf.size() - 1)
+		    errh->warning("arguments after `loop' directive ignored");
+		break;
+	    } else if (cp_seconds_as_milli(first, reinterpret_cast<uint32_t *>(&gap))) {
+		next_timeout += gap;
+		continue;
+	    }
+	}
+
+	Element *e;
+	String hname;
+	if (cp_handler(first, this, &e, &hname, errh)) {
+	    add(e, hname, rest, next_timeout);
+	    next_timeout = 0;
+	    continue;
+	}
+
+	errh->error("argument %d: expected `TIMEOUT' or `ELEMENT.HANDLER VALUE'", i + 1);
     }
 
-    Element *e;
-    String hname;
-    if (cp_handler(first, this, &e, &hname, errh)) {
-      _h_element.push_back(e);
-      _h_handler.push_back(hname);
-      _h_value.push_back(rest);
-      _h_timeout.push_back(next_timeout);
-      next_timeout = 0;
-      continue;
-    }
-    
-    errh->error("argument %d: expected `TIMEOUT' or `ELEMENT.HANDLER VALUE'",
-		i + 1);
-  }
-
-  return 0;
+    return 0;
 }
 
 int
 PokeHandlers::initialize(ErrorHandler *)
 {
-  _pos = 0;
-  _timer.initialize(this);
-  if (_h_timeout.size() != 0)
-    _timer.schedule_after_ms(_h_timeout[0] + 1);
-  return 0;
+    _pos = 0;
+    _timer.initialize(this);
+    if (_h_timeout.size() != 0)
+	_timer.schedule_after_ms(_h_timeout[0] + 1);
+    return 0;
 }
 
 void
 PokeHandlers::uninitialize()
 {
-  _timer.unschedule();
+    _timer.unschedule();
 }
 
 
 void
 PokeHandlers::timer_hook(Timer *, void *thunk)
 {
-  PokeHandlers *poke = (PokeHandlers *)thunk;
-  ErrorHandler *errh = ErrorHandler::default_handler();
-  Router *router = poke->router();
+    PokeHandlers *poke = (PokeHandlers *)thunk;
+    ErrorHandler *errh = ErrorHandler::default_handler();
+    Router *router = poke->router();
 
-  int h = poke->_pos;
-  do {
-    Element *he = poke->_h_element[h];
-    String hname = poke->_h_handler[h];
+    int h = poke->_pos;
+    do {
+	Element *he = poke->_h_element[h];
+	const String &hname = poke->_h_handler[h];
 
-    if (!hname) {		// `quit' directive
-      router->please_stop_driver();
-      h++;
-      break;
-    }
-    
-    int i = router->find_handler(he, hname);
-    if (i < 0)
-      errh->error("%s: no handler `%s'", poke->id().cc(), Router::Handler::unparse_name(he, hname).cc());
-    else {
-      const Router::Handler &rh = router->handler(i);
-      if (rh.writable()) {
-	ContextErrorHandler cerrh
-	  (errh, "In write handler `" + rh.unparse_name(he) + "':");
-	rh.call_write(poke->_h_value[h], he, &cerrh);
-      } else
-	errh->error("%s: no write handler `%s'", poke->id().cc(), Router::Handler::unparse_name(he, hname).cc());
-    }
-    h++;
-  } while (h < poke->_h_timeout.size() && poke->_h_timeout[h] == 0);
+	if (he == QUIT_MARKER) {
+	    router->please_stop_driver();
+	    h++;
+	    break;
+	} else if (he == LOOP_MARKER) {
+	    h = 0;
+	    break;
+	}
 
-  if (h < poke->_h_timeout.size())
-    poke->_timer.schedule_after_ms(poke->_h_timeout[h]);
-  poke->_pos = h;
+	int i = router->find_handler(he, hname);
+	if (i < 0)
+	    errh->error("%s: no handler `%s'", poke->id().cc(), Router::Handler::unparse_name(he, hname).cc());
+	else {
+	    const Router::Handler &rh = router->handler(i);
+	    if (rh.writable()) {
+		ContextErrorHandler cerrh
+		    (errh, "In write handler `" + rh.unparse_name(he) + "':");
+		rh.call_write(poke->_h_value[h], he, &cerrh);
+	    } else
+		errh->error("%s: no write handler `%s'", poke->id().cc(), Router::Handler::unparse_name(he, hname).cc());
+	}
+	h++;
+    } while (h < poke->_h_timeout.size() && poke->_h_timeout[h] == 0);
+
+    if (h < poke->_h_timeout.size())
+	poke->_timer.schedule_after_ms(poke->_h_timeout[h]);
+    poke->_pos = h;
 }
 
 EXPORT_ELEMENT(PokeHandlers)
--- elements/standard/pokehandlers.hh
+++ elements/standard/pokehandlers.hh
@@ -1,5 +1,6 @@
-#ifndef POKEHANDLERS_HH
-#define POKEHANDLERS_HH
+// -*- mode: c++; c-basic-offset: 4 -*-
+#ifndef CLICK_POKEHANDLERS_HH
+#define CLICK_POKEHANDLERS_HH
 #include <click/element.hh>
 #include <click/timer.hh>
 
@@ -15,9 +16,10 @@
  * Runs write handlers automatically at specified times. Each configuration
  * argument should be either `DELAY', a delay in seconds; `ELEMENT.HANDLERNAME
  * VALUE', a directive to write VALUE into ELEMENT's HANDLERNAME write
- * handler; or `quit', a directive to stop the driver. PokeHandlers processes
- * its arguments in order, writing to handlers as they appear. A `DELAY'
- * directive causes it to wait for DELAY seconds before continuing.
+ * handler; `quit'; or `loop'. PokeHandlers processes its arguments in order,
+ * writing to handlers as they appear. A `DELAY' directive causes it to wait
+ * for DELAY seconds before continuing. `quit' stops the driver, and `loop'
+ * restarts the program from the beginning.
  *
  * At user level, errors reported by write handlers are printed to standard
  * error. In the Linux kernel module, they are printed to /var/log/messages
@@ -28,27 +30,32 @@
  *                red.max_p 0.5);
  * =a PeekHandlers, dmesg(1) */
 
-class PokeHandlers : public Element {
+class PokeHandlers : public Element { public:
 
-  int _pos;
-  Vector<Element *> _h_element;
-  Vector<String> _h_handler;
-  Vector<String> _h_value;
-  Vector<int> _h_timeout;
-  Timer _timer;
+    PokeHandlers();
+    ~PokeHandlers();
 
-  static void timer_hook(Timer *, void *);
-  
- public:
+    const char *class_name() const		{ return "PokeHandlers"; }
+    PokeHandlers *clone() const			{ return new PokeHandlers; }
+    
+    int configure(const Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void uninitialize();
 
-  PokeHandlers();
-  ~PokeHandlers();
+  private:
 
-  const char *class_name() const		{ return "PokeHandlers"; }
-  PokeHandlers *clone() const			{ return new PokeHandlers; }
-  int configure(const Vector<String> &, ErrorHandler *);
-  int initialize(ErrorHandler *);
-  void uninitialize();
+    static Element * const QUIT_MARKER = (Element *)1;
+    static Element * const LOOP_MARKER = (Element *)2;
+
+    int _pos;
+    Vector<Element *> _h_element;
+    Vector<String> _h_handler;
+    Vector<String> _h_value;
+    Vector<int> _h_timeout;
+    Timer _timer;
+
+    static void timer_hook(Timer *, void *);
+    void add(Element *, const String &, const String &, int);
 
 };
 
