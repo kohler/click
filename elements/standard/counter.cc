24e6a127a4624cebe13b79f1e395fa7084fa4905
eddietwo <eddietwo>
2001-10-27 21:26:23 +0000

    Counter is 64-bit when the architecture supports it, and has new CALL_AFTER_COUNT and CALL_AFTER_BYTES keyword arguments (handler calls)

--- elements/standard/counter.cc
+++ elements/standard/counter.cc
@@ -17,11 +17,18 @@
 
 #include <click/config.h>
 #include "counter.hh"
-#include <click/confparse.hh>
 #include <click/error.hh>
+#include <click/confparse.hh>
+#include <click/handlercall.hh>
+
+#ifdef HAVE_INT64_TYPES
+static CpVaParseCmd parse_cmd = cpUnsigned64;
+#else
+static CpVaParseCmd parse_cmd = cpUnsigned;
+#endif
 
 Counter::Counter()
-  : Element(1, 1), _count(0)
+  : Element(1, 1), _count_trigger_h(0), _byte_trigger_h(0)
 {
   MOD_INC_USE_COUNT;
 }
@@ -29,6 +36,8 @@ Counter::Counter()
 Counter::~Counter()
 {
   MOD_DEC_USE_COUNT;
+  delete _count_trigger_h;
+  delete _byte_trigger_h;
 }
 
 void
@@ -36,11 +45,48 @@ Counter::reset()
 {
   _count = _byte_count = 0;
   _rate.initialize();
+  _count_triggered = _byte_triggered = false;
 }
 
 int
-Counter::initialize(ErrorHandler *)
+Counter::configure(const Vector<String> &conf, ErrorHandler *errh)
 {
+  String call_after_count, call_after_bytes;
+  if (cp_va_parse(conf, this, errh,
+		  cpKeywords,
+		  "CALL_AFTER_COUNT", cpArgument, "handler to call after a count", &call_after_count,
+		  "CALL_AFTER_BYTES", cpArgument, "handler to call after a byte count", &call_after_bytes,
+		  0) < 0)
+    return -1;
+
+  if (call_after_count) {
+    if (cp_va_space_parse(call_after_count, this, errh,
+			  parse_cmd, "count trigger", &_count_trigger,
+			  cpWriteHandlerCall, "handler to call", &_count_trigger_h,
+			  0) < 0)
+      return -1;
+  } else
+    _count_trigger = (counter_t)(-1);
+
+  if (call_after_bytes) {
+    if (cp_va_space_parse(call_after_bytes, this, errh,
+			  parse_cmd, "bytecount trigger", &_byte_trigger,
+			  cpWriteHandlerCall, "handler to call", &_byte_trigger_h,
+			  0) < 0)
+      return -1;
+  } else
+    _byte_trigger = (counter_t)(-1);
+
+  return 0;
+}
+
+int
+Counter::initialize(ErrorHandler *errh)
+{
+  if (_count_trigger_h && _count_trigger_h->initialize_write(this, errh) < 0)
+    return -1;
+  if (_byte_trigger_h && _byte_trigger_h->initialize_write(this, errh) < 0)
+    return -1;
   reset();
   return 0;
 }
@@ -51,40 +97,79 @@ Counter::simple_action(Packet *p)
   _count++;
   _byte_count += p->length();
   _rate.update(1);
+
+  if (_count == _count_trigger && !_count_triggered) {
+    _count_triggered = true;
+    if (_count_trigger_h)
+      (void) _count_trigger_h->call_write(this);
+  }
+  if (_byte_count >= _byte_trigger && !_byte_triggered) {
+    _byte_triggered = true;
+    if (_byte_trigger_h)
+      (void) _byte_trigger_h->call_write(this);
+  }
+  
   return p;
 }
+
+
+enum { H_COUNT, H_BYTE_COUNT, H_RATE, H_RESET,
+       H_CALL_AFTER_COUNT, H_CALL_AFTER_BYTES };
 
 String
 Counter::read_handler(Element *e, void *thunk)
 {
   Counter *c = (Counter *)e;
   switch ((int)thunk) {
-   case 0:
+   case H_COUNT:
     return String(c->_count) + "\n";
-   case 1:
+   case H_BYTE_COUNT:
     return String(c->_byte_count) + "\n";
-   case 2:
+   case H_RATE:
     return c->_rate.unparse() + "\n";
    default:
     return "<error>\n";
   }
 }
 
-static int
-counter_reset_write_handler(const String &, Element *e, void *, ErrorHandler *)
+int
+Counter::write_handler(const String &in_str, Element *e, void *thunk, ErrorHandler *errh)
 {
   Counter *c = (Counter *)e;
-  c->reset();
-  return 0;
+  switch ((int)thunk) {
+   case H_CALL_AFTER_COUNT:
+    if (cp_va_space_parse(in_str, c, errh,
+			  parse_cmd, "count trigger", &c->_count_trigger,
+			  cpOptional,
+			  cpWriteHandlerCall, "handler to call", &c->_count_trigger_h, 0) < 0)
+      return -1;
+    c->_count_triggered = false;
+    return 0;
+   case H_CALL_AFTER_BYTES:
+    if (cp_va_space_parse(in_str, c, errh,
+			  parse_cmd, "bytecount trigger", &c->_byte_trigger,
+			  cpOptional,
+			  cpWriteHandlerCall, "handler to call", &c->_byte_trigger_h, 0) < 0)
+      return -1;
+    c->_byte_triggered = false;
+    return 0;
+   case H_RESET:
+    c->reset();
+    return 0;
+   default:
+    return errh->error("<internal>");
+  }
 }
 
 void
 Counter::add_handlers()
 {
-  add_read_handler("count", read_handler, (void *)0);
-  add_read_handler("byte_count", read_handler, (void *)1);
-  add_read_handler("rate", read_handler, (void *)2);
-  add_write_handler("reset", counter_reset_write_handler, 0);
+  add_read_handler("count", read_handler, (void *)H_COUNT);
+  add_read_handler("byte_count", read_handler, (void *)H_BYTE_COUNT);
+  add_read_handler("rate", read_handler, (void *)H_RATE);
+  add_write_handler("reset", write_handler, (void *)H_RESET);
+  add_write_handler("call_after_count", write_handler, (void *)H_CALL_AFTER_COUNT);
+  add_write_handler("call_after_bytes", write_handler, (void *)H_CALL_AFTER_BYTES);
 }
 
 int
--- elements/standard/counter.hh
+++ elements/standard/counter.hh
@@ -4,18 +4,37 @@
 #include <click/element.hh>
 #include <click/ewma.hh>
 #include <click/llrpc.h>
+class HandlerCall;
 
 /*
 =c
-Counter()
+
+Counter([I<KEYWORDS>])
 
 =s measurement
+
 measures packet count and rate
 
 =d
 
 Passes packets unchanged from its input to its output, maintaining statistics
 information about packet count and packet rate.
+
+Keyword arguments are:
+
+=over 8
+
+=item CALL_AFTER_COUNT
+
+Argument is `I<N> I<HANDLER> [I<VALUE>]'. When the packet count reaches I<N>,
+call the write handler I<HANDLER> with value I<VALUE>.
+
+=item CALL_AFTER_BYTES
+
+Argument is `I<N> I<HANDLER> [I<VALUE>]'. When the byte count reaches or
+exceeds I<N>, call the write handler I<HANDLER> with value I<VALUE>.
+
+=back
 
 =h count read-only
 
@@ -33,6 +52,14 @@ weighted moving average) in packets/bytes per second.
 =h reset write-only
 
 Resets the counts and rates to zero.
+
+=h call_after_count write-only
+
+Writes a new CALL_AFTER_COUNT argument. The handler can be omitted.
+
+=h call_after_bytes write-only
+
+Writes a new CALL_AFTER_BYTES argument. The handler can be omitted.
 
 =h CLICK_LLRPC_GET_RATE llrpc
 
@@ -65,6 +92,7 @@ class Counter : public Element { public:
     void reset();
 
     Counter *clone() const			{ return new Counter; }
+    int configure(const Vector<String> &, ErrorHandler *);
     int initialize(ErrorHandler *);
     void add_handlers();
     int llrpc(unsigned, void *);
@@ -74,15 +102,26 @@ class Counter : public Element { public:
   private:
 
 #ifdef HAVE_INT64_TYPES
-    uint64_t _count;
-    uint64_t _byte_count;
+    typedef uint64_t counter_t;
 #else
-    uint32_t _count;
-    uint32_t _byte_count;
+    typedef uint32_t counter_t;
 #endif
+    
+    counter_t _count;
+    counter_t _byte_count;
     RateEWMA _rate;
 
+    counter_t _count_trigger;
+    HandlerCall *_count_trigger_h;
+
+    counter_t _byte_trigger;
+    HandlerCall *_byte_trigger_h;
+
+    bool _count_triggered : 1;
+    bool _byte_triggered : 1;
+
     static String read_handler(Element *, void *);
+    static int write_handler(const String&, Element*, void*, ErrorHandler*);
 
 };
 
