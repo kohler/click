ecfbb3896d5fcad3c8d4327a6af0894eaf89388b
eddietwo <eddietwo>
2001-10-31 18:16:12 +0000

    doc updates

--- doc/mkwebdoc.pl
+++ doc/mkwebdoc.pl
@@ -41,76 +41,114 @@ defined $VERSION || die "VERSION not defined in Makefile";
 close MK;
 
 if ($INSTALL) {
-  mysystem("/bin/rm -rf /tmp/%click-webdoc");
-  mysystem("cd click-$VERSION && ./configure --prefix=/tmp/%click-webdoc --enable-snmp --enable-ipsec --enable-ip6 --enable-etherswitch --enable-radio --enable-grid --enable-analysis --enable-aqm && gmake install-man EXTRA_MAN_ELEMENTS='linuxmodule'");
+    mysystem("/bin/rm -rf /tmp/%click-webdoc");
+    mysystem("cd click-$VERSION && ./configure --prefix=/tmp/%click-webdoc --enable-snmp --enable-ipsec --enable-ip6 --enable-etherswitch --enable-radio --enable-grid --enable-analysis --enable-aqm && gmake install-man EXTRA_MAN_ELEMENTS='linuxmodule'");
 }
 
 # 2. changetemplate.pl
-my(@elements);
+my(@elements, @ealpha, @esections);
 open(IN, "/tmp/%click-webdoc/man/mann/elements.n") || die "/tmp/%click-webdoc/man/mann/elements.n: $!\n";
 while (<IN>) {
-  last if (/^\.SH "ALPHABETICAL/);
+    push @{$esections[-1]}, scalar(@elements) if /^\.SS/ && @esections;
+    push @esections, [$1, scalar(@elements)] if /^\.SS \"(.*)\"/;
+    push @elements, $1 if /^\.M (.*) n/;
+    last if (/^\.SH \"ALPHABETICAL/);
 }
+push @{$esections[-1]}, scalar(@elements);
 while (<IN>) {
-  push @elements, $1 if /^\.M (.*) n/;
+    push @ealpha, $1 if /^\.M (.*) n/;
 }
+@ealpha = sort { lc($a) cmp lc($b) } @ealpha;
 close IN;
 
-open(IN, "$WEBDIR/template") || die "$WEBDIR/template: $!\n";
-open(OUT, ">$WEBDIR/template.new") || die "$WEBDIR/template.new: $!\n";
-my($active) = 0;
+open(IN, "$WEBDIR/index.html") || die "$WEBDIR/index.html: $!\n";
+open(OUT, ">$WEBDIR/index.html.new") || die "$WEBDIR/index.html.new: $!\n";
 while (<IN>) {
-  if ($active == 1 && m{</p}) {
-    $active = -1;
-  } elsif ($active == 0 && m{<!-- elements go here -->}) {
-    print OUT;
-    foreach $_ (sort { lc($a) cmp lc($b) } @elements) {
-      print OUT "<a href=\"$_.n.html\">$_</a><br>\n";
+    if (/^<!-- clickdoc: ealpha (\d+)\/(\d+)/) {
+	print OUT;
+	my($num, $total) = ($1, $2);
+	my($amt) = ((@ealpha - 1) / $2) + 1;
+	my($index) = ($num - 1) * $amt;
+	for ($i = $index; $i < $index + $amt && $i < @ealpha; $i++) {
+	    print OUT "<li><a href='$ealpha[$i].n.html'>$ealpha[$i]</a></li>\n";
+	}
+	1 while (defined($_ = <IN>) && !/^<!-- \/clickdoc/);
+	print OUT;
+    } elsif (/^<!-- clickdoc: esubject (\d+)\/(\d+)/) {
+	print OUT;
+	my($num, $total) = ($1, $2);
+	my($amt) = ((@ealpha - 1) / $2) + 1;
+	my($index) = ($num - 1) * $amt;
+
+	# find first section number
+	my($secno, $secno2);
+	for ($secno = 0; $secno < @esections; $secno++) {
+	    my($diffa, $diffb) = ($esections[$secno]->[1] - $index, $esections[$secno]->[2] - $index);
+	    last if $diffa >= 0;
+	    last if $diffb > 0 && $diffa < 0 && -$diffa < $diffb;
+	}
+
+	# find last section number
+	$index += $amt;
+	for ($secno2 = $secno; $secno2 < @esections; $secno2++) {
+	    my($diffa, $diffb) = ($esections[$secno2]->[1] - $index, $esections[$secno2]->[2] - $index);
+	    last if $diffa >= 0;
+	    last if $diffb > 0 && $diffa < 0 && -$diffa < $diffb;
+	}
+
+	# iterate over sections
+	for ($i = $secno; $i < $secno2; $i++) {
+	    print OUT "<p>", $esections[$i]->[0], "</p>\n";
+	    for ($j = $esections[$i]->[1]; $j < $esections[$i]->[2]; $j++) {
+		print OUT "<li><a href='$elements[$j].n.html'>$elements[$j]</a></li>\n";
+	    }
+	}
+	
+	1 while (defined($_ = <IN>) && !/^<!-- \/clickdoc/);
+	print OUT;
+    } else {
+	print OUT;
     }
-    $active = 2;
-    next;
-  } elsif ($active == 2 && m{\.n\.html}) {
-    next;
-  }
-  print OUT;
 }
 close IN;
 close OUT;
-if (system("cmp $WEBDIR/template $WEBDIR/template.new")) {
-  unlink("$WEBDIR/template") || die "unlink $WEBDIR/template: $!\n";
-  rename("$WEBDIR/template.new", "$WEBDIR/template") || die "rename $WEBDIR/template.new: $!\n";
+if (system("cmp $WEBDIR/index.html $WEBDIR/index.html.new >/dev/null 2>&1")) {
+    unlink("$WEBDIR/index.html") || die "unlink $WEBDIR/index.html: $!\n";
+    rename("$WEBDIR/index.html.new", "$WEBDIR/index.html") || die "rename $WEBDIR/index.html.new: $!\n";
 } else {
-  unlink("$WEBDIR/template.new") || die "unlink $WEBDIR/template.new: $!\n";
+    unlink("$WEBDIR/index.html.new") || die "unlink $WEBDIR/index.html.new: $!\n";
 }
 
 # 3. call `man2html'
 mysystem("man2html -l -m '<b>@</b>' -t $WEBDIR/template -d $WEBDIR /tmp/%click-webdoc/man/man*/*.?");
 
 # 4. change `elements.n.html' into `index.html'
-open(IN, "$WEBDIR/elements.n.html") || die "$WEBDIR/elements.n.html: $!\n";
-open(OUT, ">$WEBDIR/index.html") || die "$WEBDIR/index.html: $!\n";
-while (<IN>) {
-  s|<h1><a.*?>elements</a></h1>|<h1>Click documentation</h1>|;
-  s|<p>documented Click element classes||;
-  s|<h2><a.*?>DESCRIPTION</a></h2>||;
-  s|<a href="index\.html">(.*?)</a>|<b>$1</b>|;
-  if (/<p>This page lists all Click element classes that have manual page documentation./) {
-    print OUT <<'EOF';
-<p>Here is the programmer's documentation available for Click. All
+if (0) {
+    open(IN, "$WEBDIR/elements.n.html") || die "$WEBDIR/elements.n.html: $!\n";
+    open(OUT, ">$WEBDIR/index.html") || die "$WEBDIR/index.html: $!\n";
+    while (<IN>) {
+	s|<h1><a.*?>elements</a></h1>|<h1>Click documentation</h1>|;
+	s|<p>documented Click element classes||;
+	s|<h2><a.*?>DESCRIPTION</a></h2>||;
+	s|<a href="index\.html">(.*?)</a>|<b>$1</b>|;
+	if (/<p>This page lists all Click element classes that have manual page documentation./) {
+	    print OUT <<"EOF";
+	    <p>Here is the programmer\'s documentation available for Click. All
 these files have been automatically translated from documentation provided
 with the distribution, which you can get <a
-href="http://www.pdos.lcs.mit.edu/click/">here</a>. You may also be
+href=\"http://www.pdos.lcs.mit.edu/click/\">here</a>. You may also be
 interested in <a
-href="http://www.pdos.lcs.mit.edu/papers/click:tocs00/">our TOCS
+href=\"http://www.pdos.lcs.mit.edu/papers/click:tocs00/\">our TOCS
 paper</a>.</p>
 <p>The Click element classes that have manual page documentation are:</p>
 EOF
-    next;
-  }
-  print OUT;
+            next;
+	}
+	print OUT;
+    }
+    close IN;
+    close OUT;
 }
-close IN;
-close OUT;
 
 # 5. call `changelog2html'
 mysystem("changelog2html -d $WEBDIR click-$VERSION/NEWS $WEBDIR/../news.html");
