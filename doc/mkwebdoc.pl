0d3ddfd53ab18b7ee6f2a11994f19e1cf689aad5
eddietwo <eddietwo>
2001-11-03 21:37:42 +0000

    doc nitlets

--- doc/mkwebdoc.pl
+++ doc/mkwebdoc.pl
@@ -42,7 +42,7 @@ close MK;
 
 if ($INSTALL) {
     mysystem("/bin/rm -rf /tmp/%click-webdoc");
-    mysystem("cd click-$VERSION && ./configure --prefix=/tmp/%click-webdoc --enable-snmp --enable-ipsec --enable-ip6 --enable-etherswitch --enable-radio --enable-grid --enable-analysis --enable-aqm && gmake install-man EXTRA_MAN_ELEMENTS='linuxmodule i586 i686 linux_2_2 linux_2_4' && gmake install-local");
+    mysystem("cd click-$VERSION && ./configure --prefix=/tmp/%click-webdoc --enable-snmp --enable-ipsec --enable-ip6 --enable-etherswitch --enable-radio --enable-grid --enable-analysis --enable-aqm && gmake install-man EXTRA_PROVIDES='linuxmodule i586 i686 linux_2_2 linux_2_4' && gmake install-local EXTRA_PROVIDES='linuxmodule i586 i686 linux_2_2 linux_2_4'");
 }
 
 # 1.5. read elementmap
