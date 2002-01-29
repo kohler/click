c5b3d47b58b9f87c252ab61d1e6ac4217a3ecec9
yipal <yipal>
2002-01-29 21:01:44 +0000

    working for server creation

--- conf/ron/make-all-config.pl
+++ conf/ron/make-all-config.pl
@@ -9,7 +9,9 @@ sub usage {
 }
 
 sub main {
+    $count = 0;
 
+    # Figure out where the node list is
     if (scalar(@ARGV) < 1) {
 	if ($verbose) {print "Using STDIN\n";}
     } else {
@@ -17,20 +19,53 @@ sub main {
 	open(NODELIST, $ARGV[0]) or die "Could not open $ARGV[0]\n";
     }
 
-
+    # Read in node list
     while($line = scalar(@ARGV) < 1? <STDIN> : <NODELIST>) {
-	if ( !($line =~ /\#/)) {
-
-	    if ($line =~ /([\d\.]+)\s/) {
-		print "$1\n";
-	    }
+	if ($line =~ /(.*)\#?/ and $1 =~ /(\S+)\s+([\d\.]+)\s+(\S+)\s+(\S+)\s+(\S+)/) {
+	    push @device, $1;
+	    push @ip, $2;
+	    push @hw, $3;
+	    push @gw, $4;
+	    push @name, $5;
 	}
-
-
 
     }
 
+    # Create server configuration
+    for($i=0; $i<scalar(@ip); $i++) {
+	$s = "./make-server-ron.pl $device[$i] $ip[$i] $hw[$i] $gw[$i] ";
+	
+	for($j=0; $j<scalar(@ip); $j++) {
+	    if ($j != $i) {
+		$s = "$s$ip[$j] ";
+	    }
+	}
+	$s = "$s> ";
+	$s = "$s$name[$i]-server.conf";
+	@args = ("tcsh", "-c", $s);
+	system(@args);
+	#print "$s\n";
+    }
 
+    # Create client configuration
+    for($i=0; $i<scalar(@ip); $i++) {
+	#print "./make-client-ron.pl $device[$i] $ip[$i] $hw[$i] $gw[$i] ";
+	
+	for($j=0; $j<scalar(@ip); $j++) {
+	    if ($j != $i) {
+		#print "$ip[$j] ";
+	    }
+	}
+	
+	#print "> $name[$i]-server.conf\n";
+    }
+
+
+
+
+    close(STDIN);
+    close(NODELIST);
+    
 }
 
 &main();
