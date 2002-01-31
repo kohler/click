5ed1e83525704f1c351f327e5475af69df263125
yipal <yipal>
2002-01-31 15:13:50 +0000

    removed background

--- conf/ron/start-client.sh
+++ conf/ron/start-client.sh
@@ -3,5 +3,5 @@
 setenv CLICKPATH /usr/home/ron/yipal/click-export/lib
 rm -f /tmp/clicksocket
 cd /usr/home/ron/yipal/click-export/bin
-sudo ./click ../conf/$1-client.conf &
+sudo ./click ../conf/$1-client.conf 
 
