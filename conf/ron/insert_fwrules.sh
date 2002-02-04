b632d5c7117b95f290b82b30ffb0cb022e53ad2f
yipal <yipal>
2002-02-04 22:32:16 +0000

    fixed typo

--- conf/ron/insert_fwrules.sh
+++ conf/ron/insert_fwrules.sh
@@ -1,7 +1,7 @@
 #!/bin/tcsh
 
 sudo ipfw add 750 divert 3000 udp from any 4000 to me 4000 in recv fxp0
-sudo ipfw add 750 divert 3001 tcp from any to me 50000-50999 in recv fxp
+sudo ipfw add 750 divert 3001 tcp from any to me 50000-50999 in recv fxp0
 sudo ipfw add 750 divert 4000 tcp from $HOST 49990-49999 to any out xmit fxp0
 sudo ipfw add 750 divert 4001 tcp from any to $HOST 49990-49999 in recv fxp0
 sudo ipfw add 750 divert 4002 udp from any 4001 to $HOST 4001 in recv fxp0
