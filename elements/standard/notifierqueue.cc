1eae151ef41e924f57c86a95d314b9fd9afd3bff
eddietwo <eddietwo>
2002-03-04 08:00:00 +0000

    add ActivitySignal and ActivityNotifier and NotifierQueue, a queue that wakes up Tasks downstream when appropriate

---
+++ elements/standard/notifierqueue.cc
@@ -0,0 +1,77 @@
+// -*- c-basic-offset: 4 -*-
+/*
+ * notifierqueue.{cc,hh} -- queue element that notifies when emptiness changes
+ * Eddie Kohler
+ *
+ * Copyright (c) 2002 International Computer Science Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "notifierqueue.hh"
+
+NotifierQueue::NotifierQueue()
+    : _sleepiness(0)
+{
+    MOD_INC_USE_COUNT;
+}
+
+NotifierQueue::~NotifierQueue()
+{
+    MOD_DEC_USE_COUNT;
+}
+
+void *
+NotifierQueue::cast(const char *n)
+{
+    if (strcmp(n, "NotifierQueue") == 0)
+	return (NotifierQueue *)this;
+    else if (strcmp(n, "ActivityNotifier") == 0)
+	return (ActivityNotifier *)this;
+    else
+	return Queue::cast(n);
+}
+
+int
+NotifierQueue::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    ActivityNotifier::initialize(router());
+    return Queue::configure(conf, errh);
+}
+
+void
+NotifierQueue::push(int, Packet *packet)
+{
+    // wish I could inline...
+    Queue::push(0, packet);
+
+    if (size() == 1 && listeners_asleep()) {
+	_sleepiness = 0;
+	wake_listeners();
+    }
+}
+
+Packet *
+NotifierQueue::pull(int)
+{
+    Packet *p = deq();
+
+    if (!p && ++_sleepiness == SLEEPINESS_TRIGGER)
+	sleep_listeners();
+    
+    return p;
+}
+
+// XXX reset?
+
+ELEMENT_REQUIRES(ActivityNotifier Queue)
+EXPORT_ELEMENT(NotifierQueue)
---
+++ elements/standard/notifierqueue.hh
@@ -0,0 +1,54 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_NOTIFIERQUEUE_HH
+#define CLICK_NOTIFIERQUEUE_HH
+#include <click/element.hh>
+#include "activity.hh"
+#include "queue.hh"
+
+/*
+ * =c
+ * NotifierQueue
+ * NotifierQueue(CAPACITY)
+ * =s storage
+ * stores packets in a FIFO queue
+ * =d
+ * Stores incoming packets in a first-in-first-out queue.
+ * Drops incoming packets if the queue already holds CAPACITY packets.
+ * The default for CAPACITY is 1000.
+ * =h length read-only
+ * Returns the current number of packets in the queue.
+ * =h highwater_length read-only
+ * Returns the maximum number of packets that have ever been in the queue at once.
+ * =h capacity read/write
+ * Returns or sets the queue's capacity.
+ * =h drops read-only
+ * Returns the number of packets dropped by the Queue so far.
+ * =h reset_counts write-only
+ * When written, resets the C<drops> and C<highwater_length> counters.
+ * =h reset write-only
+ * When written, drops all packets in the Queue.
+ * =a RED, FrontDropQueue
+ */
+
+class NotifierQueue : public Queue, public ActivityNotifier { public:
+
+    NotifierQueue();
+    ~NotifierQueue();
+  
+    const char *class_name() const		{ return "NotifierQueue"; }
+    void *cast(const char *);
+    NotifierQueue *clone() const		{ return new NotifierQueue; }
+
+    int configure(Vector<String> &conf, ErrorHandler *);
+    
+    void push(int port, Packet *);
+    Packet *pull(int port);
+
+  private:
+
+    static const int SLEEPINESS_TRIGGER = 8;
+    int _sleepiness;
+    
+};
+
+#endif
