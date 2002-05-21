2d9f8c580f041d198b6810acad4b031083c041e5
eddietwo <eddietwo>
2002-05-21 15:47:02 +0000

    more changes from Scott

--- conf/make-ip-conf.pl
+++ conf/make-ip-conf.pl
@@ -1,21 +1,20 @@
 #!/usr/local/bin/perl -w
 
 # make-ip-conf.pl -- make a Click IP router configuration
-# Robert Morris
+# Robert Morris, Eddie Kohler, David Scott Page
 #
-# Copyright (c) 1999-2000 Massachusetts Institute of Technology.
+# Copyright (c) 1999-2000 Massachusetts Institute of Technology
+# Copyright (c) 2002 International Computer Science Institute
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, subject to the following
-# conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# Further elaboration of this license, including a DISCLAIMER OF ANY
-# WARRANTY, EXPRESS OR IMPLIED, is provided in the LICENSE file, which is
-# also accessible at http://www.pdos.lcs.mit.edu/click/license.html
+# to deal in the Software without restriction, subject to the conditions
+# listed in the Click LICENSE file. These conditions include: you must
+# preserve this copyright notice, and you cannot mention the copyright
+# holders in advertising related to the Software without their permission.
+# The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+# notice is a summary of the Click LICENSE file; the license in that file is
+# legally binding.
 
 # Make a Click IP router configuration.  This script generates a
 # configuration using PollDevices. You can change it to use
