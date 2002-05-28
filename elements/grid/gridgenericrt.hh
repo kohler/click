67bb9607027b1c5c8b0a3d8e1e0f805644bcec74
decouto <decouto>
2002-05-28 23:08:32 +0000

    oops need this to compile, thanks ed.

---
+++ elements/grid/gridgenericrt.hh
@@ -0,0 +1,52 @@
+#ifndef GRIDGENERICRT_HH
+#define GRIDGENERICRT_HH
+
+// public interface class to Grid routetables.  yes, i know, this is
+// not a great abstraction but it works for now.
+
+#include <click/element.hh>
+#include <click/ipaddress.hh>
+#include <click/etheraddress.hh>
+#include <click/vector.hh>
+#include "grid.hh"
+
+class GridGenericRouteTable : public Element {
+
+public:
+  
+  GridGenericRouteTable() { }
+  GridGenericRouteTable(int ninputs, int noutputs) : Element(ninputs, noutputs) { }
+
+  struct RouteEntry {
+    IPAddress dest_ip;
+    grid_location dest_loc;
+    bool loc_good;
+    unsigned short loc_err;
+    EtherAddress next_hop_eth;
+    IPAddress next_hop_ip;
+
+    RouteEntry(const IPAddress &dst, bool lg, unsigned short le, 
+	       const grid_location &l, const EtherAddress &nhe, const IPAddress &nhi) :
+      dest_ip(dst), dest_loc(l), loc_good(lg), loc_err(le), next_hop_eth(nhe), 
+      next_hop_ip(nhi) { }
+
+    RouteEntry() : loc_good(false), loc_err(0) { }
+  };
+
+
+  // return false if there is no entry for a GW, else fill in entry
+  virtual bool current_gateway(RouteEntry &entry) = 0;
+
+  // return false if there is no entry for this dest, else fill in entry
+  virtual bool get_one_entry(IPAddress &dest_ip, RouteEntry &entry) = 0;
+
+  // append all the current route entries to vec.  You should clear
+  // vec before calling this method, if desired.
+  virtual void get_all_entries(Vector<RouteEntry> &vec) = 0;
+  
+  virtual ~GridGenericRouteTable() { }
+};
+
+
+
+#endif
