c688f6d230f199234dace453991ed1a1ea93b48c
eddietwo <eddietwo>
2002-07-23 00:13:08 +0000

    add SetUDPChecksum element, requested by Filip Henderieckx

---
+++ elements/tcpudp/setudpchecksum.hh
@@ -0,0 +1,35 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_SETUDPCHECKSUM_HH
+#define CLICK_SETUDPCHECKSUM_HH
+
+/*
+ * =c
+ * SetUDPChecksum()
+ * =s UDP
+ * sets UDP packets' checksums
+ * =d
+ * Input packets should be UDP in IP.
+ *
+ * Calculates the UDP header's checksum and sets the checksum header field.
+ * Uses the IP header fields to generate the pseudo-header.
+ *
+ * =a CheckUDPHeader, SetIPChecksum, CheckIPHeader, SetTCPChecksum
+ */
+
+#include <click/element.hh>
+#include <click/glue.hh>
+
+class SetUDPChecksum : public Element { public:
+    
+    SetUDPChecksum();
+    ~SetUDPChecksum();
+  
+    const char *class_name() const	{ return "SetUDPChecksum"; }
+    const char *processing() const	{ return AGNOSTIC; }
+    SetUDPChecksum *clone() const	{ return new SetUDPChecksum; }
+
+    Packet *simple_action(Packet *);
+
+};
+
+#endif
