63b5fe6358da9f2f6208e8d82128708befe9e75a
eddietwo <eddietwo>
2003-02-25 01:38:08 +0000

    add click_static_{initialize,cleanup}() functions, and click_read_router(), taking over most of userlevel/click.cc's functionality; this will make it easier to write other drivers

--- include/click/driver.hh
+++ include/click/driver.hh
@@ -1,7 +1,19 @@
+// -*- c-basic-offset: 4; related-file-name: "../../lib/driver.cc" -*-
 #ifndef CLICK_DRIVER_HH
 #define CLICK_DRIVER_HH
 #include <click/package.hh>
 
 #define CLICK_DEFAULT_PROVIDES	/* nada */
+
+#ifdef CLICK_USERLEVEL
+CLICK_DECLS
+
+void click_static_initialize();
+void click_static_cleanup();
+
+Router *click_read_router(String filename, bool is_expr, ErrorHandler * = 0, bool initialize = true);
+
+CLICK_ENDDECLS
+#endif
 
 #endif
