f9898c7432a8389a633b869bca2286c847691d5c
eddietwo <eddietwo>
2003-03-03 07:21:44 +0000

    IPOutputCombo: update with new code from IPGWOptions

--- elements/ip/ipoutputcombo.cc
+++ elements/ip/ipoutputcombo.cc
@@ -74,92 +74,106 @@ IPOutputCombo::push(int, Packet *p_in)
   assert(ip);
   unsigned hlen = (ip->ip_hl << 2);
 
-  if(hlen > sizeof(click_ip)){
-    u_char *oa = (u_char *) (ip + 1);
-    int olen = hlen - sizeof(click_ip);
+  if (hlen > sizeof(click_ip)) {
+    uint8_t *woa = p->network_header();
+    int hlen = p->network_header_length();
+
     int oi;
-  
-    for (oi = 0; oi < olen; ) {
-      unsigned type = oa[oi];
-      int xlen;
-      if(type <= 1)
-        xlen = 1;
-      else
-        xlen = oa[oi+1];
-      if(oi + xlen > olen)
-        break;
-      if(type == IPOPT_EOL){
-        /* end of option list */
-        break;
-      } else if(type == IPOPT_RR){
-        /*
-       * Record Route.
-       * Apparently the pointer (oa[oi+2]) is 1-origin.
-       */
-        int p = oa[oi+2] - 1;
-        if(p >= 3 && p+4 <= xlen){
-          memcpy(oa + oi + p, &_my_ip, 4);
-          oa[oi+2] += 4;
-          do_cksum = 1;
-        } else if(p != xlen){
-          problem_offset = 20 + oi + 2;
-          goto ipgw_send_error;
-        }
-      } else if(type == IPOPT_TS){
-        /*
-       * Timestamp Option.
-       * We can't do a good job with the pre-specified mode (flg=3),
-       * since we don't know all our i/f addresses.
-       */
-        int p = oa[oi+2] - 1;
-        int oflw = oa[oi+3] >> 4;
-        int flg = oa[oi+3] & 0xf;
-        int overflowed = 0;
+    for (oi = sizeof(click_ip); oi < hlen; ) {
+      // handle one-byte options
+      unsigned type = woa[oi];
+      if (type == IPOPT_NOP) {
+	oi++;
+	continue;
+      } else if (type == IPOPT_EOL)
+	/* end of option list */
+	break;
 
-        struct timeval tv;
-        click_gettimeofday(&tv);
-        int ms = htonl((tv.tv_sec % 86400) * 1000 + tv.tv_usec / 1000);
-
-        if(p < 4){
-          problem_offset = 20 + oi + 2;
-          goto ipgw_send_error;
-        } else if(flg == 0){
-          /* 32-bit timestamps only */
-          if(p+4 <= xlen){
-            memcpy(oa + oi + p, &ms, 4);
-            oa[oi+2] += 4;
-            do_cksum = 1;
-          } else {
-            overflowed = 1;
-          }
-        } else if(flg == 1){
-          /* ip address followed by timestamp */
-          if(p+8 <= xlen){
-            memcpy(oa + oi + p, &_my_ip, 4);
-            memcpy(oa + oi + p + 4, &ms, 4);
-            oa[oi+2] += 8;
-            do_cksum = 1;
-          } else {
-            overflowed = 1;
-          }
-        } else if(flg == 3){
-          /* only if it's my address */
-          if(p+8 <= xlen && memcmp(oa + oi + p, &_my_ip, 4) == 0){
-            memcpy(oa + oi + p + 4, &ms, 4);
-            oa[oi+2] += 8;
-            do_cksum = 1;
-          }
-        }
-        if(overflowed){
-          if(oflw < 15){
-            oa[oi+3] = ((oflw + 1) << 4) | flg;
-            do_cksum = 1;
-          } else {
-            problem_offset = 20 + oi + 3;
-            goto ipgw_send_error;
-          }
-        }
+      // otherwise, get option length
+      int xlen = woa[oi + 1];
+      if (xlen < 2 || oi + xlen > hlen) {
+	// bad length
+	problem_offset = oi + 1; // to point at length
+	goto ipgw_send_error;
+      } else if (type != IPOPT_RR && type != IPOPT_TS) {
+	// not for us to process
+	oi += xlen;
+	continue;
       }
+
+      // have a writable packet already
+    
+      if(type == IPOPT_RR){
+	/*
+	 * Record Route.
+	 * Apparently the pointer (oa[oi+2]) is 1-origin.
+	 */
+	int p = woa[oi+2] - 1;
+	if (p >= 3 && p + 4 <= xlen) {
+	  memcpy(woa + oi + p, &_my_ip, 4);
+	  woa[oi+2] += 4;
+	  do_cksum = 1;
+	} else if (p != xlen) {
+	  problem_offset = oi + 2;
+	  goto ipgw_send_error;
+	}
+      } else if(type == IPOPT_TS){
+	/*
+	 * Timestamp Option.
+	 * We can't do a good job with the pre-specified mode (flg=3),
+	 * since we don't know all our i/f addresses.
+	 */
+	int p = woa[oi+2] - 1;
+	int oflw = woa[oi+3] >> 4;
+	int flg = woa[oi+3] & 0xf;
+	bool overflowed = 0;
+
+	struct timeval tv;
+	click_gettimeofday(&tv);
+	int ms = htonl((tv.tv_sec % 86400) * 1000 + tv.tv_usec / 1000);
+
+	if(p < 4){
+	  problem_offset = oi + 2;
+	  goto ipgw_send_error;
+	} else if(flg == 0){
+	  /* 32-bit timestamps only */
+	  if(p+4 <= xlen){
+	    memcpy(woa + oi + p, &ms, 4);
+	    woa[oi+2] += 4;
+	    do_cksum = 1;
+	  } else
+	    overflowed = 1;
+	} else if(flg == 1){
+	  /* ip address followed by timestamp */
+	  if(p+8 <= xlen){
+	    memcpy(woa + oi + p, &_my_ip, 4);
+	    memcpy(woa + oi + p + 4, &ms, 4);
+	    woa[oi+2] += 8;
+	    do_cksum = 1;
+	  } else
+	    overflowed = 1;
+	} else if (flg == 3 && p + 8 <= xlen) {
+	  /* only if it's my address */
+	  if (memcmp(woa + oi + p, &_my_ip, 4) == 0) {
+	    memcpy(woa + oi + p + 4, &ms, 4);
+	    woa[oi+2] += 8;
+	    do_cksum = 1;
+	  }
+	} else {
+	  problem_offset = oi + 3;
+	  goto ipgw_send_error;
+	}
+	if (overflowed) {
+	  if (oflw < 15) {
+	    woa[oi+3] = ((oflw + 1) << 4) | flg;
+	    do_cksum = 1;
+	  } else {
+	    problem_offset = oi + 3;
+	    goto ipgw_send_error;
+	  }
+	}
+      }
+      
       oi += xlen;
     }
   }
