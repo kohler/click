28da74da460f47011beafab91dadcdf36a10db64
decouto <decouto>
2003-03-05 23:04:53 +0000

    A poor man's double-ended queue; this is a simple O(n) class derived from Vector

---
+++ elements/grid/qvec.hh
@@ -0,0 +1,49 @@
+// qvec.hh
+// Douglas S. J. De Couto
+// 5 March 2003
+
+// Poor man's queue
+
+#ifndef CLICK_QUEUE_HH
+#define CLICK_QUEUE_HH
+
+#include <click/vector.hh>
+
+template <class T>
+class QVec : public Vector<T> {
+  typedef Vector<T> Base;
+public:
+  QVec() { }
+  explicit QVec(int capacity) : Base(capacity) { }
+
+  T &front() { return at(0); }
+  
+  void push_front(const T &);
+  void pop_front();
+};
+
+
+
+template <class T> inline void
+QVec<T>::push_front(const T &e)
+{
+  int n = size();
+  if (n > 0)
+    push_back(back());
+  for (int i = 1; i < n; i++)
+    this->at(i) = this->at(i - 1);
+  this->at(0) = e;
+}
+
+template <class T> inline void
+QVec<T>::pop_front()
+{
+  assert(size() > 0);
+  int n = size();
+  for (int i = 0; i < n - 1; i++)
+    this->at(i) = this->at(i + 1);
+  pop_back();
+}
+
+
+#endif
