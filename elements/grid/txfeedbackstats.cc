ddf6eceb61bcedacb404b79375b7cf14f2b52f71
decouto <decouto>
2003-06-27 19:56:42 +0000

    Element to track 802.11 transmission stats

---
+++ elements/grid/txfeedbackstats.cc
@@ -0,0 +1,222 @@
+/*
+ * txfeedbackstats.{cc,hh} -- track per-link transmission statistics.
+ * Douglas S. J. De Couto
+ *
+ * Copyright (c) 2003 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <clicknet/ether.h>
+#include <click/error.hh>
+#include <click/glue.hh>
+#include <click/timer.hh>
+#include <click/straccum.hh>
+#include <elements/grid/grid.hh>
+#include <elements/grid/txfeedbackstats.hh>
+#include <elements/grid/timeutils.hh>
+CLICK_DECLS
+
+TXFeedbackStats::TXFeedbackStats()
+  : _tau(10000), _min_pkts(10)
+{
+  MOD_INC_USE_COUNT;
+  add_input();
+  add_output();
+}
+
+TXFeedbackStats::~TXFeedbackStats()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+TXFeedbackStats::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+  int res = cp_va_parse(conf, this, errh,
+			cpKeywords,
+			"TAU", cpUnsigned, "TX feedback tracking period (msecs)", &_tau,
+			"MIN_PKTS", cpUnsigned, "minimum number of packets required to estimate TX count", &_min_pkts,
+			0);
+  if (res < 0)
+    return res;
+
+  if (_min_pkts < 1)
+    return errh->error("MIN_PKTS must be >= 1");
+  return 0;
+}
+
+int
+TXFeedbackStats::initialize(ErrorHandler *)
+{
+  _tau_tv.tv_sec = _tau / 1000;
+  _tau_tv.tv_usec = 1000 * (_tau % 1000);
+  return 0;
+}
+
+
+Packet *
+TXFeedbackStats::simple_action(Packet *p)
+{
+  click_ether *eh = (click_ether *) p->data();
+
+  if (p->length() < sizeof(click_ether)) {
+    click_chatter("TXFeedbackStats %s: got short packet, expected at least %u, but only got %u", 
+		  id().cc(), p->length());
+    return p;
+  }  
+
+  // decoding values from aironet tx fid, see manual p. 7-46
+  /* eventually this card-specific stuff will be moved into a
+     FooTxFeedback el, once we sort out what the annos look like */
+  tx_result_t res;
+  switch (p->user_anno_c(0)) {
+  case 0: res = TxOk; break;
+  case 2: res = TxLifetimeExceeded; break;
+  case 4: res = TxMaxRetriesExceed; break;
+  default: res = TxUnknownResult;
+  }
+
+  // XXX should we add 1 to each retry counter (since we count all attempts, not just retries)?
+  add_stat(EtherAddress(eh->ether_dhost), p->length(), p->timestamp_anno(), 
+	   res, (int) p->user_anno_c(3), (int) p->user_anno_c(2));
+
+  return p;
+}
+
+void
+TXFeedbackStats::add_stat(const EtherAddress &dest, int sz, const timeval &when, 
+			  tx_result_t res, unsigned data_attempts, unsigned  rts_attempts)
+{
+  StatQ *q = cleanup_map(dest);
+  if (!q) {
+    _stat_map.insert(dest, StatQ());
+    q = _stat_map.findp(dest);
+  }
+
+  q->push_front(stat_t(when, sz, res, data_attempts, rts_attempts));
+}
+
+TXFeedbackStats::StatQ *
+TXFeedbackStats::cleanup_map(const EtherAddress &dest)
+{
+  struct timeval oldest;
+  click_gettimeofday(&oldest);
+  oldest -= _tau_tv;
+
+
+  StatQ *q = _stat_map.findp(dest);
+  if (!q)
+    return 0;
+
+  // discard too-old stats
+  while (q->size() > 0 && q->back().when < oldest)
+    q->pop_back();
+
+  // could also remove StatQs for dests with no data.  for now we'll
+  // pretend that there are only a limited number of neighbors we'll
+  // ever see.
+  
+  return q;
+}
+
+bool
+TXFeedbackStats::est_tx_count(const EtherAddress &dest, int &etx)
+{
+  unsigned n_data, n_rts, n_pkts;
+  if (!get_counts(dest, n_data, n_rts, n_pkts))
+    return false;
+
+  if (n_pkts < _min_pkts)
+    return false;    
+
+  etx = 100 * (n_rts + n_data) / n_pkts;
+
+  return true;
+}
+
+bool
+TXFeedbackStats::get_counts(const EtherAddress &dest, unsigned &n_data, unsigned &n_rts, unsigned &n_pkts)
+{
+  StatQ *q = cleanup_map(dest);
+  if (!q)
+    return false;
+
+  n_pkts = (unsigned) q->size();
+  n_data = 0;
+  n_rts = 0;
+  for (StatQ::const_iterator i = q->begin(); i != q->end(); i++) {
+    n_data += i->n_data;
+    n_rts += i->n_rts;
+  }
+  return true;
+}
+
+String
+TXFeedbackStats::print_stats()
+{
+  Vector<EtherAddress> v;
+  for (StatMap::const_iterator i = _stat_map.begin(); i; i++)
+    v.push_back(i.key());
+
+  StringAccum sa;
+
+  for (int i = 0; i < v.size(); i++) {
+    sa << v[i] << "  ";
+    unsigned n_data, n_rts, n_pkts;
+    if (!get_counts(v[i], n_data, n_rts, n_pkts)) 
+      sa << "No data available";
+    else {
+      sa << n_data << "\t" << n_rts << "\t" << n_pkts << "\t";
+      if (n_pkts < _min_pkts)
+	sa << "Too few packets";
+      else
+	sa << (100 * (n_data + n_rts) / n_pkts);
+    }
+    sa << "\n";
+  }
+  return sa.take_string();  
+}
+
+String
+TXFeedbackStats::read_params(Element *xf, void *n)
+{
+  TXFeedbackStats *f = (TXFeedbackStats *) xf;
+  switch ((int) n) {
+  case 0: return String(f->_tau) + "\n"; break;
+  case 1: return String(f->_min_pkts) + "\n"; break;
+  case 2: return f->print_stats(); break;
+  default:
+    assert(0);
+  }
+  return "<unknown parameter>";
+}
+
+void
+TXFeedbackStats::add_handlers()
+{
+  add_default_handlers(false);
+  add_read_handler("tau", read_params, (void *)0);
+  add_read_handler("min_pkts", read_params, (void *)1);
+  add_read_handler("stats", read_params, (void *)2);
+}
+
+EXPORT_ELEMENT(TXFeedbackStats)
+
+#include <click/dequeue.cc>
+template class DEQueue<TXFeedbackStats::stat_t>;
+
+#include <click/bighashmap.cc>
+template class BigHashMap<EtherAddress, TXFeedbackStats::StatQ>;
+
+CLICK_ENDDECLS
---
+++ elements/grid/txfeedbackstats.hh
@@ -0,0 +1,116 @@
+#ifndef TXSTATSHH
+#define TXSTATSHH
+
+/*
+ * =c
+ * TXFeedbackStats([I<KEYWORDS>])
+ * =s Grid
+ * Track link transmissions statistics, such as number of retries, failures, etc. 
+ *
+ * =d
+ *
+ * Expects Ethernet packets as input, with their timestamps and
+ * Transmission Feedback annotations set.  Packets are passed through
+ * untouched.
+ *
+ * Keyword arguments are:
+ *
+ * =over 8
+ *
+ * =item TAU
+ *
+ * Unsigned integer.  Number of millisecons for which to remember
+ * packets.  Defaults to 10,000 (10 seconds).
+ *
+ * =item MIN_PKTS
+ *
+ * Unsigned integer.  Minimum number of packets required during the
+ * last TAU seconds in order to generate a transmission count
+ * estimate.  Defaults to 10 packets.  Must be >= 1.
+ *
+ * =back
+ *
+ * =h tau read-only
+ * =h min_pkts read-only
+ * =h stats read-only
+ * Returns current stats, one line per destination, in this format:
+ * <eth> <num_data_tries> <num_rts_tries> <num_pkts> <est_tx_count>
+ * =a
+ * LinkStat */
+
+#include <click/bighashmap.hh>
+#include <click/element.hh>
+#include <click/glue.hh>
+#include <click/etheraddress.hh>
+#include <click/dequeue.hh>
+#include <elements/grid/grid.hh>
+
+CLICK_DECLS
+
+class TXFeedbackStats : public Element {
+ public:
+
+  // eventually move into FooTxFeedback els, once we sort out exactly
+  // what the annotations should look like
+  enum tx_result_t {
+    TxOk               = 0,
+    TxLifetimeExceeded = 1,
+    TxMaxRetriesExceed = 2,
+    TxUnknownResult    = 99
+  };
+
+  TXFeedbackStats();
+  ~TXFeedbackStats();
+  
+  const char *class_name() const		{ return "TXFeedbackStats"; }
+  const char *processing() const		{ return AGNOSTIC; }
+  const char *flow_code()  const                { return "x/x"; }
+  
+  TXFeedbackStats *clone() const                { return new TXFeedbackStats; }
+
+  void add_handlers();
+
+  int configure(Vector<String> &, ErrorHandler *);
+  int initialize(ErrorHandler *);
+
+  Packet *simple_action(Packet *);
+
+  // Estimate the transmission count ETX to DEST.  If there is enough
+  // data (more than MIN_PKTS packet to DEST in the last TAU
+  // milliseconds), calculate ETX as a percentage (1 tx = 100), and
+  // return true.  Else return false.
+  bool est_tx_count(const EtherAddress &dest, int &etx);
+
+private:
+  
+  static String read_params(Element *, void *);
+  
+  struct stat_t {
+    struct timeval when;
+    int sz;
+    tx_result_t res;
+    unsigned n_data; // number data transmissions
+    unsigned n_rts;  // number rts transmissions
+    stat_t(const timeval &w, int s, tx_result_t r, unsigned nl, unsigned ns) :
+      when(w), sz(s), res(r), n_data(nl), n_rts(ns) { }
+  };
+
+  // most recent data goes on to head, oldest is at tail
+  typedef DEQueue<stat_t> StatQ;
+  typedef BigHashMap<EtherAddress, StatQ> StatMap;
+  StatMap _stat_map;
+
+  void add_stat(const EtherAddress &, int, const timeval &, tx_result_t, unsigned, unsigned);
+  bool get_counts(const EtherAddress &, unsigned &, unsigned  &, unsigned &);
+
+  StatQ *cleanup_map(const EtherAddress &);
+
+  String print_stats();
+
+  unsigned _tau;
+  struct timeval _tau_tv;
+  unsigned _min_pkts;
+};
+
+CLICK_ENDDECLS
+#endif
