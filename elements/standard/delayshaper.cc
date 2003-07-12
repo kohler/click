a7abb6e6560447fb81d00ec841c21a8c217ec0d8
eddietwo <eddietwo>
2003-07-12 20:02:55 +0000

    DelayShaper rewrite: now it behaves somewhat like DelayUnqueue, and it has a Notifier too

--- elements/standard/delayshaper.cc
+++ elements/standard/delayshaper.cc
@@ -1,8 +1,10 @@
+// -*- c-basic-offset: 4 -*-
 /*
  * delayshaper.{cc,hh} -- element pulls packets from input, delays returnign
  * the packet to output port.
  *
  * Copyright (c) 1999-2001 Massachusetts Institute of Technology
+ * Copyright (c) 2003 International Computer Science Institute
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -24,72 +26,112 @@
 CLICK_DECLS
 
 DelayShaper::DelayShaper()
-  : Element(1, 1), _p(0)
+    : Element(1, 1), _p(0), _timer(this)
 {
-  MOD_INC_USE_COUNT;
+    MOD_INC_USE_COUNT;
 }
 
 DelayShaper::~DelayShaper()
 {
-  MOD_DEC_USE_COUNT;
+    MOD_DEC_USE_COUNT;
+}
+
+void *
+DelayShaper::cast(const char *n)
+{
+    if (strcmp(n, "DelayShaper") == 0)
+	return (DelayShaper *)this;
+    else if (strcmp(n, "Notifier") == 0)
+	return static_cast<Notifier *>(this);
+    else
+	return Element::cast(n);
+}
+
+Notifier::SearchOp
+DelayShaper::notifier_search_op()
+{
+    return SEARCH_UPSTREAM_LISTENERS;
 }
 
 int
 DelayShaper::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-  return cp_va_parse(conf, this, errh,
-		     cpSecondsAsMicro, "delay", &_delay, 0);
+    ActiveNotifier::initialize(router());
+    return cp_va_parse(conf, this, errh,
+		       cpInterval, "delay", &_delay, 0);
+}
+
+int
+DelayShaper::initialize(ErrorHandler *)
+{
+    _timer.initialize(this);
+    _upstream_signal = Notifier::upstream_pull_signal(this, 0, 0);
+    return 0;
 }
 
 void
 DelayShaper::cleanup(CleanupStage)
 {
-  if (_p)
-    _p->kill();
-}
-
-static inline unsigned
-elapsed_us(struct timeval tv)
-{
-  struct timeval t;
-  unsigned e = 0;
-  click_gettimeofday(&t);
-  e = (t.tv_sec - tv.tv_sec)*1000000;
-  if (t.tv_usec < tv.tv_usec) {
-    t.tv_usec += 1000000;
-    e -= 1000000;
-  }
-  e += t.tv_usec-tv.tv_usec;
-  return e;
+    if (_p)
+	_p->kill();
 }
 
 Packet *
 DelayShaper::pull(int)
 {
-  if (!_p) 
-    _p = input(0).pull(); 
-  if (_p) {
-    unsigned t = elapsed_us(_p->timestamp_anno());
-    if (t >= _delay) { 
-      Packet *p = _p;
-      _p = 0;
-      return p;
+    // read a packet
+    if (!_p && (_p = input(0).pull())) {
+	if (!_p->timestamp_anno().tv_sec) // get timestamp if not set
+	    click_gettimeofday(&_p->timestamp_anno());
+	_p->timestamp_anno() += _delay;
     }
-  }
-  return 0;
+    
+    if (_p) {
+	struct timeval now, diff;
+	click_gettimeofday(&now);
+	diff = _p->timestamp_anno() - now;
+	
+	if (diff.tv_sec < 0 || (diff.tv_sec == 0 && diff.tv_usec == 0)) {
+	    // packet ready for output
+	    Packet *p = _p;
+	    p->timestamp_anno() = now;
+	    _p = 0;
+	    return p;
+	} else if (diff.tv_sec == 0 && diff.tv_usec < 100000) {
+	    // small delta, don't go to sleep -- but mark our Signal as active,
+	    // since we have something ready. NB: should not wake listeners --
+	    // we are in pull()!
+	    set_signal_active(true);
+	} else {
+	    // large delta, go to sleep and schedule Timer
+	    _timer.schedule_at(_p->timestamp_anno());
+	    sleep_listeners();
+	}
+    } else if (!_upstream_signal) {
+	// no packet available, we go to sleep right away
+	sleep_listeners();
+    }
+
+    return 0;
+}
+
+void
+DelayShaper::run_timer()
+{
+    wake_listeners();
 }
 
 String
 DelayShaper::read_param(Element *e, void *)
 {
-  DelayShaper *u = (DelayShaper *)e;
-  return cp_unparse_microseconds(u->_delay) + "\n";
+    DelayShaper *u = (DelayShaper *)e;
+    return cp_unparse_interval(u->_delay) + "\n";
 }
 
 void
 DelayShaper::add_handlers()
 {
-  add_read_handler("delay", read_param, (void *)0);
+    add_read_handler("delay", read_param, (void *)0);
 }
 
 CLICK_ENDDECLS
--- elements/standard/delayshaper.hh
+++ elements/standard/delayshaper.hh
@@ -1,7 +1,9 @@
+// -*- c-basic-offset: 4 -*-
 #ifndef CLICK_DELAYSHAPER_HH
 #define CLICK_DELAYSHAPER_HH
 #include <click/element.hh>
-#include <click/task.hh>
+#include <click/timer.hh>
+#include <click/notifier.hh>
 CLICK_DECLS
 
 /*
@@ -15,34 +17,49 @@ shapes traffic to meet delay requirements
 
 =d
 
-Pulls packets from input port. queues packet if current timestamp minus packet
-timestamp is less than DELAY seconds (microsecond precision). otherwise return
-packet on output.
+Pulls packets from the single input port. Delays them for at least DELAY
+seconds, with microsecond precision. A packet with timestamp T will be emitted
+no earlier than time (T + DELAY). On output, the packet's timestamp is set to
+the current time.
+
+Differs from DelayUnqueue in that both its input and output are pull. Packets
+being delayed are enqueued until the necessary time has passed. At most one
+packet is enqueued at a time. DelayUnqueue returns null for every pull request
+until the enqueued packet is ready.
 
 SetTimestamp element can be used to stamp the packet.
 
 =a BandwidthShaper, DelayUnqueue, SetTimestamp */
 
-class DelayShaper : public Element { public:
+class DelayShaper : public Element, public ActiveNotifier { public:
   
-  DelayShaper();
-  ~DelayShaper();
+    DelayShaper();
+    ~DelayShaper();
 
-  const char *class_name() const	{ return "DelayShaper"; }
-  const char *processing() const	{ return PULL; }
-  DelayShaper *clone() const		{ return new DelayShaper; }
+    const char *class_name() const	{ return "DelayShaper"; }
+    const char *processing() const	{ return PULL; }
+    void *cast(const char *);
+    DelayShaper *clone() const		{ return new DelayShaper; }
 
-  int configure(Vector<String> &, ErrorHandler *);
-  void cleanup(CleanupStage);
-  void add_handlers();
-  static String read_param(Element *e, void *);
+    int configure(Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void cleanup(CleanupStage);
+    void add_handlers();
+    static String read_param(Element *e, void *);
 
-  Packet* pull(int);
+    Packet *pull(int);
+    void run_timer();
 
- private:
+    // from ActiveNotifier
+    SearchOp notifier_search_op();
+    
+  private:
 
-  uint32_t _delay;
-  Packet *_p;
+    Packet *_p;
+    struct timeval _delay;
+    Timer _timer;
+    NotifierSignal _upstream_signal;
+
 };
 
 CLICK_ENDDECLS
