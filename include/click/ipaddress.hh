702bee7a162e31cf6c0b9166f489ccfe41223777
eddietwo <eddietwo>
2003-07-22 21:25:48 +0000

    change matches_prefix to match John Bicket's intuition: the "a" argument is not necessarily a network address (in otherwords, "a & ~mask" is not necessarily 0

--- include/click/ipaddress.hh
+++ include/click/ipaddress.hh
@@ -1,4 +1,4 @@
-// -*- c-basic-offset: 2; related-file-name: "../../lib/ipaddress.cc" -*-
+// -*- c-basic-offset: 4; related-file-name: "../../lib/ipaddress.cc" -*-
 #ifndef CLICK_IPADDRESS_HH
 #define CLICK_IPADDRESS_HH
 #include <click/string.hh>
@@ -8,133 +8,133 @@ CLICK_DECLS
 
 class IPAddress { public:
   
-  IPAddress()			: _addr(0) { }
-  explicit IPAddress(const unsigned char *);
-  IPAddress(unsigned int);		// network byte order IP address
-  explicit IPAddress(int);		// network byte order IP address
-  explicit IPAddress(unsigned long);	// network byte order IP address
-  explicit IPAddress(long);		// network byte order IP address
-  explicit IPAddress(const String &);	// "18.26.4.99"
-  IPAddress(struct in_addr);
-  static IPAddress make_prefix(int);
+    IPAddress()			: _addr(0) { }
+    explicit IPAddress(const unsigned char *);
+    IPAddress(unsigned int);		// network byte order IP address
+    explicit IPAddress(int);		// network byte order IP address
+    explicit IPAddress(unsigned long);	// network byte order IP address
+    explicit IPAddress(long);		// network byte order IP address
+    explicit IPAddress(const String &);	// "18.26.4.99"
+    IPAddress(struct in_addr);
+    static IPAddress make_prefix(int);
   
-  operator bool() const		{ return _addr != 0; }
-  operator uint32_t() const	{ return _addr; }
-  uint32_t addr() const		{ return _addr; }
+    operator bool() const	{ return _addr != 0; }
+    operator uint32_t() const	{ return _addr; }
+    uint32_t addr() const	{ return _addr; }
   
-  operator struct in_addr() const;
-  struct in_addr in_addr() const;
+    operator struct in_addr() const;
+    struct in_addr in_addr() const;
 
-  unsigned char *data();
-  const unsigned char *data() const;
+    unsigned char *data();
+    const unsigned char *data() const;
   
-  int mask_to_prefix_len() const;
-  bool matches_prefix(IPAddress addr, IPAddress mask) const;
-  bool mask_as_specific(IPAddress) const;
+    int mask_to_prefix_len() const;
+    bool matches_prefix(IPAddress addr, IPAddress mask) const;
+    bool mask_as_specific(IPAddress) const;
 
-  // bool operator==(IPAddress, IPAddress);
-  // bool operator==(IPAddress, uint32_t);
-  // bool operator!=(IPAddress, IPAddress);
-  // bool operator!=(IPAddress, uint32_t);
+    // bool operator==(IPAddress, IPAddress);
+    // bool operator==(IPAddress, uint32_t);
+    // bool operator!=(IPAddress, IPAddress);
+    // bool operator!=(IPAddress, uint32_t);
   
-  // IPAddress operator&(IPAddress, IPAddress);
-  // IPAddress operator|(IPAddress, IPAddress);
-  // IPAddress operator~(IPAddress);
+    // IPAddress operator&(IPAddress, IPAddress);
+    // IPAddress operator|(IPAddress, IPAddress);
+    // IPAddress operator~(IPAddress);
   
-  IPAddress &operator&=(IPAddress);
-  IPAddress &operator|=(IPAddress);
+    IPAddress &operator&=(IPAddress);
+    IPAddress &operator|=(IPAddress);
 
-  String unparse() const;
-  String unparse_mask() const;
-  String unparse_with_mask(IPAddress) const;
+    String unparse() const;
+    String unparse_mask() const;
+    String unparse_with_mask(IPAddress) const;
   
-  operator String() const	{ return unparse(); }
-  String s() const		{ return unparse(); }
+    operator String() const	{ return unparse(); }
+    String s() const		{ return unparse(); }
 
- private:
+  private:
   
-  uint32_t _addr;
+    uint32_t _addr;
 
 };
 
 inline
 IPAddress::IPAddress(unsigned int a)
-  : _addr(a)
+    : _addr(a)
 {
 }
 
 inline
 IPAddress::IPAddress(int a)
-  : _addr(a)
+    : _addr(a)
 {
 }
 
 inline
 IPAddress::IPAddress(unsigned long a)
-  : _addr(a)
+    : _addr(a)
 {
 }
 
 inline
 IPAddress::IPAddress(long a)
-  : _addr(a)
+    : _addr(a)
 {
 }
 
 inline
 IPAddress::IPAddress(struct in_addr ina)
-  : _addr(ina.s_addr)
+    : _addr(ina.s_addr)
 {
 }
 
 inline bool
 operator==(IPAddress a, IPAddress b)
 {
-  return a.addr() == b.addr();
+    return a.addr() == b.addr();
 }
 
 inline bool
 operator==(IPAddress a, uint32_t b)
 {
-  return a.addr() == b;
+    return a.addr() == b;
 }
 
 inline bool
 operator!=(IPAddress a, IPAddress b)
 {
-  return a.addr() != b.addr();
+    return a.addr() != b.addr();
 }
 
 inline bool
 operator!=(IPAddress a, uint32_t b)
 {
-  return a.addr() != b;
+    return a.addr() != b;
 }
 
 inline const unsigned char *
 IPAddress::data() const
 {
-  return reinterpret_cast<const unsigned char *>(&_addr);
+    return reinterpret_cast<const unsigned char *>(&_addr);
 }
 
 inline unsigned char *
 IPAddress::data()
 {
-  return reinterpret_cast<unsigned char *>(&_addr);
+    return reinterpret_cast<unsigned char *>(&_addr);
 }
 
 inline struct in_addr
 IPAddress::in_addr() const
 {
-  struct in_addr ia;
-  ia.s_addr = _addr;
-  return ia;
+    struct in_addr ia;
+    ia.s_addr = _addr;
+    return ia;
 }
 
 inline
 IPAddress::operator struct in_addr() const
 {
-  return in_addr();
+    return in_addr();
 }
 
 class StringAccum;
@@ -143,51 +143,51 @@ StringAccum &operator<<(StringAccum &, IPAddress);
 inline bool
 IPAddress::matches_prefix(IPAddress a, IPAddress mask) const
 {
-  return (addr() & mask.addr()) == a.addr();
+    return ((addr() ^ a.addr()) & mask.addr()) == 0;
 }
 
 inline bool
 IPAddress::mask_as_specific(IPAddress mask) const
 {
-  return (addr() & mask.addr()) == mask.addr();
+    return (addr() & mask.addr()) == mask.addr();
 }
 
 inline IPAddress
 operator&(IPAddress a, IPAddress b)
 {
-  return IPAddress(a.addr() & b.addr());
+    return IPAddress(a.addr() & b.addr());
 }
 
 inline IPAddress &
 IPAddress::operator&=(IPAddress a)
 {
-  _addr &= a._addr;
-  return *this;
+    _addr &= a._addr;
+    return *this;
 }
 
 inline IPAddress
 operator|(IPAddress a, IPAddress b)
 {
-  return IPAddress(a.addr() | b.addr());
+    return IPAddress(a.addr() | b.addr());
 }
 
 inline IPAddress &
 IPAddress::operator|=(IPAddress a)
 {
-  _addr |= a._addr;
-  return *this;
+    _addr |= a._addr;
+    return *this;
 }
 
 inline IPAddress
 operator~(IPAddress a)
 {
-  return IPAddress(~a.addr());
+    return IPAddress(~a.addr());
 }
 
 inline unsigned
 hashcode(IPAddress a)
 {
-  return a.addr();
+    return a.addr();
 }
 
 CLICK_ENDDECLS
--- lib/ipaddress.cc
+++ lib/ipaddress.cc
@@ -1,4 +1,4 @@
-// -*- c-basic-offset: 2; related-file-name: "../include/click/ipaddress.hh" -*-
+// -*- c-basic-offset: 4; related-file-name: "../include/click/ipaddress.hh" -*-
 /*
  * ipaddress.{cc,hh} -- an IP address class. Useful for its hashcode()
  * method
@@ -27,73 +27,73 @@ CLICK_DECLS
 IPAddress::IPAddress(const unsigned char *data)
 {
 #ifdef HAVE_INDIFFERENT_ALIGNMENT
-  _addr = *(reinterpret_cast<const unsigned *>(data));
+    _addr = *(reinterpret_cast<const unsigned *>(data));
 #else
-  memcpy(&_addr, data, 4);
+    memcpy(&_addr, data, 4);
 #endif
 }
 
 IPAddress::IPAddress(const String &str)
 {
-  if (!cp_ip_address(str, this))
-    _addr = 0;
+    if (!cp_ip_address(str, this))
+	_addr = 0;
 }
 
 IPAddress
 IPAddress::make_prefix(int prefix)
 {
-  assert(prefix >= 0 && prefix <= 32);
-  uint32_t umask = 0;
-  if (prefix > 0)
-    umask = 0xFFFFFFFFU << (32 - prefix);
-  return IPAddress(htonl(umask));
+    assert(prefix >= 0 && prefix <= 32);
+    uint32_t umask = 0;
+    if (prefix > 0)
+	umask = 0xFFFFFFFFU << (32 - prefix);
+    return IPAddress(htonl(umask));
 }
 
 int
 IPAddress::mask_to_prefix_len() const
 {
-  uint32_t host_addr = ntohl(_addr);
-  uint32_t umask = 0xFFFFFFFFU;
-  for (int i = 32; i >= 0; i--, umask <<= 1)
-    if (host_addr == umask)
-      return i;
-  return -1;
+    uint32_t host_addr = ntohl(_addr);
+    uint32_t umask = 0xFFFFFFFFU;
+    for (int i = 32; i >= 0; i--, umask <<= 1)
+	if (host_addr == umask)
+	    return i;
+    return -1;
 }
 
 String
 IPAddress::unparse() const
 {
-  const unsigned char *p = data();
-  char buf[20];
-  sprintf(buf, "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
-  return String(buf);
+    const unsigned char *p = data();
+    char buf[20];
+    sprintf(buf, "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
+    return String(buf);
 }
 
 String
 IPAddress::unparse_mask() const
 {
-  int prefix_len = mask_to_prefix_len();
-  if (prefix_len >= 0)
-    return String(prefix_len);
-  else
-    return unparse();
+    int prefix_len = mask_to_prefix_len();
+    if (prefix_len >= 0)
+	return String(prefix_len);
+    else
+	return unparse();
 }
 
 String
 IPAddress::unparse_with_mask(IPAddress mask) const
 {
-  return unparse() + "/" + mask.unparse_mask();
+    return unparse() + "/" + mask.unparse_mask();
 }
 
 StringAccum &
 operator<<(StringAccum &sa, IPAddress ipa)
 {
-  const unsigned char *p = ipa.data();
-  char buf[20];
-  int amt;
-  amt = sprintf(buf, "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
-  sa.append(buf, amt);
-  return sa;
+    const unsigned char *p = ipa.data();
+    char buf[20];
+    int amt;
+    amt = sprintf(buf, "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
+    sa.append(buf, amt);
+    return sa;
 }
 
 CLICK_ENDDECLS
