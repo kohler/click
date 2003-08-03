59b4364e2f9f5de405ce6c949c2f5af73812fadb
jbicket <jbicket>
2003-08-03 17:55:59 +0000

    Added element to detect wifi link failures.
    --jbicket

---
+++ elements/wifi/linkfailuredetection.cc
@@ -0,0 +1,161 @@
+/*
+ * linkfailuredetection.{cc,hh} -- sets wifi txrate annotation on a packet
+ * John Bicket
+ *
+ * Copyright (c) 2003 Massachusetts Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/glue.hh>
+#include <click/packet_anno.hh>
+#include <click/straccum.hh>
+#include <clicknet/ether.h>
+#include <click/router.hh>
+#include "linkfailuredetection.hh"
+CLICK_DECLS
+
+LinkFailureDetection::LinkFailureDetection()
+  : Element(1, 1),
+    _threshold(1)
+{
+  MOD_INC_USE_COUNT;
+  static unsigned char bcast_addr[] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+  _bcast = EtherAddress(bcast_addr);
+}
+
+LinkFailureDetection::~LinkFailureDetection()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+LinkFailureDetection *
+LinkFailureDetection::clone() const
+{
+  return new LinkFailureDetection;
+}
+
+int
+LinkFailureDetection::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+
+  if (conf.size() != 2) {
+    return errh->error("LinkFailureDetection need two args THRESHOLD and HANLDER");
+  }
+  
+  if (!cp_integer(conf[0], &_threshold) || _threshold < 1) {
+    return errh->error("THRESHOLD must be >= 1");
+  }
+
+  if (!cp_handler(conf[1], this, &_handler_e, &_handler_name, errh)) {
+    return errh->error("invalid handler %s", conf[1].cc());
+  }
+  return 0;
+}
+
+
+void 
+LinkFailureDetection::call_handler(EtherAddress dst) {
+  ErrorHandler *errh = ErrorHandler::default_handler();
+  
+  const Router::Handler *h = Router::handler(_handler_e, _handler_name);
+  if (!h) {
+    errh->error("%s: no handler `%s'", id().cc(), 
+		Router::Handler::unparse_name(_handler_e, _handler_name).cc());
+  }
+  
+  if (h->writable()) {
+    ContextErrorHandler cerrh
+      (errh, "In write handler `" + h->unparse_name(_handler_e) + "':");
+    h->call_write(dst.s(), _handler_e, &cerrh);
+  } else {
+    errh->error("%s: no write handler `%s'", 
+		id().cc(), 
+		h->unparse_name(_handler_e).cc());
+  }
+
+}
+Packet *
+LinkFailureDetection::simple_action(Packet *p_in)
+{
+  click_ether *eh = (click_ether *) p_in->data();
+  EtherAddress dst = EtherAddress(eh->ether_dhost);
+
+  if (dst == _bcast) {
+    /* don't record bcast packets */
+    return p_in;
+  }
+  int success = WIFI_TX_SUCCESS_ANNO(p_in);
+
+  DstInfo *nfo = _neighbors.findp(dst);
+  if (!nfo) {
+    DstInfo foo = DstInfo(dst);
+    _neighbors.insert(dst, foo);
+    nfo = _neighbors.findp(dst);
+  }
+  if (success) {
+    nfo->_successive_failures = 0;
+    nfo->_notified = false;
+  } else {
+    nfo->_successive_failures++;
+    if (!nfo->_notified && nfo->_successive_failures >= _threshold) {
+      /* call handler */
+      call_handler(dst);
+      nfo->_notified = true;
+    }
+  }
+  click_gettimeofday(&nfo->_last_received);
+  return p_in;
+}
+String
+LinkFailureDetection::static_print_stats(Element *e, void *)
+{
+  LinkFailureDetection *n = (LinkFailureDetection *) e;
+  return n->print_stats();
+}
+
+String
+LinkFailureDetection::print_stats() 
+{
+  struct timeval now;
+  click_gettimeofday(&now);
+  
+  StringAccum sa;
+  for (NIter iter = _neighbors.begin(); iter; iter++) {
+    DstInfo n = iter.value();
+    struct timeval age = now - n._last_received;
+    sa << n._eth.s().cc() << " ";
+    sa << "successive failures: " << n._successive_failures;
+    if (n._notified) {
+      sa << "*";
+    }
+    sa << "last_received: " << age << "\n";
+  }
+  return sa.take_string();
+}
+void
+LinkFailureDetection::add_handlers()
+{
+  add_default_handlers(true);
+  add_read_handler("stats", static_print_stats, 0);
+
+}
+// generate Vector template instance
+#include <click/bighashmap.cc>
+#if EXPLICIT_TEMPLATE_INSTANCES
+template class BigHashMap<EtherAddress, LinkFailureDetection::DstInfo>;
+#endif
+CLICK_ENDDECLS
+EXPORT_ELEMENT(LinkFailureDetection)
+
---
+++ elements/wifi/linkfailuredetection.hh
@@ -0,0 +1,98 @@
+#ifndef CLICK_LINKFAILUREDETECTION_HH
+#define CLICK_LINKFAILUREDETECTION_HH
+#include <click/element.hh>
+#include <click/etheraddress.hh>
+#include <click/bighashmap.hh>
+#include <click/glue.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * 
+ * LinkFailureDetection(THRESHOLD, HANDLER)
+ * 
+ * Takes ethernet packets and checks if the wifi tx_success
+ * is set to failure.  
+ * after THRESHOLD failures for a particular ethernet destination,
+ * it will call HANDLER.
+ * A successful transmission resets the failure count to 0.
+ * 
+ * Regular Arguments:
+ * =over 8
+ *
+ * =item THRESHOLD
+ *
+ * Unsigned integer. Number of failures before HANLDER is called
+ *
+ *
+ * =back
+ * 
+ * =item  HANDLER
+ *
+ * The write handler to call when a link failure is detected.
+ * This handler will be called with a string that is the ethernet
+ * mac address of the failed destination.
+ *
+ * =s wifi
+ *
+ *
+ */
+
+
+class LinkFailureDetection : public Element { public:
+  
+  LinkFailureDetection();
+  ~LinkFailureDetection();
+  
+  const char *class_name() const		{ return "LinkFailureDetection"; }
+  const char *processing() const		{ return AGNOSTIC; }
+  
+  LinkFailureDetection *clone() const;
+  int configure(Vector<String> &, ErrorHandler *);
+  bool can_live_reconfigure() const		{ return true; }
+
+  Packet *simple_action(Packet *);
+
+  void add_handlers();
+  static String static_print_stats(Element *e, void *);
+  String print_stats();
+
+  int get_rate(EtherAddress);
+ private:
+
+  
+  class DstInfo {
+  public:
+    EtherAddress _eth;
+    int _successive_failures;
+    struct timeval _last_received;
+    bool _notified;
+    DstInfo() { 
+      memset(this, 0, sizeof(*this));
+    }
+
+    DstInfo(EtherAddress eth) { 
+      memset(this, 0, sizeof(*this));
+      _eth = eth;
+      _successive_failures = 0;
+      _notified = false;
+    }
+
+  };
+
+  int _threshold;
+  Element *_handler_e;
+  String _handler_name;
+  typedef BigHashMap<EtherAddress, DstInfo> NeighborTable;
+  typedef NeighborTable::const_iterator NIter;
+
+  class NeighborTable _neighbors;
+  EtherAddress _bcast;
+  int _tau;
+
+
+  void call_handler(EtherAddress);
+};
+
+CLICK_ENDDECLS
+#endif
