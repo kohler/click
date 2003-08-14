ed930b3f5453d94de2dfacf6ff4ddc8350e26424
decouto <decouto>
2003-08-14 15:59:52 +0000

    add up-to-date scripts to generate Grid DSR, DSDV configs.  DSR config is still in flux

---
+++ conf/make-dsdv-config.pl
@@ -0,0 +1,429 @@
+#!/usr/bin/perl -w
+
+# script to generate DSDV configurations for Click.
+# Douglas S. J. De Couto
+# 8 August 2003
+
+use strict;
+
+my $prog = "make-dsdv-config.pl";
+
+# routing parameters, times in milliseconds
+my $rt_timeout = 60000;
+my $rt_period  = 15000;
+my $rt_jitter  =  7500;
+my $rt_min_period = 1000;
+my $rt_hops = 100; 
+my $probe_size = 148; # total bytes, including ethernet header
+
+my @allowable_metrics = ("ETX", "hopcount");
+my $metric = "hopcount";
+
+sub usage() {
+    print "usage: $prog -i IF -a ADDR [OPTIONS]
+  Generate a Click DSDV configuration to run on interface IF with IP address ADDR.
+
+Options:
+  Run in kernel or at userlevel?  Defaults to kernel.
+   -k, --kernel        Run in kernel.  Only works on Linux.
+   -u, --userlevel     Run in userlevel
+
+   -i, --interface IF  Use interface IF.  Required option.
+   -a, --address A     Use IP address A.  Required option.  
+
+   --ether ETH         Use Ethernet address ETH for interface.  If not specified, the address
+                        will be dynamically discovered. 
+
+  Optional DSDV parameters.
+   --timeout T         Expire stale route entries after T milliseconds.  Defaults to $rt_timeout.
+   --period P          Send `full dump' every P milliseconds.  Defaults to $rt_period.
+   --max-jitter J      Jitter route ad timing up to J milliseconds.  Defaults to $rt_jitter.
+   --min-period M      Don't send route ads more than once every M milliseconds.  Defaults to $rt_min_period.
+   --max-hops H        Only propagate routes for H hops.  Defaults to $rt_hops.
+   --metric M          Use metric M.  Allowable values are ";
+    print join ", ", @allowable_metrics;
+print ".  Defaults to $metric.
+   --probe-size N      Use N-byte link probes.  Ignored for hopcount metric.  Defaults to $probe_size.
+   --force-probes      Send link probes even if the metric doesn't require them.
+
+   --click CLICK       Location of userlevel click executable.  If specified, will be used to 
+                        dynamically determine protocol offsets and sizes.
+
+   -h, --help          Print this message and exit.
+";
+}
+
+my @orig_args = @ARGV;
+
+foreach my $arg (@ARGV) { if ($arg eq "-h" || $arg eq "--help") { usage(); exit(0); } }
+
+sub bail($) {
+    print STDERR "$prog: ", shift, "\n";
+    exit 1;
+}
+
+sub get_arg() {
+    my $arg = shift @ARGV;
+    if (!defined($arg)) {
+	print STDERR "$prog: Missing argument parameter\n";
+	usage();
+	exit 1;
+    }
+    return $arg;
+}
+
+my $ifname = "";
+my $click = "";
+my $in_kernel = 1;
+my $addr = "";
+my $eth = "";
+my $force_probes = 0;
+
+while (scalar(@ARGV) > 0) {
+    my $arg = shift @ARGV;
+    if ($arg eq "--help" || $arg eq "-h") {
+	usage();
+	exit 0;
+    }
+    elsif ($arg eq "--click") {
+	$click = get_arg();
+    }
+
+    elsif ($arg eq "--interface" || $arg eq "-i") {
+	$ifname = get_arg();
+    }
+    elsif ($arg eq "--address" || $arg eq "-a") {
+	$addr = get_arg();
+    }
+    elsif ($arg eq "--ether") {
+	$eth = get_arg();
+    }
+    elsif ($arg eq "--kernel" || $arg eq "-k") {
+	$in_kernel = 1;
+    }
+    elsif ($arg eq "--userlevel" || $arg eq "-u") {
+	$in_kernel = 0;
+    }
+    elsif ($arg eq "--force-probes") {
+	$force_probes = 1;
+    }
+    # DSDV params
+    elsif ($arg eq "--timeout") {
+	$rt_timeout = get_arg();
+    }
+    elsif ($arg eq "--period") {
+	$rt_period = get_arg();
+    }
+    elsif ($arg eq "--max-jitter") {
+	$rt_jitter = get_arg();
+    }
+    elsif ($arg eq "--min-period") {
+	$rt_min_period = get_arg();
+    }
+    elsif ($arg eq "--max-hops") {
+	$rt_hops = get_arg();
+    }
+    elsif ($arg eq "--probe-size") {
+	$probe_size = get_arg();
+    }
+    elsif ($arg eq "--metric") {
+	$metric = get_arg();
+    }
+    else {
+	print STDERR "$prog: Unknown argument `$arg'\n";
+	usage();
+	exit 1;
+    }
+}
+
+if ($ifname eq "") { bail("No interface specified, try --help for more info."); }
+if ($addr eq "") { bail("No IP address specified, try --help for more info"); }
+
+if ($addr !~ /\d+\.\d+\.\d+\.\d+/) { 
+    bail("IP address `$addr' has a bad format (should be like `a.b.c.d')"); 
+}
+
+if ($eth ne "" && $eth !~ /([a-f0-9][a-f0-9]?:){5}[a-f0-9][a-f0-9]?/i) {
+    bail("Ethernet address `$eth' has a bad format"); 
+}
+
+sub check_param($$$) {
+    my $n = shift;
+    my $v = shift;
+    my $min = shift;
+    if ($v =~ /(\-?\d+)/) { 
+	$v = $1; 
+	if ($v < $min) {
+	    bail("Argument to --$n must be >= $min");
+	}
+    }
+    else {
+	bail("Argument to --$n must be numeric");
+    }
+}
+
+check_param("timeout", $rt_timeout, 0);
+check_param("period", $rt_period, 0);
+check_param("max-jitter", $rt_jitter, 0);
+check_param("min-period", $rt_min_period, 0);
+check_param("max-hops", $rt_hops, 0);
+check_param("probe-size", $probe_size, 20 + 14); 
+
+my $metric_type_ok = 0;
+foreach my $m (@allowable_metrics) {
+    if ($metric eq $m) { $metric_type_ok = 1; }
+}
+if (!$metric_type_ok) {
+    bail("Unknown argument `$metric' to --metric; should be one of " . join ", ", @allowable_metrics);
+}
+
+
+my %default_proto_info = ("offsetof_grid_hdr_type" => 5,
+			  "sizeof_grid_hdr"        => 60,
+			  "sizeof_grid_nbr_encap"  => 8);
+
+sub get_proto_info($) {
+    my $n = shift;
+    my $ret = "";
+    if ($click ne "") {
+	$ret = `$click -q -e 'g::GridHeaderInfo' -h g.${n}`;
+	if ($ret =~ /(\d+)/) { return $1; }
+	else { bail("Can't get value for $n from Click"); }
+    }
+    else {
+	$ret = $default_proto_info{$n};
+	if (!defined($ret)) { bail("Don't have protocol info for `$n'"); }
+	return $ret;
+    }
+}
+
+# dynamically determine some of the configuration parameters, since
+# they depend on protocol header layouts, which have been, ahem, known
+# to change with time...
+
+my $offset_grid_proto = 14 + get_proto_info("offsetof_grid_hdr_type");
+my $sizeof_grid_hdr = get_proto_info("sizeof_grid_hdr");
+my $sizeof_grid_nbr_encap = get_proto_info("sizeof_grid_nbr_encap");
+my $offset_encap_ip = 14 + $sizeof_grid_hdr + $sizeof_grid_nbr_encap;
+my $tun_input_headroom = $sizeof_grid_hdr + $sizeof_grid_nbr_encap;
+
+my $now = `date`;
+chomp $now;
+
+# setup parts of configuration that differ between userlevel & kernel
+my $addrinfo = "";
+my $tosniffers = "";
+if ($in_kernel) {
+    my $ethspec = ($eth ne "") ? $eth : "$ifname:eth";
+    $addrinfo = "AddressInfo(me $addr $ethspec)";
+    $tosniffers = "ToHostSniffers(\$dev)";
+}
+else {
+    if ($eth eq "") {
+	my $ifconfig_out = `ifconfig $ifname`;
+	if ($ifconfig_out =~ /(\S\S:\S\S:\S\S:\S\S:\S\S:\S\S)/) {
+	    $eth = $1;
+	    print STDERR "$prog: Using ethernet address $eth\n";
+	}
+	else {
+	    bail("Unable to discover ethernet address for interface `$ifname'; try using the --ether option");
+	}
+    }
+    $addrinfo = "AddressInfo(me $addr $eth)";
+    $tosniffers = "Discard";
+}
+
+
+my $probeswitcharg = -1;
+if ($metric eq "ETX" || $force_probes) {
+    $probeswitcharg = 0;
+}
+
+my $metric_el = "";
+if ($metric eq "ETX") {
+    $metric_el = "ETXMetric(ls)";
+}
+elsif ($metric eq "hopcount") {
+    $metric_el = "HopcountMetric";
+}
+else {
+    die;
+}
+
+print "
+// This file automatically generated at $now with the following command:
+// $prog @orig_args
+
+// this configuration performs routing lookup *after* the interface
+// queue, and only works with one interface.
+
+$addrinfo;
+
+elementclass TTLChecker {
+  // expects grid packets with MAC headers --- place on output path to
+  // decrement the IP TTL for next hop and provide traceroute support.  
+  // 
+  // push -> push 
+  // 
+  // output [0] passes through the Grid MAC packets 
+  // 
+  // output [1] produces ICMP error packets to be passed back to IP
+  // routing layer 
+ 
+  input -> cl :: Classifier($offset_grid_proto/03, -);
+  cl [1] -> output; // don't try to dec ttl for non-IP packets...
+
+  cl [0] 
+    -> MarkIPHeader($offset_encap_ip) 
+    -> cl2 :: IPClassifier(src host != me, -);
+
+  cl2 [0]-> dec :: DecIPTTL; // only decrement ttl for packets we don't originate
+  cl2 [1] -> output; 
+
+  dec [0] -> output;
+  dec [1] -> ICMPError(me, 11, 0) -> [1] output;
+};
+
+li :: GridLocationInfo2(0, 0, LOC_GOOD false);
+
+elementclass FixupGridHeaders {
+  \$li | // LocationInfo element
+  input  
+    -> FixSrcLoc(\$li)
+    -> SetGridChecksum
+    -> output;
+};
+
+elementclass ToGridDev {
+  // push, no output
+  \$dev |
+  input -> cl :: Classifier(12/7ffe,
+			    $offset_grid_proto/02,
+			    $offset_grid_proto/03);
+  prio :: PrioSched;
+  cl [0] -> probe_counter :: Counter -> probe_q :: Queue(5) -> [0] prio;
+  cl [1] -> route_counter :: Counter -> route_q :: Queue(5) -> FixupGridHeaders(li) -> [1] prio;
+  cl [2] ->  data_counter :: Counter ->  data_q :: Queue(5)  
+    -> data_counter_out :: Counter
+    -> tr :: TimeRange
+    -> lr :: LookupLocalGridRoute2(me:eth, me:ip, nb) 
+    -> FixupGridHeaders(li)
+    -> [2] prio;
+  prio
+    -> dev_counter :: Counter
+    -> t :: PullTee 
+    -> ToDevice(\$dev);
+  t [1] -> SetTimestamp -> $tosniffers;
+};
+
+elementclass FromGridDev {
+  // push, no input
+  // `Grid' packets on first output
+  // `LinkStat' packets on second output
+  \$dev, \$mac |
+  FromDevice(\$dev, PROMISC false) 
+    -> t :: Tee 
+    -> HostEtherFilter(\$mac, DROP_OWN true)
+    -> cl :: Classifier(12/7fff, 12/7ffe);
+  cl [0]  // `Grid' packets
+    -> ck :: CheckGridHeader
+    -> [0] output;
+  cl [1]  // `LinkStat' packets
+    -> [1] output;
+  t [1] -> $tosniffers;
+  ck [1] -> Print('Bad Grid header received', TIMESTAMP true, NBYTES 166) -> Discard;
+};
+
+elementclass GridLoad {
+  // push, no input 
+
+  // DATASIZE should be the size of the desired UDP packet (including
+  // ethernet, Grid, and IP headers), plus 2 for alignment.  It must
+  // be at least 120.  Most of this is stripped off to be re-used
+  // later, avoiding expensive pushes in the UDP/IP and Grid
+  // encapsulation.
+  src :: InfiniteSource(ACTIVE false, DATASIZE 120)
+    -> Strip(112) // 14 + 60 + 8 + 20 + 8 + 2 = 112 
+                  // (eth + grid + grid_encap + ip + udp + 2 for alignment)
+    -> seq :: IncrementSeqNo(FIRST 0, OFFSET 0)
+    -> SetIPAddress(me)
+    -> StoreIPAddress(4)
+    -> udp :: UDPIPEncap(me, 1111, 0.0.0.0, 8021)
+    -> count :: Counter
+    -> tr :: TimeRange
+    -> output;
+
+  ph :: PokeHandlers;
+}
+  
+ls :: LinkStat(ETH me:eth, SIZE $probe_size);
+metric :: $metric_el;
+
+nb :: DSDVRouteTable($rt_timeout, $rt_period, $rt_jitter, $rt_min_period,
+		     me:eth, me:ip, 
+		     MAX_HOPS $rt_hops,
+                     METRIC metric,
+		     VERBOSE false);
+
+grid_demux :: Classifier($offset_grid_proto/03,    // encapsulated (data) packets
+			 $offset_grid_proto/02);   // route advertisement packets
+
+arp_demux :: Classifier(12/0806 20/0001, // arp queries
+			12/0800);        // IP packets
+
+// handles IP packets with no extra encapsulation
+ip_demux :: IPClassifier(dst host me,    // ip for us
+			 dst net me/24); // ip for Grid network
+
+// handles IP packets with Grid data encapsulation
+grid_data_demux :: IPClassifier(dst host me,    // ip for us
+				dst net me/24); // ip for Grid network
+
+// dev0
+dev0 :: ToGridDev($ifname);
+from_dev0 :: FromGridDev($ifname, me:eth) 
+from_dev0 [0] -> Paint(0) -> grid_demux
+from_dev0 [1] -> Paint(0) -> ls -> probe_switch :: Switch($probeswitcharg) -> dev0;
+
+// support for traceroute
+dec_ip_ttl :: TTLChecker -> dev0;
+dec_ip_ttl [1] -> ip_demux;
+
+grid_demux [0] -> CheckIPHeader( , $offset_encap_ip) -> grid_data_demux;
+grid_demux [1] -> nb -> dev0;
+
+ip_input :: CheckIPHeader -> GetIPAddress(16) -> ip_demux;
+";
+
+# setup physical wireless interfaces and Linux interfaces
+if ($in_kernel) {
+    print "
+to_host :: ToHost(grid0);
+to_host_encap :: EtherEncap(0x0800, 1:1:1:1:1:1, 2:2:2:2:2:2) -> to_host; 
+from_host :: FromHost(grid0, me/24) -> arp_demux -> ARPResponder(0.0.0.0/0 1:1:1:1:1:1) -> to_host;
+arp_demux [1] -> Strip(14) -> ip_input;
+";
+}
+else {
+    print ";
+to_host_encap :: KernelTun(me/24, HEADROOM $tun_input_headroom) -> ip_input;
+
+// not needed in userlevel
+Idle -> arp_demux [0] -> Idle;
+arp_demux [1] -> Idle;
+
+ControlSocket(tcp, 7777);
+";
+}
+
+print "
+ip_demux [0] -> IPPrint('IP loopback') -> to_host_encap;  // loopback packet sent by us
+ip_demux [1] -> GridEncap(me:eth, me:ip) -> dec_ip_ttl;   // forward packet sent by us
+
+grid_data_demux [0] -> Strip($offset_encap_ip) -> to_host_encap;  // receive packet from net for us  
+grid_data_demux [1] -> dec_ip_ttl;                                // forward packet from net for someone else
+
+
+// UDP packet generator
+load :: GridLoad -> ip_input;
+";
---
+++ conf/make-dsr-config.pl
@@ -0,0 +1,399 @@
+#!/usr/bin/perl -w
+
+# script to generate DSR configurations for Click.
+# Daniel Aguayo
+# 2003
+
+use strict;
+
+my $prog = "make-dsr-config.pl";
+
+my $probe_size = 148; # total bytes, including ethernet header
+
+my @allowable_metrics = ("ETX", "hopcount");
+my $metric = "hopcount";
+
+sub usage() {
+    print "usage: $prog -i IF -a ADDR [OPTIONS]
+  Generate a Click DSR configuration to run on interface IF with IP address ADDR.
+
+Options:
+  Run in kernel or at userlevel?  Defaults to kernel.
+   -k, --kernel        Run in kernel.  Only works on Linux.
+   -u, --userlevel     Run in userlevel
+
+   -i, --interface IF  Use interface IF.  Required option.
+   -a, --address A     Use IP address A.  Required option.  
+
+   --ether ETH         Use Ethernet address ETH for interface.  If not specified, 
+                        the address will be dynamically discovered. 
+
+  Optional DSR parameters.
+   --blacklist         Use DSR link blacklist.  Enabled by default.
+   --no-blacklist      Don't use DSR link blacklist.
+   --feedback          Use TX failure feedback to trigger route queries.  
+                        Enabled by default; requires running in kernel.
+   --no-feedback       Don't use TX failure feedback.
+   --metric M          Use metric M.  Allowable values are ";
+    print join ", ", @allowable_metrics;
+print ".  Defaults to $metric.
+   --probe-size N      Use N-byte link probes.  Ignored for hopcount metric.  
+                        Defaults to $probe_size.
+   --force-probes      Send link probes even if the metric doesn't require them.
+
+   -h, --help          Print this message and exit.
+";
+}
+
+my @orig_args = @ARGV;
+
+foreach my $arg (@ARGV) { if ($arg eq "-h" || $arg eq "--help") { usage(); exit(0); } }
+
+sub bail($) {
+    print STDERR "$prog: ", shift, "\n";
+    exit 1;
+}
+
+sub get_arg() {
+    my $arg = shift @ARGV;
+    if (!defined($arg)) {
+	print STDERR "$prog: Missing argument parameter\n";
+	usage();
+	exit 1;
+    }
+    return $arg;
+}
+
+my $ifname = "";
+my $in_kernel = 1;
+my $addr = "";
+my $eth = "";
+my $force_probes = 0;
+my $use_blacklist = 1;
+my $use_feedback = 1;
+
+while (scalar(@ARGV) > 0) {
+    my $arg = shift @ARGV;
+    if ($arg eq "--help" || $arg eq "-h") {
+	usage();
+	exit 0;
+    }
+    elsif ($arg eq "--interface" || $arg eq "-i") {
+	$ifname = get_arg();
+    }
+    elsif ($arg eq "--address" || $arg eq "-a") {
+	$addr = get_arg();
+    }
+    elsif ($arg eq "--ether") {
+	$eth = get_arg();
+    }
+    elsif ($arg eq "--kernel" || $arg eq "-k") {
+	$in_kernel = 1;
+    }
+    elsif ($arg eq "--userlevel" || $arg eq "-u") {
+	$in_kernel = 0;
+    }
+    elsif ($arg eq "--force-probes") {
+	$force_probes = 1;
+    }
+    # DSR params
+    elsif ($arg eq "--blacklist") {
+	$use_blacklist = 1;
+    }
+    elsif ($arg eq "--no-blacklist") {
+	$use_blacklist = 0;
+    }
+    elsif ($arg eq "--feedback") {
+	$use_feedback = 1;
+    }
+    elsif ($arg eq "--no-feedback") {
+	$use_feedback = 0;
+    }
+    elsif ($arg eq "--probe-size") {
+	$probe_size = get_arg();
+    }
+    elsif ($arg eq "--metric") {
+	$metric = get_arg();
+    }
+    else {
+	print STDERR "$prog: Unknown argument `$arg'\n";
+	usage();
+	exit 1;
+    }
+}
+
+if ($ifname eq "") { bail("No interface specified, try --help for more info."); }
+if ($addr eq "") { bail("No IP address specified, try --help for more info"); }
+
+if ($addr !~ /\d+\.\d+\.\d+\.\d+/) { 
+    bail("IP address `$addr' has a bad format (should be like `a.b.c.d')"); 
+}
+
+if ($eth ne "" && $eth !~ /([a-f0-9][a-f0-9]?:){5}[a-f0-9][a-f0-9]?/i) {
+    bail("Ethernet address `$eth' has a bad format"); 
+}
+
+if ($use_feedback && !$in_kernel) {
+    bail("Can't use TX feedback at userlevel");
+}
+
+sub check_param($$$) {
+    my $n = shift;
+    my $v = shift;
+    my $min = shift;
+    if ($v =~ /(\-?\d+)/) { 
+	$v = $1; 
+	if ($v < $min) {
+	    bail("Argument to --$n must be >= $min");
+	}
+    }
+    else {
+	bail("Argument to --$n must be numeric");
+    }
+}
+
+check_param("probe-size", $probe_size, 20 + 14); 
+
+my $metric_type_ok = 0;
+foreach my $m (@allowable_metrics) {
+    if ($metric eq $m) { $metric_type_ok = 1; }
+}
+if (!$metric_type_ok) {
+    bail("Unknown argument `$metric' to --metric; should be one of " . join ", ", @allowable_metrics);
+}
+
+
+my $netmask = 24;
+
+my $datasize = 134;
+my $data = sprintf "'%0${datasize}d'", 0;
+
+print "AddressInfo(me0 $addr);\n";
+if ($in_kernel) {
+    my $ethspec = ($eth ne "") ? $eth : "$ifname:eth";
+    print "AddressInfo(my_ether $ethspec);\n";
+}
+else {
+    if ($eth eq "") {
+	my $ifconfig_out = `ifconfig $ifname`;
+	if ($ifconfig_out =~ /(\S\S:\S\S:\S\S:\S\S:\S\S:\S\S)/) {
+	    $eth = $1;
+	    print STDERR "$prog: Using ethernet address $eth\n";
+	}
+	else {
+	    bail("Unable to discover ethernet address for interface `$ifname'; try using the --ether option");
+	}
+    }
+    print "AddressInfo(my_ether $eth);\n";
+}
+
+print "
+rt_q2 :: SimpleQueue(10); // just ahead of todevice
+
+dsr_ls :: LinkStat(ETH my_ether, IP me0, SIZE $probe_size) -> rt_q0 :: Queue(5);
+
+dsr_lt :: LinkTable(me0);
+";
+
+if ($metric eq "ETX") {
+    print "dsr_rt :: DSRRouteTable(me0, dsr_lt, OUTQUEUE rt_q2, USE_BLACKLIST $use_blacklist, LINKSTAT dsr_ls);\n";
+} 
+elsif ($metric eq "hopcount") {
+    print "dsr_rt :: DSRRouteTable(me0, dsr_lt, OUTQUEUE rt_q2, USE_BLACKLIST $use_blacklist);\n";
+}
+else { 
+    die; 
+}
+
+print "
+dsr_arp :: DSRArpTable(me0, my_ether);
+
+in1 :: FromDevice(${ifname}, PROMISC 0);
+
+dsr_filter :: HostEtherFilter(my_ether,1);
+
+
+in_cl :: Classifier(12/7FFF, -);
+in_cl[0] -> dsr_ls;
+";
+
+if ($in_kernel) {
+    print "
+// [1]dsr_arp takes incoming packets, and passes them through
+// unchanged to output 1, adding entries to an ARP table
+
+// in the kernel we need to explicitly copy packets going to and
+// from the device to hostsniffers.
+
+in1 -> // Print(_in, NBYTES 192) -> 
+          in_t :: Tee(2);
+
+in_t[0] -> in_cl;
+in_t[1] -> ToHostSniffers(${ifname});
+
+in_cl[1] -> dsr_filter; // non-probes -- XXX does this need to come after the hostetherfilter?
+
+// respond to ARP requests from the kernel with a bogus MAC
+from_host :: FromHost(dsr0, me0/$netmask) -> bs_t :: Tee(2);
+
+// packets destined for this host
+dsr_rt[0] -> // Print(_th) -> 
+             CheckIPHeader -> 
+             setup_cl :: IPClassifier(udp port 8022, -);
+
+setup_cl[0] -> Print(setup) -> Discard;
+setup_cl[1] -> EtherEncap(0x0800, my_ether, my_ether) ->
+               ToHost(dsr0);
+
+bs_t[0] -> arp_cl :: Classifier(12/0806, 12/0800);
+bs_t[1] -> Discard; // dodge wmem_alloc bug
+
+arp_cl[0] -> ARPResponder(0.0.0.0/0 1:1:1:1:1:1) -> 
+             ToHost;
+
+// IP packets from the kernel
+arp_cl[1] -> // Print(_fh) ->
+             Strip(14) ->
+             CheckIPHeader ->
+             MarkIPHeader ->
+             GetIPAddress(16) ->
+             [0]dsr_rt;
+
+// (again) in the kernel we need to explicitly copy packets going to
+// and from the device to hostsniffers.
+
+ls_prio :: PrioSched -> out_pt :: PullTee(2);
+
+out_pt[0] -> ToDevice(${ifname});
+out_pt[1] -> ToHostSniffers(${ifname});
+
+";
+} 
+else { 
+    print "
+cs::ControlSocket(TCP, 7777);
+
+// [1]dsr_arp takes incoming packets, and passes them through
+// unchanged to output 1, adding entries to an ARP table
+
+in1 -> in_cl;
+in_cl[1] -> dsr_filter; // non-probes
+
+// packets destined for this host
+
+kt :: KernelTun(me0/$netmask);
+
+kt -> Strip(14) -> icmp_cl :: Classifier(20/0302, -);
+
+icmp_cl[0] -> Discard; // icmp 'protocol not supported'
+icmp_cl[1] -> IPPrint(0rt, CONTENTS true, NBYTES 128) -> 
+[0]dsr_rt[0] -> CheckIPHeader -> 
+                IPPrint(rt0, CONTENTS true, NBYTES 128) -> 
+                setup_cl :: IPClassifier(udp port 8022, -);
+
+setup_cl[0] -> Print(setup) -> Discard;
+setup_cl[1] -> kt;
+
+ls_prio :: PrioSched -> ToDevice(${ifname});
+
+";
+}
+
+
+print "
+    
+dsr_filter[0] -> CheckIPHeader(14) -> [1]dsr_arp;
+
+// drop packets with my ethernet source address
+dsr_filter[1] -> // Print(Mine) ->
+                 Discard;
+
+dsr_arp[1] -> Print(_in, NBYTES 192) ->
+              Strip(14) ->
+              IPPrint ->
+              CheckIPHeader() ->
+              MarkIPHeader() ->
+              GetIPAddress(16) ->
+              DSR_class :: Classifier(09/C8, -);  // DSR packets
+
+DSR_class[0] -> // Print(DSR) ->
+                [1]dsr_rt;
+DSR_class[1] -> // Print(Other) ->
+                Discard;
+
+
+// packets to send out on the wireless; dsr_arp puts on the ethernet
+// header based on its ARP table
+td_prio :: PrioSched;
+dsr_rt[1] -> rt_q1 :: Queue(20) -> [0]td_prio;
+dsr_rt[2] -> rt_q2 -> [1]td_prio;
+td_prio -> [0]dsr_arp;
+
+";
+
+if ($metric eq "ETX" || $force_probes) {
+  print "rt_q0 -> [0]ls_prio;\n";
+} 
+else {
+  print "Idle -> [0]ls_prio;\n";
+  print "rt_q0 -> Discard;\n";
+}
+
+print "
+dsr_arp[0] -> Print(out, NBYTES 192) ->
+              [1]ls_prio;
+
+// packet spewer for throughput tests.
+spew :: RatedSource(ACTIVE false, RATE 700, DATA $data)
+        -> Strip(42) // 14 + 20 + 8 = 42
+                     // (eth + ip + udp)
+        -> seq :: IncrementSeqNo(FIRST 0, OFFSET 0)
+        -> SetIPAddress(me0)
+        -> StoreIPAddress(4)
+        -> udp :: UDPIPEncap(me0, 1111, 0.0.0.0, 8021)
+        -> CheckIPHeader 
+        -> GetIPAddress(16) 
+        -> [0]dsr_rt;
+
+// setup the DSR source route
+setup :: RatedSource(ACTIVE false, RATE 1, DATA 'xxx')
+        -> SetIPAddress(me0)
+        -> StoreIPAddress(4)
+        -> udp2 :: UDPIPEncap(me0, 1111, 0.0.0.0, 8022)
+        -> CheckIPHeader 
+        -> GetIPAddress(16) 
+        -> [0]dsr_rt;
+
+poke :: PokeHandlers(pause,
+		     write setup.active true,
+		     wait 5,
+		     write setup.active false,
+		     wait 5,
+                     write spew.active true,
+		     wait 30,
+                     write spew.active false,
+                     loop);
+
+setup_poke :: PokeHandlers(pause, write setup.active true, wait 5, write setup.active false, loop);
+
+";
+
+if ($in_kernel && $use_feedback) { 
+print "
+// tx feedback
+airo_fb :: AironetTXFeedback -> 
+           MSQueue -> 
+           Unqueue -> 
+           Classifier(! 0/FFFFFFFFFFFF) -> // throw out broadcasts
+           fbh :: FeedbackHandler;
+
+// packets which had a transmission failure
+fbh[0] -> Print(txf) -> Strip(14) ->
+          CheckIPHeader ->
+          [2]dsr_rt;
+";
+} else { 
+  print  "
+Idle -> [2]dsr_rt;
+";
+}
