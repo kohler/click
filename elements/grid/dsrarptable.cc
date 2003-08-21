15965914996f06aaf6b03e9394803b4c46d84579
aguayo <aguayo>
2003-08-21 00:39:52 +0000

    license for DSR

--- elements/grid/dsrarptable.cc
+++ elements/grid/dsrarptable.cc
@@ -1,3 +1,22 @@
+/*
+ * dsrarptable.{cc,hh}
+ * Shweta Bhandare, Sagar Sanghani, Sheetalkumar Doshi, Timothy X Brown
+ * Daniel Aguayo
+ *
+ * Copyright (c) 2003 Massachusetts Institute of Technology
+ * Copyright (c) 2003 University of Colorado at Boulder
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
 #include <click/config.h>
 #include <click/package.hh>
 #include <clicknet/ether.h>
