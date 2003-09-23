8088d8a7f677d39f999594b02ee861b2269a79e0
jbicket <jbicket>
2003-09-23 17:17:32 +0000

    make srscheduler work using yank

--- elements/grid/sr/srscheduler.cc
+++ elements/grid/sr/srscheduler.cc
@@ -42,9 +42,11 @@ CLICK_DECLS
 #endif /* srcr_assert */
 
 
+
 SRScheduler::SRScheduler()
   :  Element(2,2),
-     _debug_token(false)
+     _debug_token(false),
+     _timer(this)
 {
   MOD_INC_USE_COUNT;
 }
@@ -61,14 +63,14 @@ SRScheduler::configure (Vector<String> &conf, ErrorHandler *errh)
   unsigned int hop_duration_ms = 0;
   unsigned int rt_duration_ms = 0;
   unsigned int endpoint_duration_ms = 0;
-  unsigned int real_duration_ms = 0;
+  unsigned int active_duration_ms = 0;
   _threshold = 0;
   ret = cp_va_parse(conf, this, errh,
 		    cpKeywords,
 		    "RT_TIMEOUT", cpUnsigned, "ms", &rt_duration_ms,
 		    "HOP_TIMEOUT", cpUnsigned, "ms", &hop_duration_ms,
 		    "ENDPOINT_TIMEOUT", cpUnsigned, "ms", &endpoint_duration_ms,
-		    "REAL_TIMEOUT", cpUnsigned, "ms", &real_duration_ms,
+		    "ACTIVE_TIMEOUT", cpUnsigned, "ms", &active_duration_ms,
 		    "THRESHOLD", cpInteger, "packets", &_threshold,
 		    "SR", cpElement, "SRForwarder element", &_sr_forwarder,
 		    "DEBUG", cpBool, "Debug", &_debug_token,
@@ -80,8 +82,8 @@ SRScheduler::configure (Vector<String> &conf, ErrorHandler *errh)
     return errh->error("HOP_TIMEOUT not specified");
   if (!endpoint_duration_ms) 
     return errh->error("ENDPOINT_TIMEOUT not specified");
-  if (!real_duration_ms) 
-    return errh->error("REAL_TIMEOUT not specified");
+  if (!active_duration_ms) 
+    return errh->error("ACTIVE_TIMEOUT not specified");
   if (_threshold < 0) 
     return errh->error("HOP_TIMEOUT must be specified and > 0");
   if (!_sr_forwarder) 
@@ -106,10 +108,16 @@ SRScheduler::configure (Vector<String> &conf, ErrorHandler *errh)
   _endpoint_duration.tv_sec = endpoint_duration_ms/1000;
   _endpoint_duration.tv_usec = (endpoint_duration_ms % 1000) * 1000;
 
-  timerclear(&_real_duration);
+  timerclear(&_active_duration);
   /* convehop path_duration from ms to a struct timeval */
-  _real_duration.tv_sec = real_duration_ms/1000;
-  _real_duration.tv_usec = (real_duration_ms % 1000) * 1000;
+  _active_duration.tv_sec = active_duration_ms/1000;
+  _active_duration.tv_usec = (active_duration_ms % 1000) * 1000;
+
+  unsigned int clear_duration_ms = 30 * 1000;
+  timerclear(&_clear_duration);
+  /* convehop path_duration from ms to a struct timeval */
+  _clear_duration.tv_sec = clear_duration_ms/1000;
+  _clear_duration.tv_usec = (clear_duration_ms % 1000) * 1000;
   
   return ret;
 }
@@ -152,9 +160,58 @@ SRScheduler::initialize (ErrorHandler *errh)
   else 
     errh->error("more than 1 upstream q");
 
+
+  _timer.initialize(this);
+  _timer.schedule_now();
   return 0;
 }
 
+void
+SRScheduler::run_timer() 
+{
+  struct timeval now;
+  click_gettimeofday(&now);
+  Vector<Path> to_clear;
+  Vector<Path> not_active;
+
+  for (STIter iter = _schedules.begin(); iter; iter++) {
+    const ScheduleInfo &nfo = iter.value();
+    struct timeval expire;
+    timeradd(&nfo._last_real, &_active_duration, &expire);
+    if (!nfo._active && nfo.clear_timedout()) {
+      to_clear.push_back(nfo._p);
+    } else if (nfo._active && nfo.active_timedout()) {
+      not_active.push_back(nfo._p);
+    }
+  }
+
+  for (int x = 0; x < not_active.size(); x++) {
+    ScheduleInfo *nfo = _schedules.findp(not_active[x]);
+    nfo->_active = false;
+    if (_debug_token) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " mark_inactive " << path_to_string(nfo->_p);
+      click_chatter("%s", sa.take_string().cc());
+    }
+  }
+  for (int x = 0; x < to_clear.size(); x++) {
+    if (_debug_token) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " removing " << path_to_string(to_clear[x]);
+      click_chatter("%s", sa.take_string().cc());
+    }
+    _schedules.remove(to_clear[x]);
+  }
+
+
+  _timer.schedule_after_ms(_active_duration.tv_sec/2);
+}
 SRScheduler::ScheduleInfo *
 SRScheduler::find_nfo(Path p) {
   Path rev = reverse_path(p);
@@ -174,7 +231,7 @@ SRScheduler::find_nfo(Path p) {
 SRScheduler::ScheduleInfo *
 SRScheduler::create_nfo(Path p) 
 {
-  _schedules.insert(p, ScheduleInfo());
+  _schedules.insert(p, ScheduleInfo(this));
   ScheduleInfo *nfo = _schedules.findp(p);
   nfo->_p = p;
   return nfo;
@@ -196,170 +253,314 @@ SRScheduler::push(int port, Packet *p_in)
   for (int x = 0; x < pk->num_hops(); x++) {
     p.push_back(pk->get_hop(x));
   }
-
   ScheduleInfo *nfo = find_nfo(p);
-  if (!nfo) {
-    nfo = create_nfo(p);
+  if (!nfo || !nfo->_active) {
+    if (!nfo) {
+      nfo = create_nfo(p);
+    }
     click_gettimeofday(&nfo->_last_tx);
-  }
-  if (!nfo->_token) {
-    nfo->_token = pk->flag(FLAG_SCHEDULE_TOKEN);
-  }
-  if (pk->flag(FLAG_SCHEDULE_TOKEN)) {
+    nfo->_packets_rx = 0;
+    nfo->_seq = pk->seq();
+    if (nfo->is_endpoint(_sr_forwarder->ip())) {
+      /* make it the other endpoint, the source of this packet */
+      nfo->_towards = p[0];
+    } else {
+      nfo->_towards = p[p.size()-1];
+    }
+
     if (_debug_token) {
-      click_chatter("SRScheduler %s: got token\n",
-		    id().cc());
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " new_path " << path_to_string(p);
+      click_chatter("%s", sa.take_string().cc());
     }
   }
+  
+
+  if (pk->seq() != nfo->_seq && 
+      pk->seq() < nfo->_seq + (nfo->_p.size()-1)) {
+    if (pk->flag(FLAG_SCHEDULE_TOKEN)) {
+      if (_debug_token) {
+	struct timeval now;
+	click_gettimeofday(&now);
+	StringAccum sa;
+	sa << "SRScheduler " << now;
+	sa << " duplicate token";
+	sa << " towards  " << p[p.size()-1];
+	sa << " expected " << nfo->_towards;
+	sa << " pk->seq " << pk->seq();
+	sa << " nfo->seq " << nfo->_seq;
+	click_chatter("%s", sa.take_string().cc());
+      }
+    }
+    if (!pk->flag(FLAG_SCHEDULE_FAKE)) {
+      output(port).push(p_in);
+      return;
+    }
+  } else if (p[p.size()-1] != nfo->_towards && !nfo->is_endpoint(_sr_forwarder->ip())) {
+    if (_debug_token) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " rt_reset_detected";
+      sa << " towards  " << p[p.size()-1];
+      sa << " expected " << nfo->_towards;
+      click_chatter("%s", sa.take_string().cc());
+    }
+      nfo->_towards = p[p.size()-1];
+  }
+
+
+  if (pk->flag(FLAG_SCHEDULE_TOKEN)) {
+    nfo->_token = true;
+    if (nfo->is_endpoint(_sr_forwarder->ip())) {
+      nfo->_seq = pk->seq() + (nfo->_p.size() - 1);
+    } else {
+      nfo->_seq = pk->seq() + 1;
+    }
+  }
+
+  nfo->_packets_rx++;
   click_gettimeofday(&nfo->_last_rx);
-  IPAddress ip = _sr_forwarder->ip();
-  if (pk->flag(FLAG_SCHEDULE_FAKE) && 
-      (p[0] == ip || p[p.size()-1] == ip)) {
+
+  if (nfo->_packets_rx == 1) {
+    nfo->_first_rx = nfo->_last_rx;
+  }
+
+  if (!pk->flag(FLAG_SCHEDULE_FAKE)) {
+    click_gettimeofday(&nfo->_last_real);
+    nfo->_active = true;
+  } else if (nfo->is_endpoint(_sr_forwarder->ip())) {
     /* i'm an endpoint */
     p_in->kill();
     return;
-  } else {
-    click_gettimeofday(&nfo->_last_real);
   }
   output(port).push(p_in);
 }
+
+bool 
+SRScheduler::ready_for(const Packet *p_in, Path match_this_path) {
+  click_ether *eh = (click_ether *) p_in->data();
+  struct srpacket *pk = (struct srpacket *) (eh+1);
+  
+  if (!pk->flag(FLAG_SCHEDULE)) {
+    click_chatter("SRScheduler %s: outgoing schedule flag not set",
+		  id().cc());
+    return true;
+  }
+  
+  Path p;
+  for (int x = 0; x < pk->num_hops(); x++) {
+    p.push_back(pk->get_hop(x));
+  }
+  
+
+  if (match_this_path == p) {
+    return true;
+  } else  if (match_this_path.size()) {
+    return false;
+  }
+  ScheduleInfo *nfo = find_nfo(p);
+  if (!nfo || !nfo->_active) {
+    if (_debug_token) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " create_token: new_path " << path_to_string(p);
+      click_chatter("%s", sa.take_string().cc());
+    }
+    if (!nfo) {
+      nfo = create_nfo(p);
+      nfo->_seq = 0;
+    }
+    nfo->_active = true;
+    nfo->_token = true;
+    nfo->_towards = p[p.size()-1];
+    return true;
+  } 
+  if (nfo->_towards != p[p.size()-1]) {
+    /* this packet is going in the wrong direction */
+    return false;
+  } 
+  if (nfo->_token) {
+    return true;
+  }
+  if (nfo->hop_timedout()) {
+    if (_debug_token) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " create_token: hop_timedout";
+      click_chatter("%s", sa.take_string().cc());
+    }
+    nfo->_token = true;
+    nfo->_towards = p[p.size()-1];
+    return true;
+  } 
+  if (nfo->rt_timedout()) {
+    if (_debug_token) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      sa << "SRScheduler " << now;
+      sa << " create_token: rt_timeout";
+      click_chatter("%s", sa.take_string().cc());
+    }
+    nfo->_token = true;
+    nfo->_towards = p[p.size()-1];
+    return true;
+  } 
+  return false;
+}
+
 Packet *
 SRScheduler::pull(int)
 {
-  Packet *head = _queue1->head();
+  Packet *p_out = _queue1->yank1(sr_filter(this, Path()));
+  IPAddress ip = _sr_forwarder->ip();
 
-  if (head != NULL) {
-
-    click_ether *eh = (click_ether *) head->data();
-    struct srpacket *pk = (struct srpacket *) (eh+1);
-
-    if (!pk->flag(FLAG_SCHEDULE)) {
-      click_chatter("SRScheduler %s: outgoing schedule flag not set",
-		    id().cc());
-      return input(1).pull();
-    }
-
-    Path p;
-    for (int x = 0; x < pk->num_hops(); x++) {
-      p.push_back(pk->get_hop(x));
+  if (!p_out) {
+    Path p = Path();
+    /* find an expired token */
+    struct timeval now;
+    click_gettimeofday(&now);
+    
+    for (STIter iter = _schedules.begin(); iter; iter++) {
+      const ScheduleInfo &nfo = iter.value();
+      /* 
+       * only send a fake if I'm active
+       * an endpoint, and the last rx packet wasn't a fake.
+       */
+      if (nfo._active && 
+	  nfo.endpoint_timedout(ip) && 
+	  timercmp(&nfo._last_rx, &nfo._last_real, ==)) {
+	p = nfo._p;
+	break;
+      }
     }
     
-    ScheduleInfo *nfo = find_nfo(p);
-    if (!nfo) {
-      nfo = create_nfo(p);
-      nfo->_token = true;
-      if (_debug_token) {
-	click_chatter("SRScheduler %s: create_token: new_path\n",
-		      id().cc());
-      }
-    } else {   
-      struct timeval hop_expire;
-      struct timeval rt_expire;
-      struct timeval now;
-      click_gettimeofday(&now);
-
-      timeradd(&nfo->_last_rx, &_hop_duration, &hop_expire);
-      timeradd(&nfo->_last_tx, &_rt_duration, &rt_expire);
-      if (timercmp(&nfo->_last_rx, &nfo->_last_tx, >) && 
-	  timercmp(&hop_expire, &now, <)) {
-	if (_debug_token) {
-	  click_chatter("SRScheduler %s: create_token: hop_timeout\n",
-			id().cc());
-	}
-	nfo->_token = true;
-      } else if (timercmp(&rt_expire, &now, <)) {
-	  if (_debug_token) {
-	    click_chatter("SRScheduler %s: create_token: rt_timeout\n",
-			  id().cc());
-	  }
-	nfo->_token = true;
-      }
+    if (!p.size()) {
+      return (0);
     }
-  
-    if (nfo->_token) {
-      /* send */
-      nfo->_packets_sent++;    
-      if (nfo->_packets_sent == _threshold || _queue1->size() < 2) {
-	pk->set_flag(FLAG_SCHEDULE_TOKEN);
-	if (_debug_token) {
-	  click_chatter("SRScheduler %s: pass_token\n",
-			id().cc());
-	}
-	nfo->_token = false;
-	nfo->_packets_sent = 0;
-      }
-      click_gettimeofday(&nfo->_last_tx);
-      click_gettimeofday(&nfo->_last_real);
-      return input(1).pull();
+
+    if (ip == p[p.size()-1]) {
+      p = reverse_path(p);
     }
-    return (0);
-  } 
-
-  if (_schedules.size() == 0) {
-    return (0);
-  }
-
-  Path p = Path();
-  /* find an expired token */
-  struct timeval now;
-  click_gettimeofday(&now);
-
-  for (STIter iter = _schedules.begin(); iter; iter++) {
-    const ScheduleInfo &nfo = iter.value();
-    struct timeval expire;
-    timeradd(&nfo._last_rx, &_endpoint_duration, &expire);
-    if (nfo._token && timercmp(&expire, &now, <)) {
-      p = nfo._p;
-      break;
+    
+    if (ip != p[0]) {
+      /* i'm an intermediate hop 
+       *  intermediate hop's don't send fake packets
+       */
+      return (0);
     }
+
+    /* fake up a token packet */
+    p_out = _sr_forwarder->encap((0), 0, p, 0);
+    if (!p_out) {
+      return 0;
+    }
+    click_ether *eh = (click_ether *) p_out->data();
+    struct srpacket *pk = (struct srpacket *) (eh+1);
+    pk->set_flag(FLAG_SCHEDULE | FLAG_SCHEDULE_TOKEN | FLAG_SCHEDULE_FAKE);
+
   }
   
-  if (!p.size()) {
-    return (0);
-  }
-  IPAddress ip = _sr_forwarder->ip();
-  if (ip == p[p.size()-1]) {
-    p = reverse_path(p);
+  click_ether *eh = (click_ether *) p_out->data();
+  struct srpacket *pk = (struct srpacket *) (eh+1);
+  Path p;
+  for (int x = 0; x < pk->num_hops(); x++) {
+    p.push_back(pk->get_hop(x));
   }
 
   ScheduleInfo *nfo = find_nfo(p);
-  
-  struct timeval expire;
-  timeradd(&nfo->_last_real, &_real_duration, &expire);
-  if (timercmp(&expire, &now, <)) {
-    _schedules.remove(nfo->_p);
-    if (_debug_token) {
-      click_chatter("SRScheduler %s: delete_token\n",
-		    id().cc());
-    }
-    return (0);
-  }
-  
-  if (ip != p[0]) {
-    /* i'm an intermediate hop */
-    return (0);
-  }
-
-
-
-
-
-  if (_debug_token) {
-    click_chatter("SRScheduler %s: pass_token: fake\n",
-		  id().cc());
-  }
-
-  /* fake up a token packet */
-  Packet *p_out = _sr_forwarder->encap((0), 0, p, 0);
-  click_ether *eh = (click_ether *) p_out->data();
-  struct srpacket *pk = (struct srpacket *) (eh+1);
-  pk->set_flag(FLAG_SCHEDULE | FLAG_SCHEDULE_TOKEN | FLAG_SCHEDULE_FAKE);
 
   /* send */
-  nfo->_packets_sent++;    
-  nfo->_token = false;
-  nfo->_packets_sent = 0;
+  if (!pk->flag(FLAG_SCHEDULE_FAKE)) {
+    click_gettimeofday(&nfo->_last_real);
+  }
+   
+  if (nfo->_packets_tx == _threshold || 
+      !_queue1->yank1_peek(sr_filter(this, p))) {
+    pk->set_flag(FLAG_SCHEDULE_TOKEN);
+  }
+  
+  if(pk->flag(FLAG_SCHEDULE_TOKEN)) {
+    if (_debug_token && 0) {
+      struct timeval now;
+      click_gettimeofday(&now);
+      StringAccum sa;
+      if (pk->flag(FLAG_SCHEDULE_FAKE)) {
+	sa << "SRScheduler " << now << " token_fake";
+      } else {
+	sa << "SRScheduler " << now << " token_pass";
+      }
+      sa << " seq " << nfo->_seq;
+      sa << " towards endpoint " << p[p.size()-1].s();
+      sa << " received " << nfo->_packets_rx;
+      sa << " sent " << nfo->_packets_tx;
+      sa << " time " << now - nfo->_first_rx;
+      click_chatter("%s", sa.take_string().cc());
+    }
+    
+    nfo->_token = false;
+    nfo->_packets_tx = 0;
+    nfo->_packets_rx = 0;
+    nfo->_tokens_passed++;
+    if (!nfo->is_endpoint(ip)) {
+      nfo->_towards = nfo->other_endpoint(nfo->_towards);
+    }
+  } 
+
+  if (!pk->flag(FLAG_SCHEDULE_FAKE)) {
+    nfo->_active = true;
+  }
+  nfo->_packets_tx++;    
+  click_gettimeofday(&nfo->_last_tx);
+  pk->set_seq(nfo->_seq);
+  
+  /* send */
   return p_out;
+}
+  
+
+
+
+String
+SRScheduler::static_print_stats(Element *f, void *)
+{
+  SRScheduler *d = (SRScheduler *) f;
+  return d->print_stats();
+}
+
+String
+SRScheduler::print_stats()
+{
+  StringAccum sa;
+
+  struct timeval now;
+  click_gettimeofday(&now);
+  for (STIter iter = _schedules.begin(); iter; iter++) {
+    const ScheduleInfo &nfo = iter.value();
+    sa << path_to_string(nfo._p) << " :";
+    sa << " active " << nfo._active;
+    sa << " token " << nfo._token;
+    sa << " towards " << nfo._towards;
+    sa << " seq " << nfo._seq;
+    sa << " packets_tx " << nfo._packets_tx;
+    sa << " tokens_passed " << nfo._tokens_passed;
+    sa << " first_rx " << now - nfo._first_rx;
+    sa << " last_rx " << now - nfo._last_rx;
+    sa << " last_tx " << now - nfo._last_tx;
+    sa << " last_real " << now - nfo._last_real;
+    sa << "\n";
+  }
+
+  return sa.take_string();
 }
 
 int
@@ -382,7 +583,7 @@ void
 SRScheduler::clear() 
 {
   
-  struct timeval now;
+    struct timeval now;
   click_gettimeofday(&now);
   Vector<Path> to_clear;
   for (STIter iter = _schedules.begin(); iter; iter++) {
@@ -402,6 +603,7 @@ void
 SRScheduler::add_handlers()
 {
   add_write_handler("clear", static_clear, 0);
+  add_read_handler("stats", static_print_stats, 0);
 }
 
 void
--- elements/grid/sr/srscheduler.hh
+++ elements/grid/sr/srscheduler.hh
@@ -46,38 +46,149 @@ class SRScheduler : public Element {
   static int static_clear(const String &arg, Element *e,
 			  void *, ErrorHandler *errh); 
   void clear();
+  static String static_print_stats(Element *, void *);
+  String print_stats();
 
-
+  void run_timer();
 private:
   class ScheduleInfo {
   public:
+    SRScheduler *_sr;
     Path _p;
     bool _token; // do I have the token?
-    int _packets_sent;
+    int _tokens_passed;
+    IPAddress _towards; // which direction the token is going to
+    uint32_t _seq;
+    bool _active;
+
+    int _packets_rx;
+    int _packets_tx;
+
+    struct timeval _first_rx;
     struct timeval _last_tx;
     struct timeval _last_rx;
     struct timeval _last_real;
     ScheduleInfo() : 
+      _sr(0),
       _p(), 
       _token(false), 
-      _packets_sent(0)
+      _tokens_passed(0),
+      _seq(0),
+      _active(false),
+      _packets_rx(0),
+      _packets_tx(0)
     { }
 
+    ScheduleInfo(SRScheduler *sr) : 
+      _sr(sr),
+      _p(), 
+      _token(false), 
+      _tokens_passed(0),
+      _seq(0),
+      _active(false),
+      _packets_rx(0),
+      _packets_tx(0)
+    { }
+    bool is_endpoint(IPAddress ip) const{
+      if (_p.size() < 1) {
+	return false;
+      }
+      return (_p[0] == ip || _p[_p.size()-1] == ip);
+    }
+
+    IPAddress other_endpoint(IPAddress ip) {
+      return (ip == _p[0]) ? _p[_p.size()-1] : _p[0];
+    }
+
+    bool endpoint_timedout(IPAddress ip) const {
+      if (!_sr) {
+	return false;
+      }
+      if (!is_endpoint(ip) || !_token || !_active) {
+	return false;
+      }
+      struct timeval now;
+      struct timeval expire;
+      click_gettimeofday(&now);
+      timeradd(&_last_rx, &_sr->_endpoint_duration, &expire);
+      return timercmp(&expire, &now, <);
+    }
+
+    bool hop_timedout() const {
+      struct timeval hop_expire;
+      struct timeval now;
+      click_gettimeofday(&now);
+      if (!_sr) {
+	return false;
+      }
+      timeradd(&_last_rx, &_sr->_hop_duration, &hop_expire);
+      return (timercmp(&_last_rx, &_last_tx, >) && 
+	      timercmp(&hop_expire, &now, <));
+    }
+    bool rt_timedout() const {
+      struct timeval expire;
+      struct timeval now;
+      click_gettimeofday(&now);
+      if (!_sr) {
+	return false;
+      }
+      timeradd(&_last_tx, &_sr->_rt_duration, &expire);
+      return timercmp(&expire, &now, <);
+    }
+
+
+    bool active_timedout() const {
+      struct timeval expire;
+      struct timeval now;
+      click_gettimeofday(&now);
+      if (!_sr) {
+	return false;
+      }
+      timeradd(&_last_real, &_sr->_active_duration, &expire);
+      return timercmp(&expire, &now, <);
+    }
+
+    bool clear_timedout() const {
+      struct timeval expire;
+      struct timeval now;
+      click_gettimeofday(&now);
+      if (!_sr || _active) {
+	return false;
+      }
+      timeradd(&_last_real, &_sr->_clear_duration, &expire);
+      return timercmp(&expire, &now, <);
+    }
   };
   
   typedef BigHashMap<Path, ScheduleInfo> ScheduleTable;
   typedef ScheduleTable::const_iterator STIter;
   ScheduleTable _schedules;
 
-  struct timeval _hop_duration;
-  struct timeval _rt_duration;
-  struct timeval _endpoint_duration;
-  struct timeval _real_duration;
+  struct timeval _hop_duration; // 1-hop tieout
+  struct timeval _rt_duration; // round trip timeout
+  struct timeval _endpoint_duration; // endpoint timeout before fake packet
+  struct timeval _active_duration;  // how long to fake packets before inactive
+
+  struct timeval _clear_duration; // how long to keep nfo's around after inactive
 
   class SRForwarder *_sr_forwarder;
 
   Vector<NotifierQueue *> _queues;
   NotifierQueue *_queue1;
+
+  
+
+  struct sr_filter {
+    SRScheduler *s;
+    Path _p;
+    sr_filter(SRScheduler *t, Path p) {
+      s = t;
+      _p = p;
+    }
+    bool operator()(const Packet *p) {
+      return (s) ? s->ready_for(p, _p) : false;
+    }
+  };
 
 
   bool _debug_token;
@@ -90,6 +201,7 @@ private:
   void end_hook();
   void start_path(Path p);
   void end_path(Path p);
+  bool ready_for(const Packet *, Path p);
   static void static_start_hook(Timer *, void *e) { 
     ((SRScheduler *) e)->start_hook(); 
   }
@@ -99,6 +211,7 @@ private:
 
   void srscheduler_assert_(const char *, int, const char *) const;
 
+  Timer _timer;
 };
 
 
