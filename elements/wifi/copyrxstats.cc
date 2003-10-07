85cd2e5b472cf2e4783301f541ea0338bbf4b414
aguayo <aguayo>
2003-10-07 17:19:23 +0000

    cheap hack to facilitate roofnet experiment logging

---
+++ elements/wifi/copyrxstats.cc
@@ -0,0 +1,59 @@
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/glue.hh>
+#include <click/packet_anno.hh>
+#include <click/straccum.hh>
+#include <clicknet/ether.h>
+#include "copyrxstats.hh"
+CLICK_DECLS
+
+CopyRXStats::CopyRXStats()
+  : Element(1, 1)
+{
+  MOD_INC_USE_COUNT;
+}
+
+CopyRXStats::~CopyRXStats()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+CopyRXStats *
+CopyRXStats::clone() const
+{
+  return new CopyRXStats;
+}
+
+int
+CopyRXStats::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+  if (cp_va_parse(conf, this, errh,
+		  cpUnsigned, "write offset", &_offset,
+		  0) < 0) {
+    return -1;
+  }
+  return 0;
+}
+
+Packet *
+CopyRXStats::simple_action(Packet *p_in)
+{
+  uint8_t rate = WIFI_RATE_ANNO(p_in);
+  uint8_t signal = WIFI_SIGNAL_ANNO(p_in);
+  uint8_t noise = WIFI_NOISE_ANNO(p_in);
+  
+  WritablePacket *p = p_in->uniqueify();
+  if (!p) { return 0; }
+  
+  uint8_t *c = p->data();
+  c[offset+0] = rate;
+  c[offset+1] = signal;
+  c[offset+2] = noise;
+  
+  return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(CopyRXStats)
+
---
+++ elements/wifi/copyrxstats.hh
@@ -0,0 +1,42 @@
+#ifndef CLICK_COPYRXSTATS_HH
+#define CLICK_COPYRXSTATS_HH
+#include <click/element.hh>
+#include <click/glue.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * 
+ * CopyRXStats(OFFSET)
+ * 
+ * =s wifi
+ * 
+ * Copies rate, signal and noise annotations into the packet at the
+ * specified offset.  This is a cheap hack to make it simple to run
+ * experiments and accumulate these stats per-packet.
+ *
+ * =over 8
+ *
+ */
+
+class CopyRXStats : public Element { public:
+  
+  CopyRXStats();
+  ~CopyRXStats();
+  
+  const char *class_name() const		{ return "CopyRXStats"; }
+  const char *processing() const		{ return AGNOSTIC; }
+  
+  CopyRXStats *clone() const;
+  int configure(Vector<String> &, ErrorHandler *);
+  bool can_live_reconfigure() const		{ return true; }
+
+  Packet *simple_action(Packet *);
+
+ private:
+  
+  unsigned int _offset;
+};
+
+CLICK_ENDDECLS
+#endif
