700b904b5ea542a1377bb7dd2ad8ca68280f80c5
eddietwo <eddietwo>
2003-12-02 19:22:28 +0000

    add SetTimestampDelta element: set timestamp based on delta from first timestamp seen (useful for testing)

---
+++ elements/analysis/settimestampdelta.cc
@@ -0,0 +1,82 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+/*
+ * settimestampdelta.{cc,hh} -- element observes range of timestamps
+ * Eddie Kohler
+ *
+ * Copyright (c) 2003 International Computer Science Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include <click/error.hh>
+#include "settimestampdelta.hh"
+#include <click/confparse.hh>
+#include <click/straccum.hh>
+CLICK_DECLS
+
+SetTimestampDelta::SetTimestampDelta()
+    : Element(1, 1)
+{
+    MOD_INC_USE_COUNT;
+    timerclear(&_first);
+}
+
+SetTimestampDelta::~SetTimestampDelta()
+{
+    MOD_DEC_USE_COUNT;
+}
+
+Packet *
+SetTimestampDelta::simple_action(Packet *p)
+{
+    struct timeval &tv = p->timestamp_anno();
+    if (timerisset(&tv)) {
+	if (!timerisset(&_first))
+	    _first = tv;
+	tv -= _first;
+    }
+    return p;
+}
+
+String
+SetTimestampDelta::read_handler(Element *e, void *thunk)
+{
+    SetTimestampDelta *td = static_cast<SetTimestampDelta *>(e);
+    StringAccum sa;
+    switch ((intptr_t)thunk) {
+      case 0: {
+	  StringAccum sa;
+	  sa << td->_first << '\n';
+	  return sa.take_string();
+      }
+      default:
+	return "<error>\n";
+    }
+}
+
+int
+SetTimestampDelta::write_handler(const String &, Element *e, void *, ErrorHandler *)
+{
+    SetTimestampDelta *td = static_cast<SetTimestampDelta *>(e);
+    timerclear(&td->_first);
+    return 0;
+}
+
+void
+SetTimestampDelta::add_handlers()
+{
+    add_read_handler("first", read_handler, (void *)0);
+    add_write_handler("reset", write_handler, (void *)0);
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(SetTimestampDelta)
---
+++ elements/analysis/settimestampdelta.hh
@@ -0,0 +1,59 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+#ifndef CLICK_SETTIMESTAMPDELTA_HH
+#define CLICK_SETTIMESTAMPDELTA_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+
+SetTimestampDelta()
+
+=s measurement, analysis
+
+replace packet timestamps with deltas
+
+=d
+
+SetTimestampDelta passes packets through, but nonzero packet timestamps are
+replaced with deltas relative to the first nonzero timestamp encountered.
+
+Keyword arguments are:
+
+=h first read-only
+
+Returns the earliest nonzero timestamp observed, if any.
+
+=h reset write-only
+
+Clears the timestamp record. Future packet timestamps will be measured
+relative to the next nonzero timestamp encountered.
+
+=a
+
+TimeFilter */
+
+class SetTimestampDelta : public Element { public:
+
+    SetTimestampDelta();
+    ~SetTimestampDelta();
+
+    const char *class_name() const	{ return "SetTimestampDelta"; }
+    const char *processing() const	{ return AGNOSTIC; }
+    SetTimestampDelta *clone() const	{ return new SetTimestampDelta; }
+
+    void add_handlers();
+
+    Packet *simple_action(Packet *);
+    
+  private:
+
+    struct timeval _first;
+
+    static String read_handler(Element *, void *);
+    static int write_handler(const String &, Element *, void *, ErrorHandler*);
+    
+};
+
+CLICK_ENDDECLS
+#endif
