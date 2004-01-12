d83aebb43773d3c37d8f26c0430a0a6ce9bcc729
jbicket <jbicket>
2004-01-12 03:33:04 +0000

    pflood probabilistically forwards a broadcast packet that it receives. you specify p.

---
+++ elements/grid/sr/pflood.cc
@@ -0,0 +1,318 @@
+/*
+ * PFlood.{cc,hh} -- DSR implementation
+ * John Bicket
+ *
+ * Copyright (c) 1999-2001 Massachuspfloods Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "pflood.hh"
+#include <click/ipaddress.hh>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/glue.hh>
+#include <click/straccum.hh>
+#include <clicknet/ether.h>
+#include "srpacket.hh"
+CLICK_DECLS
+
+PFlood::PFlood()
+  :  Element(2,2),
+     _en(),
+     _et(0),
+     _packets_originated(0),
+     _packets_tx(0),
+     _packets_rx(0)
+{
+  MOD_INC_USE_COUNT;
+
+  static unsigned char bcast_addr[] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+  _bcast = EtherAddress(bcast_addr);
+}
+
+PFlood::~PFlood()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+int
+PFlood::configure (Vector<String> &conf, ErrorHandler *errh)
+{
+  int ret;
+  _debug = false;
+  _p = 0;
+  _max_delay_ms = 750;
+  _history = 100;
+  ret = cp_va_parse(conf, this, errh,
+                    cpKeywords,
+		    "ETHTYPE", cpUnsigned, "Ethernet encapsulation type", &_et,
+                    "IP", cpIPAddress, "IP address", &_ip,
+                    "BCAST_IP", cpIPAddress, "IP address", &_bcast_ip,
+		    "ETH", cpEtherAddress, "EtherAddress", &_en,
+		    "P", cpInteger, "Count", &_p,
+		    "MAX_DELAY", cpUnsigned, "Max Delay (ms)", &_max_delay_ms,
+		    /* below not required */
+		    "DEBUG", cpBool, "Debug", &_debug,
+		    "HISTORY", cpUnsigned, "history", &_history,
+                    0);
+
+  if (!_et) 
+    return errh->error("ETHTYPE not specified");
+  if (!_ip) 
+    return errh->error("IP not specified");
+  if (!_bcast_ip) 
+    return errh->error("BCAST_IP not specified");
+  if (!_en) 
+    return errh->error("ETH not specified");
+  if (_p < 0 || _p > 100)
+    return errh->error ("P must be between 0 and 100");
+
+
+  return ret;
+}
+
+PFlood *
+PFlood::clone () const
+{
+  return new PFlood;
+}
+
+int
+PFlood::initialize (ErrorHandler *)
+{
+  return 0;
+}
+
+void
+PFlood::forward(Broadcast *bcast) {
+  Packet *p_in = bcast->_p;
+  click_ether *eh_in = (click_ether *) p_in->data();
+  struct srpacket *pk_in = (struct srpacket *) (eh_in+1);
+
+  int hops = 1;
+  int len = 0;
+  if (bcast->_originated) {
+    hops = 1;
+    len = srpacket::len_with_data(hops, p_in->length());
+  } else {
+    hops = pk_in->num_hops() + 1;
+    len = srpacket::len_with_data(hops, pk_in->data_len());
+  }
+
+  WritablePacket *p = Packet::make(len + sizeof(click_ether));
+  if (p == 0)
+    return;
+
+  click_ether *eh = (click_ether *) p->data();
+  struct srpacket *pk = (struct srpacket *) (eh+1);
+
+  memset(pk, '\0', len);
+
+  pk->_version = _sr_version;
+  pk->_type = PT_DATA;
+  pk->_flags = 0;
+  pk->_qdst = _bcast_ip;
+  pk->set_num_hops(hops);
+  for (int x = 0; x < hops - 1; x++) {
+    pk->set_hop(x, pk_in->get_hop(x));
+  }
+  pk->set_hop(hops - 1,_ip);
+  pk->set_next(hops);
+  pk->set_seq(bcast->_seq);
+  if (bcast->_originated) {
+    memcpy(pk->data(), p_in->data(), p_in->length());
+    pk->set_data_len(p_in->length());
+
+  } else {
+    memcpy(pk->data(), pk_in->data(), pk_in->data_len());
+    pk->set_data_len(pk_in->data_len());
+  }
+  bcast->_forwarded = true;
+  _packets_tx++;
+  
+  eh->ether_type = htons(_et);
+  memcpy(eh->ether_shost, _en.data(), 6);
+  memset(eh->ether_dhost, 0xff, 6);
+  output(0).push(p);
+
+}
+
+void
+PFlood::forward_hook() 
+{
+  struct timeval now;
+  click_gettimeofday(&now);
+  for (int x = 0; x < _packets.size(); x++) {
+    if (timercmp(&_packets[x]._to_send, &now, <=)) {
+      /* this timer has expired */
+      if (!_packets[x]._forwarded) {
+	/* we haven't forwarded this packet yet */
+	if (random() % 100 <= _p) {
+	  forward(&_packets[x]);
+	} else {
+	  _packets[x]._forwarded = true;
+	}
+      }
+    }
+  }
+}
+
+
+void
+PFlood::trim_packets() {
+  /* only keep track of the last _max_packets */
+  while ((_packets.size() > _history)) {
+    /* unschedule and remove packet*/
+    if (_debug) {
+      click_chatter("%{element} removing seq %d\n",
+		    this,
+		    _packets[0]._seq);
+    }
+    _packets[0].del_timer();
+    if (_packets[0]._p) {
+      _packets[0]._p->kill();
+    }
+    _packets.pop_front();
+  }
+}
+void
+PFlood::push(int port, Packet *p_in)
+{
+  struct timeval now;
+  click_gettimeofday(&now);
+  
+  if (port == 1) {
+    _packets_originated++;
+    /* from me */
+    int index = _packets.size();
+    _packets.push_back(Broadcast());
+    _packets[index]._seq = random();
+    _packets[index]._originated = true;
+    _packets[index]._p = p_in;
+    _packets[index]._num_rx = 0;
+    _packets[index]._first_rx = now;
+    _packets[index]._forwarded = false;
+    _packets[index].t = NULL;
+    _packets[index]._to_send = now;
+    forward(&_packets[index]);
+  } else {
+    _packets_rx++;
+
+    click_ether *eh = (click_ether *) p_in->data();
+    struct srpacket *pk = (struct srpacket *) (eh+1);
+    
+    uint32_t seq = pk->seq();
+    
+    int index = -1;
+    for (int x = 0; x < _packets.size(); x++) {
+      if (_packets[x]._seq == seq) {
+	index = x;
+	break;
+      }
+    }
+
+    if (index == -1) {
+      /* haven't seen this packet before */
+      index = _packets.size();
+      _packets.push_back(Broadcast());
+      _packets[index]._seq = seq;
+      _packets[index]._originated = false;
+      _packets[index]._p = p_in;
+      _packets[index]._num_rx = 1;
+      _packets[index]._first_rx = now;
+      _packets[index]._forwarded = false;
+      _packets[index].t = NULL;
+
+      /* schedule timer */
+      int delay_time = (random() % _max_delay_ms) + 1;
+      sr_assert(delay_time > 0);
+      
+      struct timeval delay;
+      delay.tv_sec = 0;
+      delay.tv_usec = delay_time*1000;
+      timeradd(&now, &delay, &_packets[index]._to_send);
+      _packets[index].t = new Timer(static_forward_hook, (void *) this);
+      _packets[index].t->initialize(this);
+      _packets[index].t->schedule_at(_packets[index]._to_send);
+
+      /* finally, clone the packet and push it out */
+      Packet *p_out = p_in->clone();
+      output(1).push(p_out);
+    } else {
+      /* we've seen this packet before */
+      p_in->kill();
+      _packets[index]._num_rx++;
+    }
+  }
+
+  trim_packets();
+}
+
+
+String
+PFlood::static_print_stats(Element *f, void *)
+{
+  PFlood *d = (PFlood *) f;
+  return d->print_stats();
+}
+
+String
+PFlood::print_stats()
+{
+  StringAccum sa;
+
+  sa << "originated " << _packets_originated << "\n";
+  sa << "tx " << _packets_tx << "\n";
+  sa << "rx " << _packets_rx << "\n";
+  return sa.take_string();
+}
+
+int
+PFlood::static_write_debug(const String &arg, Element *e,
+			void *, ErrorHandler *errh) 
+{
+  PFlood *n = (PFlood *) e;
+  bool b;
+
+  if (!cp_bool(arg, &b))
+    return errh->error("`debug' must be a boolean");
+
+  n->_debug = b;
+  return 0;
+}
+String
+PFlood::static_print_debug(Element *f, void *)
+{
+  StringAccum sa;
+  PFlood *d = (PFlood *) f;
+  sa << d->_debug << "\n";
+  return sa.take_string();
+}
+void
+PFlood::add_handlers()
+{
+  add_read_handler("stats", static_print_stats, 0);
+  add_read_handler("debug", static_print_debug, 0);
+
+  add_write_handler("debug", static_write_debug, 0);
+}
+
+// generate Vector template instance
+#include <click/vector.cc>
+#include <click/dequeue.cc>
+#if EXPLICIT_TEMPLATE_INSTANCES
+template class Vector<PFlood::Broadcast>;
+#endif
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(PFlood)
---
+++ elements/grid/sr/pflood.hh
@@ -0,0 +1,131 @@
+#ifndef CLICK_PFLOOD_HH
+#define CLICK_PFLOOD_HH
+#include <click/element.hh>
+#include <click/glue.hh>
+#include <click/timer.hh>
+#include <click/ipaddress.hh>
+#include <click/etheraddress.hh>
+#include <click/vector.hh>
+#include <click/hashmap.hh>
+#include <click/dequeue.hh>
+#include <elements/grid/linktable.hh>
+#include <elements/grid/arptable.hh>
+#include <elements/grid/sr/path.hh>
+#include "pflood.hh"
+#include <elements/wifi/rxstats.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * PFlood(ETHTYPE eth, IP ip, BCAST_IP ip, ETH eth, P int, 
+ *              MAX_DELAY int, 
+ *              [DEBUG bool], [HISTORY int]);
+ * =d
+ * DSR-inspired end-to-end ad-hoc routing protocol.
+ * Input 0: ethernet packets from dev
+ * Input 1: ip packets from higher layer 
+ * Output 0: ethernet packets to device (protocol)
+ * Output 1: ip packets to higher layer
+ *
+ * =over 8
+ *
+ * =item P
+ * 
+ * value of 0 to 100 where P is the probability that this
+ * element will forward any particular packet. It will only
+ * forward it once.
+ * 
+ * 
+ * =item MAX_DELAY
+ *
+ * max time to wait after 1st packet rx to forward packet. default is 750
+ *
+ * =item HISTORY
+ *
+ * number of sequence numbers to remember. default is 100
+ *
+ *
+ *
+ *
+ */
+
+
+class PFlood : public Element {
+ public:
+  
+  PFlood();
+  ~PFlood();
+  
+  const char *class_name() const		{ return "PFlood"; }
+  const char *processing() const		{ return PUSH; }
+  int initialize(ErrorHandler *);
+  PFlood *clone() const;
+  int configure(Vector<String> &conf, ErrorHandler *errh);
+
+
+  static String static_print_debug(Element *f, void *);
+  static int static_write_debug(const String &arg, Element *e,
+				void *, ErrorHandler *errh); 
+
+  static String static_print_stats(Element *e, void *);
+  String print_stats();
+
+  void push(int, Packet *);
+
+  void add_handlers();
+private:
+
+
+  class Broadcast {
+  public:
+    uint32_t _seq;
+    bool _originated; /* this node started the bcast */
+    Packet *_p;
+    int _num_rx;
+    struct timeval _first_rx;
+    bool _forwarded;
+    Timer *t;
+    struct timeval _to_send;
+
+    void del_timer() {
+      if (t) {
+	t->unschedule();
+	delete t;
+	t = NULL;
+      }
+    }
+  };
+
+
+  DEQueue<Broadcast> _packets;
+
+  IPAddress _ip;    // My IP address.
+  EtherAddress _en; // My ethernet address.
+  uint32_t _et;     // This protocol's ethertype
+  IPAddress _bcast_ip;
+
+  EtherAddress _bcast;
+
+  bool _debug;
+
+
+  int _packets_originated;
+  int _packets_tx;
+  int _packets_rx;
+
+  int _p;
+  int _max_delay_ms;
+
+  int _history;
+
+  void forward(Broadcast *bcast);
+  void forward_hook();
+  void trim_packets();
+  static void static_forward_hook(Timer *, void *e) { 
+    ((PFlood *) e)->forward_hook(); 
+  }
+};
+
+
+CLICK_ENDDECLS
+#endif
