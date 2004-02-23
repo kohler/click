bf4580eed1209d0d90f3be0f2ce3a164fc4de595
max <max>
2004-02-23 06:08:56 +0000

    SourceIPHashMapper ---  load balancing mapper; performs mappings based on the hash of the source IP, but uses consistent hashing so that mappings are not upset (too terribly) when nodes are removed from the cluster.

---
+++ elements/ip/siphmapper.cc
@@ -0,0 +1,151 @@
+/*
+ * sourceipmapper.{cc,hh} -- source IP mapper (using consistent hashing)
+ *
+ * $Id: siphmapper.cc,v 1.1 2004/02/23 06:08:56 max Exp $
+ *
+ */
+
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/glue.hh>
+#include "siphmapper.hh"
+CLICK_DECLS
+
+SourceIPHashMapper::SourceIPHashMapper()
+  : _hasher (NULL)
+{
+  MOD_INC_USE_COUNT;
+}
+
+SourceIPHashMapper::~SourceIPHashMapper()
+{
+  MOD_DEC_USE_COUNT;
+}
+
+void *
+SourceIPHashMapper::cast(const char *name)
+{
+  if (name && strcmp("SourceIPHashMapper", name) == 0)
+    return (Element *)this;
+  else if (name && strcmp("IPMapper", name) == 0)
+    return (IPMapper *)this;
+  else
+    return 0;
+}
+
+int
+SourceIPHashMapper::parse_server (const String &conf, IPRw::Pattern **pstore,
+			      int *fport_store, int *rport_store,
+			      int *id_store, Element *e, 
+			      ErrorHandler *errh)
+{
+  Vector<String> words;
+  cp_spacevec(conf, words);
+  int32_t id;
+
+  if (words.size () <= 1
+      || !cp_integer(words[words.size() - 1], &id)
+      || id < 0)
+    return errh->error("bad server ID in pattern spec");
+  words.resize(words.size() - 1);
+  *id_store = id;
+  return IPRw::Pattern::parse_with_ports (cp_unspacevec(words), pstore, 
+					  fport_store,
+					  rport_store, e, errh);
+}
+
+int
+SourceIPHashMapper::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+  if (conf.size () == 0)
+    return errh->error ("no hash seed given");
+  else if (conf.size() == 1)
+    return errh->error("no patterns given");
+  else if (conf.size() == 2)
+    errh->warning("only one pattern given");
+
+  int before = errh->nerrors();
+
+  int nnodes;
+  int32_t seed;
+  Vector<String> params;
+  cp_spacevec (conf[0], params);
+  if (params.size () != 2)
+    return errh->error("requires 2 config params: numnodes seed");
+  if (!cp_integer(params[0], &nnodes) || nnodes <= 0)
+    return errh->error("number of nodes must be an integer");
+  if (!cp_integer(params[1], &seed))
+    return errh->error("hash seed must be an integer");
+  
+  int idp = 0;
+  unsigned short *ids = new unsigned short[conf.size ()];
+  
+  for (int i = 1; i < conf.size(); i++) {
+    IPRw::Pattern *p;
+    int f, r, id;
+    if (parse_server (conf[i], &p, &f, &r, &id, this, errh) >= 0) {
+      p->use();
+      _patterns.push_back(p);
+      _forward_outputs.push_back(f);
+      _reverse_outputs.push_back(r);
+      ids[idp++] = id;
+    }
+  }
+
+  if (_hasher) 
+    delete (_hasher);
+  _hasher = new chash_t<unsigned int> (idp, ids, nnodes, seed);
+
+  delete [] ids;
+  return (errh->nerrors() == before ? 0 : -1);
+}
+
+void
+SourceIPHashMapper::cleanup(CleanupStage)
+{
+  for (int i = 0; i < _patterns.size(); i++)
+    _patterns[i]->unuse();
+  delete _hasher;
+}
+
+void
+SourceIPHashMapper::notify_rewriter(IPRw *rw, ErrorHandler *errh)
+{
+  int no = rw->noutputs();
+  for (int i = 0; i < _patterns.size(); i++) {
+    if (_forward_outputs[i] >= no || _reverse_outputs[i] >= no)
+      errh->error("port in `%s' out of range for `%s'", declaration().cc(), rw->declaration().cc());
+    rw->notify_pattern(_patterns[i], errh);
+  }
+}
+
+IPRw::Mapping *
+SourceIPHashMapper::get_map(IPRw *rw, int ip_p, const IPFlowID &flow, 
+			    Packet *p)
+{
+  const click_ip *iph = p->ip_header();
+  const struct in_addr ipsrc = iph->ip_src;
+  unsigned int tmp, t2;
+  memcpy (&tmp, &ipsrc, sizeof (tmp));
+  t2 = tmp & 0xff;
+
+  // make the lower bits have some more impact so that adjacent
+  // IPs can be hashed to different servers.
+  // note that this really isn't necessary for i386 alignment...
+  tmp *= ((t2 << 24) | 0x1);
+  
+  int v = _hasher->hash2ind (tmp);
+  IPRw::Pattern *pat = _patterns[v];
+  int fport = _forward_outputs[v];
+  int rport = _reverse_outputs[v];
+
+  // debug code
+  //click_chatter ("%p -> %d", (void *)tmp, v);
+
+  return (rw->apply_pattern(pat, ip_p, flow, fport, rport));
+}
+
+CLICK_ENDDECLS
+ELEMENT_REQUIRES(IPRw)
+EXPORT_ELEMENT(SourceIPHashMapper)
---
+++ elements/ip/siphmapper.hh
@@ -0,0 +1,222 @@
+#ifndef CLICK_SOURCEIPMAPPER_HH
+#define CLICK_SOURCEIPMAPPER_HH
+
+#include <click/element.hh>
+#include <click/ipflowid.hh>
+#include <clicknet/ip.h>
+#include "elements/ip/iprw.hh"
+#include <click/glue.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * SourceIPHashMapper(NNODES SEED, PATTERN1, ..., PATTERNn)
+ * =s TCP
+ * Source IP Hash mapper for IPRewriter(n)
+ * =io
+ * None
+ * =d
+ *
+ * Works in tandem with IPRewriter to provide source IP-based rewriting. 
+ * This is useful, for example, in load-balancing applications. Implements the
+ * IPMapper interface.
+ *
+ * Like RoundRobinIPMapper, but also uses consistent hashing to map
+ * map elements by source IP to the same node in the cluster, even
+ * if nodes are added or removed.
+ *
+ *
+ * =a IPRewriter, TCPRewriter, IPRewriterPatterns, RoundRobinIPMapper 
+ */
+
+//
+// $Id: siphmapper.hh,v 1.1 2004/02/23 06:08:56 max Exp $
+//
+
+//
+// JV-tree.  A dirt-simple binary tree for use in the consistent
+// hash table.  Is balanced, but static.
+//
+template<class C, class K, K C::*key> int
+jvcomp (const void *a, const void *b)
+{
+  const C *ca = static_cast<const C *> (a);
+  const C *cb = static_cast<const C *> (b);
+  return int (ca->*key - cb->*key);
+}
+
+template<class C, class K, K C::*key>
+class jvtree_t
+{
+public:
+  jvtree_t (int sz, C *arr)
+    : num (sz), tree (new C[sz])
+  {
+    click_qsort ((void *)arr, sz, sizeof (C), jvcomp<C,K,key>);
+
+    int n = next2pow (sz);
+    int h = n - 1;
+
+    // f is the "first odd" that we start to map in our translation
+    // scheme.
+    int f = 2*((sz+1)/2) - 2*((h - sz)/ 2) + 1;
+
+    int s,d;
+    d = n;
+    s = d >> 1;
+    
+    int to = 0;
+    while (s > 0 && to < sz) {
+      for (int x = s; x < n && to < sz; x += d) {
+	int from = (x < f) ? x : (x + f - 1)/2;
+	tree[to++] = arr[--from];
+      }
+      d = s;
+      s = s >> 1;
+    }
+  }
+
+  // should really be static, but if we keep it here, then everything
+  // fits into the .h file
+  int next2pow (int i)
+  {
+    int tmp = i;
+    int p = 1;
+    while ((tmp = (tmp >> 1)) > 0) p++;
+    int r = 1;
+    for (int i = 0; i < p; i++) r = r << 1;
+    return r;
+  }
+  
+  ~jvtree_t () { delete [] tree; }
+
+
+  C *search (K k) const
+  {
+    int i = 0;
+    C *ret = NULL;
+    C *curr;
+    K tkv;
+    while (i < num) {
+      curr = tree + i;
+      tkv = curr->*key;
+      if (k == tkv) { 
+	return curr;
+      } else if (k > tkv) {
+	i = 2*i + 2;
+      } else {
+	i = 2*i + 1;
+	ret = curr;
+      }
+    }
+    return ret ? ret : tree;
+  }
+private:
+  int num;
+  C *tree;
+};
+
+template<class K>
+class chash_node_t {
+public:
+  chash_node_t () {}
+  chash_node_t (K k, unsigned short v) : key (k), val (v), index (0) {}
+  K key;
+  unsigned short val;
+  unsigned short index;
+};
+
+//
+// consistent hash table, based on JV tree.
+//
+template<class K>
+class chash_t {
+public:
+  chash_t (size_t ns, unsigned short *ids, size_t nn, int seed = 0x1) 
+    : num_servers (ns), num_nodes (nn)
+  {
+    srandom (seed);
+
+    int max_servers = -1;
+    for (size_t i = 0; i < num_servers; i++) 
+      if (ids[i] > max_servers)
+	max_servers = ids[i];
+    max_servers++;
+
+    // temporary map for boolean server lookup
+    char *servmap = new char[max_servers];
+    memset (servmap, 0, max_servers);
+    for (size_t i = 0; i < num_servers; i++)
+      servmap[ids[i]] = 1;
+
+    int n = num_servers * num_nodes;
+    chash_node_t<K> *in = new chash_node_t<K> [n];
+    int p = 0;
+    unsigned short index = -1;
+    for (unsigned short i = 0; i < max_servers; i++) {
+      bool inc = false;
+      for (unsigned int j = 0; j < num_nodes; j++) {
+	unsigned int tmp = random ();
+	if (servmap[i]) {
+	  if (!inc) {
+	    inc = true;
+	    index ++;
+	  }
+	  K ktmp = static_cast<K> (tmp);
+	  in[p].key = ktmp;
+	  in[p].val = i;
+	  in[p++].index = index;
+	}
+      }
+    }
+    tree = new jvtree_t<chash_node_t<K>, K, &chash_node_t<K>::key> (n, in);
+    
+    delete [] in;
+    delete [] servmap;
+  }
+
+  unsigned short hash (K k) const { return tree->search (k)->val; }
+  unsigned short hash2ind (K k) const { return tree->search (k)->index; }
+
+  ~chash_t ()
+  {
+    delete tree;
+  }
+
+private:
+  size_t num_servers, num_nodes;
+  jvtree_t<chash_node_t<K>, K, &chash_node_t<K>::key> *tree;
+};
+
+
+class SourceIPHashMapper : public Element, public IPMapper { public:
+
+  SourceIPHashMapper();
+  ~SourceIPHashMapper();
+
+  const char *class_name() const	{ return "SourceIPHashMapper"; }
+  void *cast(const char *);
+  
+  SourceIPHashMapper *clone() const	{ return new SourceIPHashMapper; }
+  int configure_phase() const		{ return IPRw::CONFIGURE_PHASE_MAPPER;}
+  int configure(Vector<String> &, ErrorHandler *);
+  void cleanup(CleanupStage);
+  
+  void notify_rewriter(IPRw *, ErrorHandler *);
+  IPRw::Mapping *get_map(IPRw *, int ip_p, const IPFlowID &, Packet *);
+
+protected:
+  int parse_server (const String &conf, IPRw::Pattern **pstore,
+		    int *fport_store, int *rport_store,
+		    int *id_store, Element *e, ErrorHandler *errh);
+
+ private:
+
+  Vector<IPRw::Pattern *> _patterns;
+  Vector<int> _forward_outputs;
+  Vector<int> _reverse_outputs;
+  chash_t<unsigned int> *_hasher;
+};
+
+CLICK_ENDDECLS
+#endif
