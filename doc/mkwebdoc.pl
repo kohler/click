25e5039eebd5325c044450e804e8093ab1847d4e
eddietwo <eddietwo>
2004-03-19 09:35:56 +0000

    nits

--- doc/mkwebdoc.pl
+++ doc/mkwebdoc.pl
@@ -196,13 +196,13 @@ sub element_li ($) {
     my($e) = @_;
     my($t) = "<li><a href='$e.n.html'>$e</a>";
     my(@x);
-    push @x, "<a href='#D'>D</a>" if $edeprecated{$e};
+    push @x, "<b>D</b>" if $edeprecated{$e};
     if ($ereq{$e}) {
 	my($r) = $ereq{$e};
-	push @x, "<a href='#U'>U</a>" if $r =~ /\buserlevel\b/;
-	push @x, "<a href='#L'>L</a>" if $r =~ /\blinuxmodule\b/;
-	push @x, "<a href='#B'>B</a>" if $r =~ /\bbsdmodule\b/;
-	push @x, "<a href='#Ns'>Ns</a>" if $r =~ /\bns\b/;
+	push @x, "<b>U</b>" if $r =~ /\buserlevel\b/;
+	push @x, "<b>L</b>" if $r =~ /\blinuxmodule\b/;
+	push @x, "<b>B</b>" if $r =~ /\bbsdmodule\b/;
+	push @x, "<b>Ns</b>" if $r =~ /\bns\b/;
     }
     $t .= " <small>[" . join('&nbsp;', @x) . "]</small>" if @x;
     "$t</li>\n";
