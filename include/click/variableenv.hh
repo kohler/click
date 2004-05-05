bc8c39cfa99c58e5897b4b18438a0cd486bd873e
eddietwo <eddietwo>
2004-05-05 21:51:25 +0000

    whitespace

--- include/click/variableenv.hh
+++ include/click/variableenv.hh
@@ -1,4 +1,4 @@
-// -*- c-basic-offset: 2; related-file-name: "../../lib/variableenv.cc" -*-
+// -*- c-basic-offset: 4; related-file-name: "../../lib/variableenv.cc" -*-
 #ifndef CLICK_VARIABLEENVIRONMENT_HH
 #define CLICK_VARIABLEENVIRONMENT_HH
 #include <click/string.hh>
@@ -7,26 +7,26 @@ CLICK_DECLS
 
 class VariableEnvironment { public:
   
-  VariableEnvironment()				{ }
-  VariableEnvironment(const String &suffix);
-  VariableEnvironment(const VariableEnvironment &, const String &suffix);
+    VariableEnvironment()		{ }
+    VariableEnvironment(const String &suffix);
+    VariableEnvironment(const VariableEnvironment &, const String &suffix);
 
-  operator bool() const			{ return _formals.size() != 0; }
-  const String &prefix() const		{ return _prefix; }
-  int depth() const			{ return _depths.size() ? _depths.back() : -1; }
+    operator bool() const		{ return _formals.size() != 0; }
+    const String &prefix() const	{ return _prefix; }
+    int depth() const			{ return _depths.size() ? _depths.back() : -1; }
 
-  void enter(const VariableEnvironment &);
-  void enter(const Vector<String> &formals, const Vector<String> &values, int depth);
-  void limit_depth(int);
+    void enter(const VariableEnvironment &);
+    void enter(const Vector<String> &formals, const Vector<String> &values, int depth);
+    void limit_depth(int);
   
-  String interpolate(const String &) const;
+    String interpolate(const String &) const;
 
- private:
+  private:
 
-  String _prefix;
-  Vector<String> _formals;
-  Vector<String> _values;
-  Vector<int> _depths;
+    String _prefix;
+    Vector<String> _formals;
+    Vector<String> _values;
+    Vector<int> _depths;
 
 };
 
