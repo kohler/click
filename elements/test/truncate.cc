6c18bf38083d261cb8731798ffece55e6aad529e
eddietwo <eddietwo>
2004-07-10 20:52:52 +0000

    add Truncate element (mirror image of Strip)

---
+++ elements/test/truncate.cc
@@ -0,0 +1,54 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+/*
+ * truncate.{cc,hh} -- element strips bytes from back of packet
+ * Eddie Kohler
+ *
+ * Copyright (c) 2004 Regents of the University of California
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "truncate.hh"
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/glue.hh>
+CLICK_DECLS
+
+Truncate::Truncate()
+    : Element(1, 1)
+{
+    MOD_INC_USE_COUNT;
+}
+
+Truncate::~Truncate()
+{
+    MOD_DEC_USE_COUNT;
+}
+
+int
+Truncate::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    return cp_va_parse(conf, this, errh,
+		       cpUnsigned, "number of bytes to strip", &_nbytes,
+		       cpEnd);
+}
+
+Packet *
+Truncate::simple_action(Packet *p)
+{
+    p->take(_nbytes);
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(Truncate)
+ELEMENT_MT_SAFE(Truncate)
---
+++ elements/test/truncate.hh
@@ -0,0 +1,36 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+#ifndef CLICK_TRUNCATE_HH
+#define CLICK_TRUNCATE_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * Truncate(MAX)
+ * =s encapsulation
+ * strips bytes from the back of packets
+ * =d
+ * Deletes the last MAX bytes from each packet.
+ * =a Strip
+ */
+
+class Truncate : public Element { public:
+
+    Truncate();
+    ~Truncate();
+
+    const char *class_name() const		{ return "Truncate"; }
+    const char *processing() const		{ return AGNOSTIC; }
+
+    int configure(Vector<String> &, ErrorHandler *);
+
+    Packet *simple_action(Packet *);
+
+  private:
+
+    unsigned _nbytes;
+
+};
+
+CLICK_ENDDECLS
+#endif
