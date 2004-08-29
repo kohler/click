659743622cc9f38071a68911df08e01197c2e1a5
jbicket <jbicket>
2004-08-29 17:31:47 +0000

    more byte-error analysis elements

---
+++ elements/wifi/analysis/errorthreshold.hh
@@ -0,0 +1,43 @@
+#ifndef CLICK_ERRORTHRESHOLD_HH
+#define CLICK_ERRORTHRESHOLD_HH
+#include <click/element.hh>
+#include <click/etheraddress.hh>
+#include <click/bighashmap.hh>
+#include <click/glue.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * 
+ * ErrorThreshold()
+ * 
+ * =s wifi
+ * 
+ * Accumulate errorthreshold for each ethernet src you hear a packet from.
+ * =over 8
+ *
+ *
+ */
+
+
+class ErrorThreshold : public Element { public:
+  
+  ErrorThreshold();
+  ~ErrorThreshold();
+  
+  const char *class_name() const		{ return "ErrorThreshold"; }
+  const char *processing() const		{ return PUSH; }
+  
+  int configure(Vector<String> &, ErrorHandler *);
+  bool can_live_reconfigure() const		{ return true; }
+
+  void push (int port, Packet *p_in);
+
+  void add_handlers();
+  void notify_noutputs(int n);
+  unsigned _correct_threshold;
+  unsigned _length;
+};
+
+CLICK_ENDDECLS
+#endif
