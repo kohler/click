57b022d96da9b6867af833e9e768177216fe03ac
eddietwo <eddietwo>
2004-09-30 01:04:16 +0000

    add FullNoteQueue: a NotifierQueue that adds a nonfull notifier

---
+++ elements/standard/fullnotequeue.cc
@@ -0,0 +1,117 @@
+// -*- c-basic-offset: 4 -*-
+/*
+ * fullnotequeue.{cc,hh} -- queue element that notifies on full
+ * Eddie Kohler
+ *
+ * Copyright (c) 2004 Regents of the University of California
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "fullnotequeue.hh"
+CLICK_DECLS
+
+FullNoteQueue::FullNoteQueue()
+{
+    MOD_INC_USE_COUNT;
+}
+
+FullNoteQueue::~FullNoteQueue()
+{
+    MOD_DEC_USE_COUNT;
+}
+
+void *
+FullNoteQueue::cast(const char *n)
+{
+    if (strcmp(n, "FullNoteQueue") == 0)
+	return (FullNoteQueue *)this;
+    else if (strcmp(n, Notifier::NONFULL_NOTIFIER) == 0)
+	return static_cast<Notifier*>(&_full_note);
+    else
+	return NotifierQueue::cast(n);
+}
+
+int
+FullNoteQueue::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    _full_note.initialize(router());
+    _full_note.set_signal_active(true);
+    return NotifierQueue::configure(conf, errh);
+}
+
+void
+FullNoteQueue::push(int, Packet *p)
+{
+    // Code taken from SimpleQueue::push().
+    int next = next_i(_tail);
+
+    if (next != _head) {
+	_q[_tail] = p;
+	_tail = next;
+
+	int s = size();
+	if (s > _highwater_length)
+	    _highwater_length = s;
+
+#if !NOTIFIERQUEUE_LOCK
+	// This can leave a single packet in the queue indefinitely in
+	// multithreaded Click, because of a race condition with pull().
+        if (!signal_active()) 
+	    wake_listeners(); 
+#else
+        if (s == 1) {
+            _lock.acquire();
+	    if (!signal_active())
+	        wake_listeners();
+	    _lock.release();
+	}
+#endif
+
+	if (s == capacity())
+	    _full_note.sleep_listeners();
+
+    } else {
+	if (_drops == 0)
+	    click_chatter("%{element}: overflow", this);
+	_drops++;
+	p->kill();
+    }
+}
+
+Packet *
+FullNoteQueue::pull(int)
+{
+    Packet *p = deq();
+
+    if (p) {
+	_sleepiness = 0;
+	if (size() == capacity() - 1)
+	    _full_note.wake_listeners();
+	
+    } else if (++_sleepiness == SLEEPINESS_TRIGGER) {
+#if !NOTIFIERQUEUE_LOCK
+        sleep_listeners();
+#else
+	_lock.acquire();
+	if (_head == _tail)  // if still empty...
+	    sleep_listeners();
+	_lock.release();
+#endif
+    }
+
+    return p;
+}
+
+CLICK_ENDDECLS
+ELEMENT_REQUIRES(NotifierQueue)
+EXPORT_ELEMENT(FullNoteQueue)
---
+++ elements/standard/fullnotequeue.hh
@@ -0,0 +1,77 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_FULLNOTEQUEUE_HH
+#define CLICK_FULLNOTEQUEUE_HH
+#include "notifierqueue.hh"
+CLICK_DECLS
+
+/*
+=c
+
+Queue
+Queue(CAPACITY)
+
+=s storage
+
+stores packets in a FIFO queue
+
+=d
+
+Stores incoming packets in a first-in-first-out queue.
+Drops incoming packets if the queue already holds CAPACITY packets.
+The default for CAPACITY is 1000.
+
+Queue notifies interested parties when it becomes empty and when a
+formerly-empty queue receives a packet. The empty notification takes place
+some time after the queue goes empty, to prevent thrashing for queues that
+hover around 1 or 2 packets long. In all other respects, Queue behaves like
+SimpleQueue.
+
+You may also use the old element name "NotifierQueue".
+
+=h length read-only
+
+Returns the current number of packets in the queue.
+
+=h highwater_length read-only
+
+Returns the maximum number of packets that have ever been in the queue at once.
+
+=h capacity read/write
+
+Returns or sets the queue's capacity.
+
+=h drops read-only
+
+Returns the number of packets dropped by the queue so far.
+
+=h reset_counts write-only
+
+When written, resets the C<drops> and C<highwater_length> counters.
+
+=h reset write-only
+
+When written, drops all packets in the queue.
+
+=a SimpleQueue, MixedQueue, FrontDropQueue */
+
+class FullNoteQueue : public NotifierQueue { public:
+
+    FullNoteQueue();
+    ~FullNoteQueue();
+
+    const char *class_name() const		{ return "FullNoteQueue"; }
+    void *cast(const char *);
+
+    int configure(Vector<String> &conf, ErrorHandler *);
+    
+    void push(int port, Packet *);
+    Packet *pull(int port);
+
+  private:
+
+    ActiveNotifier _full_note;
+
+};
+
+CLICK_ENDDECLS
+#endif
