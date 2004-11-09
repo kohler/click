14998f0da54e34a4e8475d6f2eaf476a4cdb8bab
eddietwo <eddietwo>
2004-11-09 20:24:12 +0000

    documentation

--- elements/test/schedordertest.hh
+++ elements/test/schedordertest.hh
@@ -9,30 +9,48 @@ CLICK_DECLS
 /*
 =c
 
-SchedOrderTest(ID, [<keyword> SIZE])
+SchedOrderTest(ID, [<keyword> SIZE, LIMIT, STOP])
 
 =s test
 
 remembers scheduling order
 
+=io
+
+None
+
 =d
+
+SchedOrderTest elements repeatedly schedule themselves, and keep track of the
+order in which they were scheduled.  ID is an integer used to distinguish
+between different SchedOrderTest elements.  The "order" handler reports the
+sequence of IDs corresponding to the order in which the first SIZE
+SchedOrderTest tasks were scheduled.  Use ScheduleInfo to set SchedOrderTest's
+tickets.
 
 =over 8
 
 =item SIZE
 
-Integer.
+Integer.  The maximum length of the stored ID sequence.  Default is 1024.
 
 =item LIMIT
 
-Unsigned.
+Unsigned.  SchedOrderTest will schedule itself at most LIMIT times.  0 means
+forever.  Default is 0.
 
 =item STOP
 
-Boolean.
+Boolean.  If true, SchedOrderTest will stop the driver when the ID sequence is
+full.  (Note that this has to do with SIZE, not LIMIT.)  Default is false.
 
 =h order read-only
 
+Reports the ID sequence as a space-separated list of integers.
+
+=a
+
+ScheduleInfo
 */
 
 class SchedOrderTest : public Element { public:
