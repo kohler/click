7c0894879af00ee7c2dec2ac25796a744ab43185
eddietwo <eddietwo>
2005-02-25 21:27:28 +0000

    include exportstub: definitions of click_export_elements() and click_unexport_elements(), which applications can override

---
+++ lib/exportstub.cc
@@ -0,0 +1,29 @@
+// -*- c-basic-offset: 4 -*-
+/*
+ * exportstub.cc -- stub export_elements() and unexport_elements()
+ * Eddie Kohler
+ *
+ * Copyright (c) 2005 Regents of the University of California
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+
+void
+click_export_elements()
+{
+}
+
+void
+click_unexport_elements()
+{
+}
