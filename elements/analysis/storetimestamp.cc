8dfb5f07dff618c6a0df7df63ca1d1bf5f2d72ee
eddietwo <eddietwo>
2005-08-17 09:48:44 +0000

    add potentially useful element

---
+++ elements/analysis/storetimestamp.cc
@@ -0,0 +1,68 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+/*
+ * storetimestamp.{cc,hh} -- element stores timestamp in packet data
+ * Eddie Kohler
+ *
+ * Copyright (c) 2003 International Computer Science Institute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include <click/error.hh>
+#include "storetimestamp.hh"
+#include <click/confparse.hh>
+#include <click/straccum.hh>
+CLICK_DECLS
+
+StoreTimestamp::StoreTimestamp()
+    : Element(1, 1)
+{
+    MOD_INC_USE_COUNT;
+}
+
+StoreTimestamp::~StoreTimestamp()
+{
+    MOD_DEC_USE_COUNT;
+}
+
+int
+StoreTimestamp::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    _offset = -1;
+    bool tail = false;
+    if (cp_va_parse(conf, this, errh,
+		    cpOptional,
+		    cpInteger, "storage offset", &_offset,
+		    cpKeywords,
+		    "TAIL", cpBool, "put timestamp at end of packet?", &tail,
+		    "OFFSET", cpInteger, "storage offset", &_offset,
+		    0) < 0)
+	return -1;
+    if (_offset >= 0 ? tail : !tail)
+	return errh->error("supply exactly one of 'OFFSET' and 'TAIL'");
+    return 0;
+}
+
+Packet *
+StoreTimestamp::simple_action(Packet *p)
+{
+    int offset = (_offset < 0 ? p->length() : _offset);
+    int delta = offset + 8 - p->length();
+    if (WritablePacket *q = p->put(delta < 0 ? 0 : delta)) {
+	memcpy(q->data() + offset, &q->timestamp_anno(), 8);
+	return q;
+    } else
+	return 0;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(StoreTimestamp)
---
+++ elements/analysis/storetimestamp.hh
@@ -0,0 +1,59 @@
+// -*- mode: c++; c-basic-offset: 4 -*-
+#ifndef CLICK_STORETIMESTAMP_HH
+#define CLICK_STORETIMESTAMP_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+
+StoreTimestamp([I<keywords> OFFSET, TAIL])
+
+=s measurement, analysis
+
+store packet timestamps in packet data
+
+=d
+
+StoreTimestamp stores each packet's timestamp annotation somewhere in its
+data.  Supply exactly one of the OFFSET and TAIL keywords.
+
+Keyword arguments are:
+
+=over 8
+
+=item OFFSET
+
+Integer.  Store the timestamp starting OFFSET bytes into the packet.  The
+packet will be extended, if necessary, so that it's at least OFFSET+8 bytes
+long.
+
+=item TAIL
+
+Boolean.  If true, the timestamp will be tacked on to the end of the packet.
+
+=back
+
+=a
+
+SetTimestamp */
+
+class StoreTimestamp : public Element { public:
+
+    StoreTimestamp();
+    ~StoreTimestamp();
+
+    const char *class_name() const	{ return "StoreTimestamp"; }
+    const char *processing() const	{ return AGNOSTIC; }
+    int configure(Vector<String> &, ErrorHandler *);
+
+    Packet *simple_action(Packet *);
+    
+  private:
+
+    int _offset;
+    
+};
+
+CLICK_ENDDECLS
+#endif
