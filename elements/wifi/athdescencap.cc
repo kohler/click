bb3574155bcf2cb3ffa018a48ebd359cffafc6b9
eddietwo <eddietwo>
2006-03-04 08:33:07 +0000

    intptr_t

--- elements/wifi/athdescencap.cc
+++ elements/wifi/athdescencap.cc
@@ -91,7 +91,7 @@ AthdescEncap_write_param(const String &in_s, Element *e, void *vparam,
 {
   AthdescEncap *f = (AthdescEncap *)e;
   String s = cp_uncomment(in_s);
-  switch((int)vparam) {
+  switch((intptr_t)vparam) {
   case H_DEBUG: {    //debug
     bool debug;
     if (!cp_bool(s, &debug)) 
