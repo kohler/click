ade1ef38282cd55347133e98174e6f471e1f1536
eddietwo <eddietwo>
2006-09-19 00:55:25 +0000

    add patch from Adam Greenhalgh and Beyers Cronje to avoid many tx timeouts: set time_stamp appropriately, as the other transmit paths do

--- drivers/e1000-5.x/src/e1000_main.c
+++ drivers/e1000-5.x/src/e1000_main.c
@@ -3493,6 +3493,8 @@ e1000_tx_pqueue(struct net_device *netdev, struct sk_buff *skb)
 			       skb->data + offset,
 			       len,
 			       PCI_DMA_TODEVICE);
+	/* thanks Adam Greenhalgh and Beyers Cronje! */
+	adapter->tx_ring.buffer_info[i].time_stamp = jiffies;
 
 	tx_desc->buffer_addr = cpu_to_le64(adapter->tx_ring.buffer_info[i].dma);
 	tx_desc->lower.data = cpu_to_le32(txd_lower | len);
