13ac13b186c94d5fc5f3e4850d433a98916483c2
eddietwo <eddietwo>
2007-06-16 20:39:02 +0000

    copyright fix

--- lib/glue.cc
+++ lib/glue.cc
@@ -4,6 +4,7 @@
  * Robert Morris, Eddie Kohler
  *
  * Copyright (c) 1999-2000 Massachusetts Institute of Technology
+ * Copyright (c) 2007 Regents of the University of California
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
