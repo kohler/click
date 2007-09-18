862612cd6e461287fbeacb0d180844a4634ee66f
Eddie Kohler <kohler@cs.ucla.edu>
2007-09-18 08:55:47 -0700

    Correctly spell a configuration variable.
    NOW LEAVING MORON COUNTRY #!(*&$^^(*&(*&(*!(*)$#!)!)@(#*()@(!  Also, let
    the Linux build process include autoconf.h for us; don't override it.

--- include/click/fixconfig.h
+++ include/click/fixconfig.h
@@ -1,7 +1,7 @@
 #ifndef CLICK_FIXCONFIG_H
 #define CLICK_FIXCONFIG_H 1
 
-#if CLICK_LINUXMODULE && HAVE_ASM_ALTERNATIVE_H
+#if CLICK_LINUXMODULE && HAVE_LINUX_ASM_ALTERNATIVE_H
 // The .smp_locks section and C++-style weak linkage interact badly.
 # if CONFIG_SMP && (defined(__i386__) || defined(__x86_64__))
 #  include <asm/alternative.h>
