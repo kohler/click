1658409a363f0f13071ab316857bb55d33f11e8e
Eddie Kohler <kohler@cs.ucla.edu>
2008-02-19 14:35:20 -0800

    Correct documentation.

--- elements/test/clptest.hh
+++ elements/test/clptest.hh
@@ -15,43 +15,8 @@ runs regression tests for CLP command line parser
 
 =d
 
-SortTest runs click_qsort regression tests at initialization time. It
-does not route packets.
-
-If additional arguments are provided, SortTest will not perform its normal
-tests.  Instead, it will sort those arguments and optionally print out the
-results.  At userlevel a file can be sorted as well.
-
-Keyword arguments are:
-
-=over 8
-
-=item FILE
-
-Filename.  Sorts the lines of FILE.
-
-=item NUMERIC
-
-Boolean.  Sort values as numeric.  Default is false.
-
-=item REVERSE
-
-Boolean.  Reverse sort values.  Default is false.
-
-=item PERMUTE
-
-Boolean.  Stable sort values.  Default is false.
-
-=item STDC
-
-Boolean.  Use standard C qsort, not Click sort.  Default is false.
-
-=item OUTPUT
-
-Boolean.  If true, results of the extra sort are printed to standard output.
-Default is false.
-
-=back
+CLPTest runs regression tests for the CLP command line parsing library at
+initialization time. It does not route packets.
 
 */
 
