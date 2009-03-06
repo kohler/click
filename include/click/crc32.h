e38f25d3fc41ee855a99aee038be4be47ea6e055
Eddie Kohler <ekohler@gmail.com>
2009-03-05 17:16:21 -0800

    CRC32: Const correctness.

--- include/click/crc32.h
+++ include/click/crc32.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 unsigned long update_crc(unsigned long crc_accum,
-                         char *data_blk_ptr,
+                         const char *data_blk_ptr,
                          int data_blk_size);
 
 #ifdef __cplusplus
--- lib/crc32.c
+++ lib/crc32.c
@@ -62,7 +62,7 @@ gen_crc_table()
  */
 unsigned long
 update_crc(unsigned long crc_accum,
-           char *data_blk_ptr,
+           const char *data_blk_ptr,
            int data_blk_size)
 {
   int i, j;
