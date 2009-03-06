c19d9ec12ea6b4130b2586ee8578347d7fb648c1
Eddie Kohler <ekohler@gmail.com>
2009-03-05 17:28:54 -0800

    Hub: Add new element.
    Hub acts like an Ethernet hub, sending packets out all outputs except the
    one corresponding to the input, but is not Ethernet specific.

---
+++ elements/standard/hub.cc
@@ -0,0 +1,48 @@
+/*
+ * hub.{cc,hh} -- element duplicates packets like a hub
+ * Eddie Kohler
+ *
+ * Copyright (c) 2008 Meraki, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "hub.hh"
+CLICK_DECLS
+
+Hub::Hub()
+{
+}
+
+Hub::~Hub()
+{
+}
+
+void
+Hub::push(int port, Packet *p)
+{
+    int n = 0, nout = noutputs();
+    for (int i = 0; i < nout; ++i) {
+	Packet *q;
+	if (i == port)
+	    q = 0;
+	else if (++n == nout - 1)
+	    q = p;
+	else
+	    q = p->clone();
+	if (q)
+	    output(i).push(q);
+    }
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(Hub)
---
+++ elements/standard/hub.hh
@@ -0,0 +1,41 @@
+#ifndef CLICK_HUB_HH
+#define CLICK_HUB_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+
+Hub()
+
+=s basictransfer
+
+duplicates packets like a hub
+
+=d
+
+Hub sends a copy of each input packet out each output port.  However, a packet
+received on input port N is not emitted on output port N.  Thus, the element
+acts sort of like an Ethernet hub (but it is not Ethernet specific).
+
+=a
+
+Tee, EtherSwitch
+*/
+
+class Hub : public Element { public:
+
+    Hub();
+    ~Hub();
+
+    const char *class_name() const		{ return "Hub"; }
+    const char *port_count() const		{ return "-/="; }
+    const char *processing() const		{ return PUSH; }
+    const char *flow_code() const		{ return "#/[^#]"; }
+
+    void push(int port, Packet* p);
+
+};
+
+CLICK_ENDDECLS
+#endif
