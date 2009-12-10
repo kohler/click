12c2c477aceba67fa0a13798d74e899a6fa4b05e
Eddie Kohler <ekohler@gmail.com>
2009-12-10 13:02:26 -0800

    SimplePullSwitch: Add new element.
    This element is just a clone of PullSwitch for now; but after the next
    commit it won't be.

---
+++ elements/simple/simplepullswitch.cc
@@ -0,0 +1,99 @@
+/*
+ * simplepullswitch.{cc,hh} -- element routes packets from one input of several
+ * Eddie Kohler
+ *
+ * Copyright (c) 2000 Mazu Networks, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "simplepullswitch.hh"
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/llrpc.h>
+CLICK_DECLS
+
+SimplePullSwitch::SimplePullSwitch()
+{
+}
+
+SimplePullSwitch::~SimplePullSwitch()
+{
+}
+
+int
+SimplePullSwitch::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    int input = 0;
+    if (cp_va_kparse(conf, this, errh,
+		     "INPUT", cpkP, cpInteger, &input,
+		     cpEnd) < 0)
+	return -1;
+    set_input(input);
+    return 0;
+}
+
+void
+SimplePullSwitch::set_input(int input)
+{
+    _input = (input < 0 || input >= ninputs() ? -1 : input);
+}
+
+Packet *
+SimplePullSwitch::pull(int)
+{
+    if (_input < 0)
+	return 0;
+    else
+	return input(_input).pull();
+}
+
+int
+SimplePullSwitch::write_param(const String &s, Element *e, void *, ErrorHandler *errh)
+{
+    SimplePullSwitch *sw = static_cast<SimplePullSwitch *>(e);
+    int input;
+    if (!cp_integer(s, &input))
+	return errh->error("syntax error");
+    sw->set_input(input);
+    return 0;
+}
+
+void
+SimplePullSwitch::add_handlers()
+{
+    add_data_handlers("switch", Handler::OP_READ, &_input);
+    add_write_handler("switch", write_param, 0);
+    add_data_handlers("config", Handler::OP_READ, &_input);
+    set_handler_flags("config", 0, Handler::CALM);
+}
+
+int
+SimplePullSwitch::llrpc(unsigned command, void *data)
+{
+    if (command == CLICK_LLRPC_SET_SWITCH) {
+	int32_t *val = reinterpret_cast<int32_t *>(data);
+	set_input(*val);
+	return 0;
+
+    } else if (command == CLICK_LLRPC_GET_SWITCH) {
+	int32_t *val = reinterpret_cast<int32_t *>(data);
+	*val = _input;
+	return 0;
+
+    } else
+	return Element::llrpc(command, data);
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(SimplePullSwitch)
+ELEMENT_MT_SAFE(SimplePullSwitch)
---
+++ elements/simple/simplepullswitch.hh
@@ -0,0 +1,67 @@
+#ifndef CLICK_SIMPLEPULLSWITCH_HH
+#define CLICK_SIMPLEPULLSWITCH_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+
+SimplePullSwitch([INPUT])
+
+=s scheduling
+
+forwards pull requests to settable input, without notification
+
+=d
+
+On every pull, SimplePullSwitch returns the packet pulled from one of its
+input ports -- specifically, INPUT. The default INPUT is zero; negative INPUTs
+mean always return a null packet. You can change INPUT with a write handler.
+PullSwitch has an unlimited number of inputs.
+
+=h switch read/write
+
+Return or set the K parameter.
+
+=h CLICK_LLRPC_GET_SWITCH llrpc
+
+Argument is a pointer to an integer, in which the Switch's K parameter is
+stored.
+
+=h CLICK_LLRPC_SET_SWITCH llrpc
+
+Argument is a pointer to an integer. Sets the K parameter to that integer.
+
+=a PullSwitch, StaticPullSwitch, PrioSched, RoundRobinSched, StrideSched,
+Switch */
+
+class SimplePullSwitch : public Element { public:
+
+    SimplePullSwitch();
+    ~SimplePullSwitch();
+
+    const char *class_name() const		{ return "SimplePullSwitch"; }
+    const char *port_count() const		{ return "-/1"; }
+    const char *processing() const		{ return PULL; }
+
+    int configure(Vector<String> &conf, ErrorHandler *errh);
+    bool can_live_reconfigure() const		{ return true; }
+    void add_handlers();
+
+    virtual void set_input(int input);
+
+    Packet *pull(int);
+
+    int llrpc(unsigned command, void *data);
+
+  protected:
+
+    int _input;
+
+    static String read_param(Element *, void *);
+    static int write_param(const String &, Element *, void *, ErrorHandler *);
+
+};
+
+CLICK_ENDDECLS
+#endif
