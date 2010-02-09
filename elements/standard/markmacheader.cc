8f4b7816c95e66b0354100c5baf8e381b1230a6e
Cliff Frey <cliff@meraki.net>
2010-02-09 14:31:24 -0800

    MarkMACHeader: Add new element.
    Signed-off-by: Eddie Kohler <ekohler@gmail.com>

---
+++ elements/standard/markmacheader.cc
@@ -0,0 +1,58 @@
+/*
+ * markmacheader.{cc,hh} -- element sets IP Header annotation
+ * Eddie Kohler, Cliff Frey
+ *
+ * Copyright (c) 1999-2000 Massachusetts Institute of Technology
+ * Copyright (c) 2009 Meraki, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "markmacheader.hh"
+#include <click/confparse.hh>
+CLICK_DECLS
+
+MarkMACHeader::MarkMACHeader()
+{
+}
+
+MarkMACHeader::~MarkMACHeader()
+{
+}
+
+int
+MarkMACHeader::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    uint32_t offset = 0, length = 0;
+    if (cp_va_kparse(conf, this, errh,
+		     "OFFSET", cpkP, cpUnsigned, &offset,
+		     "LENGTH", cpkP, cpUnsigned, &length,
+		     cpEnd) < 0)
+	return -1;
+    _offset = offset;
+    _length = length;
+    return 0;
+}
+
+Packet *
+MarkMACHeader::simple_action(Packet *p)
+{
+    if (_length)
+	p->set_mac_header(p->data() + _offset, _length);
+    else
+	p->set_mac_header(p->data() + _offset);
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(MarkMACHeader)
+ELEMENT_MT_SAFE(MarkMACHeader)
---
+++ elements/standard/markmacheader.hh
@@ -0,0 +1,41 @@
+#ifndef CLICK_MARKMACHEADER_HH
+#define CLICK_MARKMACHEADER_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+ * =c
+ * MarkMACHeader([OFFSET, LENGTH])
+ * =s ip
+ * sets MAC header annotation
+ * =d
+ *
+ * Sets the MAC header annotation to start OFFSET bytes into the packet.
+ * OFFSET defaults to 0.  If LENGTH is provided and non-zero, the MAC header
+ * is LENGTH bytes long, so the network header annotation is set to
+ * OFFSET+LENGTH bytes into the packet.  The packet must be long enough to
+ * support the provided OFFSET.
+ *
+ * =a MarkIPHeader */
+
+class MarkMACHeader : public Element { public:
+
+    MarkMACHeader();
+    ~MarkMACHeader();
+
+    const char *class_name() const		{ return "MarkMACHeader"; }
+    const char *port_count() const		{ return PORTS_1_1; }
+    int configure(Vector<String> &, ErrorHandler *);
+    bool can_live_reconfigure() const		{ return true; }
+
+    Packet *simple_action(Packet *);
+
+  private:
+
+    int _offset;
+    int _length;
+
+};
+
+CLICK_ENDDECLS
+#endif
