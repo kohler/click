kohler/click

commit 9817f16ba5d5a10fa1dfc2c1cf3fef10ab128b67

Handlers: Separate comprehensive handlers for read & write.
This is an INCOMPATIBLE CHANGE, although I expect most user code to
continue to work without changes.

And take this opportunity to improve constant names and remove deprecated
functions.

After this change, a handler can have two different "comprehensive" (i.e.,
set_handler) handlers, one for read and one for write.  This will be useful
for data_handlers.

A side effect is that the Handler::user_data1() and Hander::user_data2()
values might be overwritten.  For example, after this:

  set_handler("foo", Handler::h_read, func1, 0, 1);

when func1 is called, it will see h->read_user_data() == 0 and
h->write_user_data() == 1.  But if this is also called:

  set_handler("foo", Handler::h_write, func2, 2, 3);

then when either func1 or func2 is called, they will see
h->read_user_data() == 0 and h->write_user_data() == 3 -- the read
handler's write_user_data is overwritten, as is the write handler's
read_user_data.

include/click/handler.hh
@@ -19,44 +19,45 @@ typedef int (*WriteHandlerCallback)(const String &data, Element *element,
 class Handler { public:
 
     enum Flags {
-	OP_READ = 0x0001,	///< @brief Handler supports read operations.
-	OP_WRITE = 0x0002,	///< @brief Handler supports write operations.
-	READ_PARAM = 0x0004,	///< @brief Read handler takes parameters.
-	COMPREHENSIVE = 0x0008,	///< @brief Use comprehensive callback for all
-				///  operations.
-	SPECIAL_FLAGS = OP_READ | OP_WRITE | READ_PARAM | COMPREHENSIVE,
-				///< @brief These flags may not be set by
-				///  Router::set_handler_flags().
-	EXCLUSIVE = 0,		///< @brief Handler is exclusive (the default):
+	h_read = 0x0001,	///< @brief Handler supports read operations.
+	h_write = 0x0002,	///< @brief Handler supports write operations.
+	h_read_param = 0x0004,	///< @brief Read handler takes parameters.
+	h_exclusive = 0,	///< @brief Handler is exclusive (the default):
 				///  router threads must stop while it is
 				///  called.
-	NONEXCLUSIVE = 0x0010,	///< @brief Handler is nonexclusive: router
+	h_nonexclusive = 0x0020,///< @brief Handler is nonexclusive: router
 				///  threads don't need to stop while it is
 				///  called.
-	RAW = 0x0020,		///< @brief Don't add newline to results.
-	READ_PRIVATE = 0x0040,	///< @brief Read handler private (invisible
+	h_raw = 0x0040,		///< @brief Don't add newline to results.
+	h_read_private = 0x0080,///< @brief Read handler private (invisible
 				///  outside the router configuration).
-	WRITE_PRIVATE = 0x0080,	///< @brief Write handler private (invisible
+	h_write_private = 0x0100,///< @brief Write handler private (invisible
 				///  outside the router configuration).
-	DEPRECATED = 0x0100,	///< @brief Handler is deprecated and available
+	h_deprecated = 0x0200,	///< @brief Handler is deprecated and available
 				///  only for compatibility.
-	UNCOMMON = 0x0200,	///< @brief User interfaces should not display
+	h_uncommon = 0x0400,	///< @brief User interfaces should not display
 				///  handler by default.
-	CALM = 0x0400,		///< @brief Read handler value changes rarely.
-	EXPENSIVE = 0x0800,	///< @brief Read handler is expensive to call.
-	BUTTON = 0x1000,	///< @brief Write handler ignores data.
-	CHECKBOX = 0x2000,	///< @brief Read/write handler is boolean and
+	h_calm = 0x0800,	///< @brief Read handler value changes rarely.
+	h_expensive = 0x1000,	///< @brief Read handler is expensive to call.
+	h_button = 0x2000,	///< @brief Write handler ignores data.
+	h_checkbox = 0x4000,	///< @brief Read/write handler is boolean and
 				///  should be rendered as a checkbox.
-	DRIVER_FLAG_SHIFT = 20,
-	DRIVER_FLAG_0 = 1 << DRIVER_FLAG_SHIFT,
+	h_driver_flag_shift = 20,
+	h_driver_flag_0 = 1 << h_driver_flag_shift,
 				///< @brief First uninterpreted handler flag
 				///  available for drivers.  Equals 1 <<
-				///  DRIVER_FLAG_SHIFT.
-	USER_FLAG_SHIFT = 25,
-	USER_FLAG_0 = 1 << USER_FLAG_SHIFT
+				///  h_driver_flag_shift.
+	h_user_flag_shift = 25,
+	h_user_flag_0 = 1 << h_user_flag_shift,
 				///< @brief First uninterpreted handler flag
 				///  available for element-specific use.
-				///  Equals 1 << USER_FLAG_SHIFT.
+				///  Equals 1 << h_user_flag_shift.
+
+	h_read_comprehensive = 0x0008,
+	h_write_comprehensive = 0x0010,
+	h_special_flags = h_read | h_write | h_read_param | h_read_comprehensive | h_write_comprehensive
+				///< @brief These flags may not be set by
+				///  Router::set_handler_flags().
     };
 
     /** @brief Return this handler's name. */
@@ -71,81 +72,54 @@ class Handler { public:
 	return _flags;
     }
 
-    /** @brief Return this handler's comprehensive callback function.
-     *
-     * Returns 0 if the handler doesn't have a comprehensive callback. */
-    inline HandlerCallback callback() const {
-	if (_flags & COMPREHENSIVE)
-	    return _hook.h;
-	else
-	    return 0;
+    /** @brief Return this handler's read callback data. */
+    inline void *read_user_data() const {
+	return _read_user_data;
     }
 
-    /** @brief Return this handler's read callback function.
-     *
-     * Returns 0 if the handler is not readable or has a comprehensive
-     * callback. */
-    inline ReadHandlerCallback read_callback() const {
-	if ((_flags & (OP_READ | COMPREHENSIVE)) == OP_READ)
-	    return _hook.rw.r;
-	else
-	    return 0;
+    /** @brief Return this handler's write callback data. */
+    inline void *write_user_data() const {
+	return _write_user_data;
     }
 
-    /** @brief Return this handler's write callback function.
-     *
-     * Returns 0 if the handler is not writable or has a comprehensive
-     * callback. */
-    inline WriteHandlerCallback write_callback() const {
-	if ((_flags & (OP_WRITE | COMPREHENSIVE)) == OP_WRITE)
-	    return _hook.rw.w;
-	else
-	    return 0;
-    }
-
-    /** @brief Return this handler's first callback data. */
-    inline void *user_data1() const {
-	return _thunk1;
-    }
-
-    /** @brief Return this handler's second callback data. */
-    inline void *user_data2() const {
-	return _thunk2;
-    }
+    /** @cond never */
+    inline void *user_data1() const CLICK_DEPRECATED;
+    inline void *user_data2() const CLICK_DEPRECATED;
+    /** @endcond never */
 
 
     /** @brief Check if this is a valid read handler. */
     inline bool readable() const {
-	return _flags & OP_READ;
+	return _flags & h_read;
     }
 
     /** @brief Check if this is a valid read handler that may accept
      * parameters. */
     inline bool read_param() const {
-	return _flags & READ_PARAM;
+	return _flags & h_read_param;
     }
 
     /** @brief Check if this is a public read handler.
      *
      * Private handlers may be not called from outside the router
      * configuration.  Handlers are public by default; to make a read handler
-     * private, add the READ_PRIVATE flag. */
+     * private, add the h_read_private flag. */
     inline bool read_visible() const {
-	return (_flags & (OP_READ | READ_PRIVATE)) == OP_READ;
+	return (_flags & (h_read | h_read_private)) == h_read;
     }
 
     /** @brief Check if this is a valid write handler. */
     inline bool writable() const {
-	return _flags & OP_WRITE;
+	return _flags & h_write;
     }
 
     /** @brief Check if this is a public write handler.
      *
      * Private handlers may not be called from outside the router
      * configuration.  Handlers are public by default; to make a write handler
-     * private, add the WRITE_PRIVATE flag. */
+     * private, add the h_write_private flag. */
     inline bool write_visible() const {
-	return (_flags & (OP_WRITE | WRITE_PRIVATE)) == OP_WRITE;
+	return (_flags & (h_write | h_write_private)) == h_write;
     }
 
     /** @brief Check if this is a public read or write handler. */
@@ -159,9 +133,9 @@ class Handler { public:
      * processing.  In the Linux kernel module driver, reading or writing an
      * exclusive handler using the Click filesystem will first lock all router
      * threads and handlers.  Handlers are exclusive by default.  Exclusivity
-     * is cleared by the NONEXCLUSIVE flag.  */
+     * is cleared by the h_nonexclusive flag.  */
     inline bool exclusive() const {
-	return !(_flags & NONEXCLUSIVE);
+	return !(_flags & h_nonexclusive);
     }
 
     /** @brief Check if spaces should be preserved when calling this handler.
@@ -170,7 +144,7 @@ class Handler { public:
      * values, for example by removing a terminating newline from write
      * handler values or adding a terminating newline to read handler values.
      * Raw handlers do not have their values manipulated in this way.  Rawness
-     * is set by the RAW flag.
+     * is set by the h_raw flag.
      *
      * <ul>
      * <li>The linuxmodule driver adds a terminating newline to non-raw read
@@ -183,7 +157,7 @@ class Handler { public:
      * handlers' values in any way.</li>
      * </ul> */
     inline bool raw() const {
-	return _flags & RAW;
+	return _flags & h_raw;
     }
 
 
@@ -196,19 +170,6 @@ class Handler { public:
      * relevant router's Router::root_element().  @a errh may be null, in
      * which case errors are reported to ErrorHandler::silent_handler(). */
     String call_read(Element *e, const String &param, ErrorHandler *errh) const;
-
-    /** @brief Call a read handler, possibly with parameters.
-     * @param e element on which to call the handler
-     * @param param parameters, or an empty string if no parameters
-     * @param raw true iff param is raw text (see raw())
-     * @param errh optional error handler
-     * @deprecated The @a raw argument is deprecated and ignored.
-     *
-     * The element must be nonnull; to call a global handler, pass the
-     * relevant router's Router::root_element().  @a errh may be null, in
-     * which case errors are reported to ErrorHandler::silent_handler(). */
-    inline String call_read(Element *e, const String &param, bool raw,
-			    ErrorHandler *errh) const CLICK_DEPRECATED;
 
     /** @brief Call a read handler without parameters.
      * @param e element on which to call the handler
@@ -230,19 +191,6 @@ class Handler { public:
      * relevant router's Router::root_element().  @a errh may be null, in
      * which case errors are reported to ErrorHandler::silent_handler(). */
     int call_write(const String &value, Element *e, ErrorHandler *errh) const;
-
-    /** @brief Call a write handler.
-     * @param value value to write to the handler
-     * @param e element on which to call the handler
-     * @param raw true iff value is raw text (see raw())
-     * @param errh optional error handler
-     * @deprecated The @a raw argument is deprecated and ignored.
-     *
-     * The element must be nonnull; to call a global handler, pass the
-     * relevant router's Router::root_element().  @a errh may be null, in
-     * which case errors are reported to ErrorHandler::silent_handler(). */
-    inline int call_write(const String &value, Element *e, bool raw,
-			  ErrorHandler *errh) const CLICK_DEPRECATED;
 
 
     /** @brief Unparse this handler's name.
@@ -271,18 +219,43 @@ class Handler { public:
 	return the_blank_handler;
     }
 
+
+    /** @cond never */
+    enum DeprecatedFlags {
+	OP_READ = h_read,
+	OP_WRITE = h_write,
+	READ_PARAM = h_read_param,
+	EXCLUSIVE = h_exclusive,
+	NONEXCLUSIVE = h_nonexclusive,
+	RAW = h_raw,
+	READ_PRIVATE = h_read_private,
+	WRITE_PRIVATE = h_write_private,
+	DEPRECATED = h_deprecated,
+	UNCOMMON = h_uncommon,
+	CALM = h_calm,
+	EXPENSIVE = h_expensive,
+	BUTTON = h_button,
+	CHECKBOX = h_checkbox,
+	DRIVER_FLAG_SHIFT = h_driver_flag_shift,
+	DRIVER_FLAG_0 = h_driver_flag_0,
+	USER_FLAG_SHIFT = h_user_flag_shift,
+	USER_FLAG_0 = h_user_flag_0
+    };
+    /** @endcond never */
+
   private:
 
     String _name;
     union {
 	HandlerCallback h;
-	struct {
-	    ReadHandlerCallback r;
-	    WriteHandlerCallback w;
-	} rw;
-    } _hook;
-    void *_thunk1;
-    void *_thunk2;
+	ReadHandlerCallback r;
+    } _read_hook;
+    union {
+	HandlerCallback h;
+	WriteHandlerCallback w;
+    } _write_hook;
+    void *_read_user_data;
+    void *_write_user_data;
     uint32_t _flags;
     int _use_count;
     int _next_by_name;
@@ -291,8 +264,7 @@ class Handler { public:
 
     Handler(const String & = String());
 
-    enum { combine_read, combine_write, combine_comprehensive };
-    inline void combine(const Handler &x, int type);
+    inline void combine(const Handler &x);
     inline bool compatible(const Handler &x) const;
 
     friend class Router;
@@ -302,24 +274,23 @@ class Handler { public:
 /* The largest size a write handler is allowed to have. */
 #define LARGEST_HANDLER_WRITE 65536
 
-inline String
-Handler::call_read(Element *e, const String &param, bool,
-		   ErrorHandler *errh) const
-{
-    return call_read(e, param, errh);
-}
-
-inline int
-Handler::call_write(const String &value, Element *e, bool,
-		    ErrorHandler *errh) const
-{
-    return call_write(value, e, errh);
-}
-
 typedef HandlerCallback HandlerHook CLICK_DEPRECATED;
 typedef ReadHandlerCallback ReadHandlerHook CLICK_DEPRECATED;
 typedef WriteHandlerCallback WriteHandlerHook CLICK_DEPRECATED;
 
+/** @cond never */
+inline void *
+Handler::user_data1() const
+{
+    return _read_user_data;
+}
+
+inline void *
+Handler::user_data2() const
+{
+    return _write_user_data;
+}
+/** @endcond never */
+
 CLICK_ENDDECLS
 #endif
-

elements/analysis/aggpktcounter.cc
@@ -300,7 +300,7 @@ AggregatePacketCounter::thing_read_handler(int, String& s, Element* e, const Han
 	aggregate = 0;
     else if (!cp_integer(cp_uncomment(s), &aggregate))
 	return errh->error("argument should be aggregate number");
-    FlowFunc ff = (h->user_data1() ? &Flow::undelivered : &Flow::received);
+    FlowFunc ff = (h->read_user_data() ? &Flow::undelivered : &Flow::received);
     AggregatePacketCounter *apc = static_cast<AggregatePacketCounter *>(e);
     s = apc->flow_handler(aggregate, ff);
     return 0;
@@ -311,8 +311,8 @@ AggregatePacketCounter::add_handlers()
 {
     add_write_handler("clear", write_handler, (void *)H_CLEAR);
     add_read_handler("count", read_handler, (void *)H_COUNT);
-    set_handler("received", Handler::OP_READ | Handler::READ_PARAM, thing_read_handler, 0);
-    set_handler("undelivered", Handler::OP_READ | Handler::READ_PARAM, thing_read_handler, (void*) 1);
+    set_handler("received", Handler::h_read | Handler::h_read_param, thing_read_handler, 0);
+    set_handler("undelivered", Handler::h_read | Handler::h_read_param, thing_read_handler, (void*) 1);
 }
 
 CLICK_ENDDECLS

elements/ethernet/etherencap.cc
@@ -34,66 +34,66 @@ EtherEncap::~EtherEncap()
 int
 EtherEncap::configure(Vector<String> &conf, ErrorHandler *errh)
 {
-  unsigned etht;
-  if (cp_va_kparse(conf, this, errh,
-		   "ETHERTYPE", cpkP+cpkM, cpUnsigned, &etht,
-		   "SRC", cpkP+cpkM, cpEthernetAddress, &_ethh.ether_shost,
-		   "DST", cpkP+cpkM, cpEthernetAddress, &_ethh.ether_dhost,
-		   cpEnd) < 0)
-    return -1;
-  if (etht > 0xFFFF)
-    return errh->error("argument 1 (Ethernet encapsulation type) must be <= 0xFFFF");
-  _ethh.ether_type = htons(etht);
-  return 0;
+    unsigned etht;
+    if (cp_va_kparse(conf, this, errh,
+		     "ETHERTYPE", cpkP+cpkM, cpUnsigned, &etht,
+		     "SRC", cpkP+cpkM, cpEthernetAddress, &_ethh.ether_shost,
+		     "DST", cpkP+cpkM, cpEthernetAddress, &_ethh.ether_dhost,
+		     cpEnd) < 0)
+	return -1;
+    if (etht > 0xFFFF)
+	return errh->error("bad ETHERTYPE");
+    _ethh.ether_type = htons(etht);
+    return 0;
 }
 
 Packet *
 EtherEncap::smaction(Packet *p)
 {
-  if (WritablePacket *q = p->push_mac_header(14)) {
-    memcpy(q->data(), &_ethh, 14);
-    return q;
-  } else
-    return 0;
+    if (WritablePacket *q = p->push_mac_header(14)) {
+	memcpy(q->data(), &_ethh, 14);
+	return q;
+    } else
+	return 0;
 }
 
 void
 EtherEncap::push(int, Packet *p)
 {
-  if (Packet *q = smaction(p))
-    output(0).push(q);
+    if (Packet *q = smaction(p))
+	output(0).push(q);
 }
 
 Packet *
 EtherEncap::pull(int)
 {
-  if (Packet *p = input(0).pull())
-    return smaction(p);
-  else
-    return 0;
+    if (Packet *p = input(0).pull())
+	return smaction(p);
+    else
+	return 0;
 }
 
 String
 EtherEncap::read_handler(Element *e, void *thunk)
 {
-  EtherEncap *ee = static_cast<EtherEncap *>(e);
-  switch ((intptr_t)thunk) {
-   case 0:	return EtherAddress(ee->_ethh.ether_shost).unparse();
-   case 1:	return EtherAddress(ee->_ethh.ether_dhost).unparse();
-   case 2:	return String(ntohs(ee->_ethh.ether_type));
-   default:	return "<error>";
-  }
+    EtherEncap *ee = static_cast<EtherEncap *>(e);
+    switch ((intptr_t)thunk) {
+    case 0:	return EtherAddress(ee->_ethh.ether_shost).unparse();
+    case 1:	return EtherAddress(ee->_ethh.ether_dhost).unparse();
+    case 2:	return String(ntohs(ee->_ethh.ether_type));
+    default:	return "<error>";
+    }
 }
 
 void
 EtherEncap::add_handlers()
 {
-  add_read_handler("src", read_handler, (void *)0);
-  add_write_handler("src", reconfigure_keyword_handler, "1 SRC");
-  add_read_handler("dst", read_handler, (void *)1);
-  add_write_handler("dst", reconfigure_keyword_handler, "2 DST");
-  add_read_handler("etht", read_handler, (void *)2);
-  add_write_handler("etht", reconfigure_keyword_handler, "0 ETHERTYPE");
+    add_read_handler("src", read_handler, (void *)0);
+    add_write_handler("src", reconfigure_keyword_handler, "1 SRC");
+    add_read_handler("dst", read_handler, (void *)1);
+    add_write_handler("dst", reconfigure_keyword_handler, "2 DST");
+    add_read_handler("etht", read_handler, (void *)2);
+    add_write_handler("etht", reconfigure_keyword_handler, "0 ETHERTYPE");
 }
 
 CLICK_ENDDECLS

elements/standard/script.cc
@@ -714,7 +714,7 @@ Script::step_handler(int op, String &str, Element *e, const Handler *h, ErrorHan
     Script *scr = (Script *) e;
     String data = cp_uncomment(str);
     int nsteps, steptype;
-    int what = (uintptr_t) h->user_data1();
+    int what = (uintptr_t) h->write_user_data();
     scr->_run_handler_name = h->name();
     scr->_run_args = String();
     scr->_run_op = op;
@@ -771,7 +771,7 @@ Script::step_handler(int op, String &str, Element *e, const Handler *h, ErrorHan
 int
 Script::arithmetic_handler(int, String &str, Element *e, const Handler *h, ErrorHandler *errh)
 {
-    int what = (uintptr_t) h->user_data1();
+    int what = (uintptr_t) h->read_user_data();
 
     switch (what) {
 
@@ -1101,13 +1101,6 @@ Script::arithmetic_handler(int, String &str, Element *e, const Handler *h, Error
 	return 0;
     }
 
-    case ar_readexport: {
-	Script *scr = static_cast<Script *>(e);
-	int v = (intptr_t) h->user_data2();
-	str = scr->_vars[v + 1];
-	return 0;
-    }
-
     case ar_length:
 	str = String(str.length());
 	return 0;
@@ -1128,16 +1121,23 @@ int
 Script::star_write_handler(const String &str, Element *e, void *, ErrorHandler *)
 {
     Script *s = static_cast<Script *>(e);
-    s->set_handler(str, Handler::OP_READ | Handler::READ_PARAM | Handler::OP_WRITE, step_handler, ST_RUN, 0);
+    s->set_handler(str, Handler::OP_READ | Handler::READ_PARAM | Handler::OP_WRITE, step_handler, 0, ST_RUN);
     return Router::hindex(s, str);
+}
+
+String
+Script::read_export_handler(Element *e, void *user_data)
+{
+    Script *scr = static_cast<Script *>(e);
+    return scr->_vars[(intptr_t) user_data + 1];
 }
 
 void
 Script::add_handlers()
 {
-    set_handler("step", Handler::OP_WRITE, step_handler, ST_STEP, 0);
-    set_handler("goto", Handler::OP_WRITE, step_handler, ST_GOTO, 0);
-    set_handler("run", Handler::OP_READ | Handler::READ_PARAM | Handler::OP_WRITE, step_handler, ST_RUN, 0);
+    set_handler("step", Handler::OP_WRITE, step_handler, 0, ST_STEP);
+    set_handler("goto", Handler::OP_WRITE, step_handler, 0, ST_GOTO);
+    set_handler("run", Handler::OP_READ | Handler::READ_PARAM | Handler::OP_WRITE, step_handler, 0, ST_RUN);
     set_handler("add", Handler::OP_READ | Handler::READ_PARAM, arithmetic_handler, AR_ADD, 0);
     set_handler("sub", Handler::OP_READ | Handler::READ_PARAM, arithmetic_handler, AR_SUB, 0);
     set_handler("mul", Handler::OP_READ | Handler::READ_PARAM, arithmetic_handler, AR_MUL, 0);
@@ -1173,7 +1173,7 @@ Script::add_handlers()
 	add_write_handler("*", star_write_handler, 0);
     for (int i = 0; i < _insns.size(); ++i)
 	if (_insns[i] == insn_export || _insns[i] == insn_exportq)
-	    set_handler(_vars[_args[i]], Handler::OP_READ, arithmetic_handler, ar_readexport, _args[i]);
+	    add_read_handler(_vars[_args[i]], read_export_handler, _args[i]);
 }
 
 EXPORT_ELEMENT(Script)

elements/standard/script.hh
@@ -460,7 +460,7 @@ class Script : public Element { public:
 	AR_LT, AR_EQ, AR_GT, AR_GE, AR_NE, AR_LE, // order is important
 	AR_FIRST, AR_NOT, AR_SPRINTF, ar_random, ar_cat,
 	ar_and, ar_or, ar_nand, ar_nor, ar_now, ar_if, ar_in,
-	ar_readable, ar_writable, ar_readexport, ar_length, ar_unquote
+	ar_readable, ar_writable, ar_length, ar_unquote
     };
 
     void add_insn(int, int, int = 0, const String & = String());
@@ -471,6 +471,7 @@ class Script : public Element { public:
 
     static int step_handler(int, String&, Element*, const Handler*, ErrorHandler*);
     static int arithmetic_handler(int, String&, Element*, const Handler*, ErrorHandler*);
+    static String read_export_handler(Element*, void*);
     static int star_write_handler(const String&, Element*, void*, ErrorHandler*);
 
     friend class DriverManager;

elements/test/neighborhoodtest.cc
@@ -49,8 +49,8 @@ NeighborhoodTest::handler(int, String &data, Element *element,
     if (data && !cp_integer(data, &diameter))
 	return errh->error("syntax error");
     ElementNeighborhoodTracker tracker(element->router(), diameter);
-    intptr_t port = (intptr_t) handler->user_data2();
-    if (handler->user_data1())
+    intptr_t port = (intptr_t) handler->write_user_data();
+    if (handler->read_user_data())
 	element->router()->visit_downstream(element, port, &tracker);
     else
 	element->router()->visit_upstream(element, port, &tracker);

include/click/element.hh
@@ -157,8 +157,8 @@ class Element { public:
     void add_read_handler(const String &name, ReadHandlerCallback read_callback, int user_data, uint32_t flags = 0);
     void add_write_handler(const String &name, WriteHandlerCallback write_callback, const void *user_data = 0, uint32_t flags = 0);
     void add_write_handler(const String &name, WriteHandlerCallback write_callback, int user_data, uint32_t flags = 0);
-    void set_handler(const String &name, int flags, HandlerCallback callback, const void *user_data1 = 0, const void *user_data2 = 0);
-    void set_handler(const String &name, int flags, HandlerCallback callback, int user_data1, int user_data2 = 0);
+    void set_handler(const String &name, int flags, HandlerCallback callback, const void *read_user_data = 0, const void *write_user_data = 0);
+    void set_handler(const String &name, int flags, HandlerCallback callback, int read_user_data, int write_user_data = 0);
     int set_handler_flags(const String &name, int set_flags, int clear_flags = 0);
     void add_task_handlers(Task *task, const String& prefix = String());
 

commit 9c9309590992b22c5c2aa4d62a4048869151db6a

Linuxmodule: Correct compilation errors related to Handlers.
Problems introduced mistakenly in 9817f16ba5d5a10fa1dfc2c1cf3fef10ab128b67,
reported by Sami Ruponen and possibly others.

include/click/handler.hh
@@ -249,6 +249,19 @@ class Handler { public:
     };
     /** @endcond never */
 
+    /** @cond never */
+    /** @brief Call a read handler without parameters, passing new user_data().
+     * @param e element on which to call the handler
+     * @param new_user_data new user data
+     *
+     * This function should only be used for special purposes.  It fails
+     * unless called on a handler created with a ReadHandlerCallback. */
+    inline String __call_read(Element *e, void *new_user_data) const {
+	assert((_flags & (h_read | h_read_comprehensive)) == h_read);
+	return _read_hook.r(e, new_user_data);
+    }
+    /** @endcond never */
+
   private:
 
     String _name;

linuxmodule/clickfs.cc
@@ -4,7 +4,7 @@
  * Eddie Kohler
  *
  * Copyright (c) 2002-2003 International Computer Science Institute
- * Copyright (c) 2004-2007 Regents of the University of California
+ * Copyright (c) 2004-2010 Regents of the University of California
  * Copyright (c) 2008 Meraki, Inc.
  *
  * This source code is free software; you can redistribute it and/or modify it
@@ -606,7 +606,7 @@ handler_read(struct file *filp, char *buffer, size_t count, loff_t *store_f_pos)
 		hdi.store_f_pos = store_f_pos;
 		hdi.string = &handler_strings[stringno];
 		hdi.retval = 0;
-		(void) (*h->read_callback())(e, &hdi);
+		(void) h->__call_read(e, &hdi);
 		count = hdi.count;
 		retval = hdi.retval;
 	    } else if (h->exclusive()) {
@@ -723,10 +723,10 @@ handler_do_write(struct file *filp, void *address_ptr)
 	ClickfsHandlerErrorHandler cerrh;
 	if (h->exclusive()) {
 	    lock_threads();
-	    retval = h->call_write(data, e, true, &cerrh);
+	    retval = h->call_write(data, e, &cerrh);
 	    unlock_threads();
 	} else
-	    retval = h->call_write(data, e, true, &cerrh);
+	    retval = h->call_write(data, e, &cerrh);
 
 	handler_strings_info[stringno].flags |= HANDLER_DONE;
 