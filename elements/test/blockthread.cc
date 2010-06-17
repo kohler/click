c83bba84b8a56027e49d984a36f79061c3fb61fc
Eddie Kohler <ekohler@gmail.com>
2010-06-17 13:24:07 -0700

    BlockThread: Add new element useful for testing.

---
+++ elements/test/blockthread.cc
@@ -0,0 +1,52 @@
+// -*- c-basic-offset: 4 -*-
+/*
+ * blockthread.{cc,hh} -- test element that blocks execution
+ * Eddie Kohler
+ *
+ * Copyright (c) 2010 Meraki, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "blockthread.hh"
+#include <click/confparse.hh>
+#include <click/error.hh>
+CLICK_DECLS
+
+BlockThread::BlockThread()
+{
+}
+
+BlockThread::~BlockThread()
+{
+}
+
+int
+BlockThread::handler(int, String &str, Element *, const Handler *, ErrorHandler *errh)
+{
+    struct timeval tv;
+    if (!cp_time(str, &tv))
+	return errh->error("bad TIME");
+    int r = select(0, 0, 0, 0, &tv);
+    str = String(r);
+    return 0;
+}
+
+void
+BlockThread::add_handlers()
+{
+    set_handler("block", Handler::h_write | Handler::h_write_private, handler, 0, 0);
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(BlockThread)
+ELEMENT_REQUIRES(userlevel)
---
+++ elements/test/blockthread.hh
@@ -0,0 +1,43 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_BLOCKTHREAD_HH
+#define CLICK_BLOCKTHREAD_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+
+BlockThread()
+
+=s control
+
+provides handler for blocking thread execution
+
+=d
+
+BlockThread's "block" handler can be used to block a Click thread's execution
+for a specified time period.  This is only useful for testing at userlevel.
+
+=h block write-only
+
+Write this handler to block execution for a specified number of seconds.
+
+*/
+
+class BlockThread : public Element { public:
+
+    BlockThread();
+    ~BlockThread();
+
+    const char *class_name() const	{ return "BlockThread"; }
+
+    void add_handlers();
+
+  private:
+
+    static int handler(int, String&, Element*, const Handler*, ErrorHandler*);
+
+};
+
+CLICK_ENDDECLS
+#endif
