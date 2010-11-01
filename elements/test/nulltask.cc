b4a37951032cf51285ef5520fd9ce2bd9bb29af1
Eddie Kohler <ekohler@gmail.com>
2010-10-31 20:08:16 -0700

    NullTask: Add element useful for benchmarking tasks.

---
+++ elements/test/nulltask.cc
@@ -0,0 +1,81 @@
+// -*- c-basic-offset: 4 -*-
+/*
+ * nulltask.{cc,hh} -- schedule a do-nothing task
+ * Eddie Kohler
+ *
+ * Copyright (c) 2010 Regents of the University of California
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, subject to the conditions
+ * listed in the Click LICENSE file. These conditions include: you must
+ * preserve this copyright notice, and you cannot mention the copyright
+ * holders in advertising related to the Software without their permission.
+ * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
+ * notice is a summary of the Click LICENSE file; the license in that file is
+ * legally binding.
+ */
+
+#include <click/config.h>
+#include "nulltask.hh"
+#include <click/confparse.hh>
+#include <click/router.hh>
+#include <click/standard/scheduleinfo.hh>
+CLICK_DECLS
+
+NullTask::NullTask()
+    : _count(0), _limit(0), _task(this), _stop(false), _active(true)
+{
+}
+
+NullTask::~NullTask()
+{
+}
+
+int
+NullTask::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+    return cp_va_kparse(conf, this, errh,
+			"STOP", 0, cpBool, &_stop,
+			"LIMIT", 0, cpUnsigned, &_limit,
+			"ACTIVE", 0, cpBool, &_active,
+			cpEnd);
+}
+
+int
+NullTask::initialize(ErrorHandler *errh)
+{
+    ScheduleInfo::initialize_task(this, &_task, _active, errh);
+    return 0;
+}
+
+bool
+NullTask::run_task(Task *)
+{
+    ++_count;
+    if (_limit == 0 || _count < _limit)
+	_task.fast_reschedule();
+    if (_limit != 0 && _count >= _limit && _stop)
+	router()->please_stop_driver();
+    return true;
+}
+
+int
+NullTask::write_handler(const String &, Element *e, void *, ErrorHandler *)
+{
+    NullTask *nt = static_cast<NullTask *>(e);
+    nt->_count = 0;
+    return 0;
+}
+
+void
+NullTask::add_handlers()
+{
+    add_data_handlers("count", Handler::OP_READ, &_count);
+    add_data_handlers("limit", Handler::OP_READ | Handler::OP_WRITE, &_limit);
+    add_write_handler("reset", write_handler, 0);
+    add_task_handlers(&_task, 0, TASKHANDLER_WRITE_ALL);
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(NullTask)
---
+++ elements/test/nulltask.hh
@@ -0,0 +1,83 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_NULLTASK_HH
+#define CLICK_NULLTASK_HH
+#include <click/element.hh>
+#include <click/task.hh>
+CLICK_DECLS
+
+/*
+=c
+
+NullTask([<keyword> LIMIT, STOP, ACTIVE])
+
+=s test
+
+associated with a do-nothing Task
+
+=d
+
+NullTask simply schedule a task which, when scheduled, does nothing.
+This can be useful for benchmarking.
+
+=over 8
+
+=item LIMIT
+
+Unsigned.  NullTask will schedule itself at most LIMIT times.  0 means
+forever.  Default is 0.
+
+=item STOP
+
+Boolean.  If true, NullTask will stop the driver when LIMIT is
+reached.  Default is false.
+
+=item ACTIVE
+
+Boolean.  If false, NullTask will not schedule itself at initialization time.
+Use the C<scheduled> write handler to schedule the task later.  Default is
+true.
+
+=back
+
+=h count r
+
+Returns the number of times the element has been scheduled.
+
+=h reset w
+
+Resets the count to 0.
+
+=a
+
+ScheduleInfo
+*/
+
+class NullTask : public Element { public:
+
+    NullTask();
+    ~NullTask();
+
+    const char *class_name() const		{ return "NullTask"; }
+    const char *port_count() const		{ return PORTS_0_0; }
+    const char *processing() const		{ return AGNOSTIC; }
+
+    int configure(Vector<String> &, ErrorHandler *);
+    int initialize(ErrorHandler *);
+    void add_handlers();
+
+    bool run_task(Task *);
+
+  private:
+
+    uint32_t _count;
+    uint32_t _limit;
+    Task _task;
+    bool _stop;
+    bool _active;
+
+    static int write_handler(const String &str, Element *e, void *thunk, ErrorHandler *errh);
+
+};
+
+CLICK_ENDDECLS
+#endif
