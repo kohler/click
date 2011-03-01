b1aacc984fbd5d5b4f057cd5c717122f0b6c21f0
Eddie Kohler <ekohler@gmail.com>
2011-03-01 08:47:47 -0800

    Protect inclusion of <asm/alternative.h>.
    This is probably only important with relatively old Linux versions.
    
    Signed-off-by: Eddie Kohler <ekohler@gmail.com>

--- include/click/fixconfig.h
+++ include/click/fixconfig.h
@@ -4,9 +4,13 @@
 #if CLICK_LINUXMODULE && HAVE_LINUX_ASM_ALTERNATIVE_H
 // The .smp_locks section and C++-style weak linkage interact badly.
 # if CONFIG_SMP && (defined(__i386__) || defined(__x86_64__))
+#  include <click/cxxprotect.h>
+CLICK_CXX_PROTECT
 #  include <asm/alternative.h>
 #  undef LOCK_PREFIX
 #  define LOCK_PREFIX "lock ; "
+CLICK_CXX_UNPROTECT
+#  include <click/cxxunprotect.h>
 # endif
 #endif
 
