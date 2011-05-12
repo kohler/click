0955949303fc40522008854a1fc85b13e4449efa
Eddie Kohler <ekohler@gmail.com>
2011-05-11 22:33:30 -0700

    ClickyInfo: Add new element.
    This element does nothing; it exists merely to store information for Clicky.
    
    Signed-off-by: Eddie Kohler <ekohler@gmail.com>

---
+++ elements/standard/clickyinfo.hh
@@ -0,0 +1,31 @@
+#ifndef CLICK_CLICKYINFO_HH
+#define CLICK_CLICKYINFO_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+ClickyInfo(STYLE ...)
+
+=s information
+stores information for the Clicky GUI
+
+=d
+
+Click drivers ignore this element; it has no inputs or outputs and ignores its
+configuration arguments.  The Clicky GUI parses the configuration arguments
+and adds them to its styles.
+*/
+
+class ClickyInfo : public Element { public:
+
+    ClickyInfo();
+    ~ClickyInfo();
+
+    const char *class_name() const	{ return "ClickyInfo"; }
+    int configure(Vector<String> &conf, ErrorHandler *errh);
+
+};
+
+CLICK_ENDDECLS
+#endif
