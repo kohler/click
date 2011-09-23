kohler/click

commit 5f8be00f93dd6c7ada354a9f3758a8d7cc2c990f

remove unnecessary (void *) casts in add_read/write_handler calls
I did this by running:
bug:~/co/click$ git grep -l -E '\badd_(read|write)_handler.*\(void ?\*\) ?' elements/ > files
bug:~/co/click$ sed -r -i -e 's/(\badd_(read|write)_handler.*)\(void ?\*\) ?/\1/' `cat files`

and then making a few related manual changes inside of lib/

Signed-off-by: Cliff Frey <cliff@meraki.com>

lib/router.cc
@@ -2395,9 +2395,9 @@ Router::static_initialize()
 	add_read_handler(0, "scheduling_profile", router_read_handler, (void *) GH_SCHEDULING_PROFILE);
 #endif
 #if CLICK_STATS >= 2
-        add_read_handler(0, "element_cycles.csv", router_read_handler, (void *)GH_ELEMENT_CYCLES);
-        add_read_handler(0, "class_cycles.csv", router_read_handler, (void *)GH_CLASS_CYCLES);
-        add_write_handler(0, "reset_cycles", router_write_handler, (void *)GH_RESET_CYCLES);
+        add_read_handler(0, "element_cycles.csv", router_read_handler, GH_ELEMENT_CYCLES);
+        add_read_handler(0, "class_cycles.csv", router_read_handler, GH_CLASS_CYCLES);
+        add_write_handler(0, "reset_cycles", router_write_handler, GH_RESET_CYCLES);
 #endif
     }
 }

commit 6bdf3528409a905864f89423b10603b17ef822d7

fix build with --enable-stats=2
I accidentally broke this in 5f8be00f93dd6c7ada354a9f3758a8d7cc2c990f

Signed-off-by: Cliff Frey <cliff@meraki.com>

lib/router.cc
@@ -2395,9 +2395,9 @@ Router::static_initialize()
 	add_read_handler(0, "scheduling_profile", router_read_handler, (void *) GH_SCHEDULING_PROFILE);
 #endif
 #if CLICK_STATS >= 2
-        add_read_handler(0, "element_cycles.csv", router_read_handler, GH_ELEMENT_CYCLES);
-        add_read_handler(0, "class_cycles.csv", router_read_handler, GH_CLASS_CYCLES);
-        add_write_handler(0, "reset_cycles", router_write_handler, GH_RESET_CYCLES);
+        add_read_handler(0, "element_cycles.csv", router_read_handler, (void *)GH_ELEMENT_CYCLES);
+        add_read_handler(0, "class_cycles.csv", router_read_handler, (void *)GH_CLASS_CYCLES);
+        add_write_handler(0, "reset_cycles", router_write_handler, (void *)GH_RESET_CYCLES);
 #endif
     }
 }