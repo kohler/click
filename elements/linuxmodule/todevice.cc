67dc2b12e7a9dfe024558a0f0549e9d7b39b1484
Cliff Frey <cliff@meraki.com>
2013-02-09 10:09:25 -0800

    Revert "ToDevice/linuxmodule: Transmit skb with dev_queue_xmit()"
    This reverts commit d65bfe4054a23468c808e95df4ab6b4a5ef9d9a3.

--- elements/linuxmodule/todevice.cc
+++ elements/linuxmodule/todevice.cc
@@ -502,7 +502,8 @@ ToDevice::queue_packet(Packet *p, struct netdev_queue *txq)
 #endif
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 30)
-    ret = dev_queue_xmit(skb1);
+    // XXX should we call dev_hard_start_xmit???  Probably
+    ret = dev->netdev_ops->ndo_start_xmit(skb1, dev);
 #else
     ret = dev->hard_start_xmit(skb1, dev);
 #endif
