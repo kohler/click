61c2490f431f684653e874557d3cc5be563010bc
Tom Barbette <tom.barbette@ulg.ac.be>
2017-01-15 02:13:03 +0000

    DPDKInfo: NB_MBUF is not mandatory
    User may want to change something else, but it stays positional as it is
    the most used one

--- elements/userlevel/dpdkinfo.cc
+++ elements/userlevel/dpdkinfo.cc
@@ -29,7 +29,7 @@ int DPDKInfo::configure(Vector<String> &conf, ErrorHandler *errh) {
     }
     instance = this;
     if (Args(conf, this, errh)
-        .read_mp("NB_MBUF", DPDKDevice::NB_MBUF)
+        .read_p("NB_MBUF", DPDKDevice::NB_MBUF)
         .read("MBUF_SIZE", DPDKDevice::MBUF_SIZE)
         .read("MBUF_CACHE_SIZE", DPDKDevice::MBUF_CACHE_SIZE)
         .read("RX_PTHRESH", DPDKDevice::RX_PTHRESH)
