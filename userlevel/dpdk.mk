06900a1e9267a4fa632647a0195b9a13ed654519
Tom Barbette <tom.barbette@ulg.ac.be>
2017-02-24 17:08:17 +0100

    Compile with DPDK 17.02

--- userlevel/dpdk.mk
+++ userlevel/dpdk.mk
@@ -82,7 +82,7 @@ _LDLIBS-$(CONFIG_RTE_LIBRTE_MBUF)           += -lrte_mbuf
 _LDLIBS-$(CONFIG_RTE_LIBRTE_MBUF_OFFLOAD)   += -lrte_mbuf_offload
 _LDLIBS-$(CONFIG_RTE_LIBRTE_IP_FRAG)        += -lrte_ip_frag
 
-ifeq ($(shell [ -n "$(RTE_VER_YEAR)" ] &&  [ $(RTE_VER_YEAR) -ge 16 ] && [ $(RTE_VER_MONTH) -ge 11 ] && echo true),true)
+ifeq ($(shell [ -n "$(RTE_VER_YEAR)" ] && ( ( [ "$(RTE_VER_YEAR)" -ge 16 ] && [ "$(RTE_VER_MONTH)" -ge 11 ] ) || [ $(RTE_VER_YEAR) -ge 17 ] ) && echo true),true)
 _LDLIBS-$(CONFIG_RTE_LIBRTE_ETHER)          += -lrte_ethdev
 _LDLIBS-$(CONFIG_RTE_LIBRTE_NET)            += -lrte_net
 else
