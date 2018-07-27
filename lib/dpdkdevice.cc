ecd1247fb14caf86344b824ef0ba3520ab8250e1
Tom Barbette <tom.barbette@ulg.ac.be>
2018-07-27 10:30:03 +0200

    DPDK: Set RSS to hash also UDP and TCP by default
    In XL710 if a packet is UDP or TCP it is not considered to be IP, so
    RSS would not work with the old default parameter with XL710

--- lib/dpdkdevice.cc
+++ lib/dpdkdevice.cc
@@ -99,7 +99,7 @@ int DPDKDevice::initialize_device(ErrorHandler *errh)
 
     dev_conf.rxmode.mq_mode = ETH_MQ_RX_RSS;
     dev_conf.rx_adv_conf.rss_conf.rss_key = NULL;
-    dev_conf.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_IP;
+    dev_conf.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_IP | ETH_RSS_UDP | ETH_RSS_TCP;
 
     //We must open at least one queue per direction
     if (info.rx_queues.size() == 0) {
